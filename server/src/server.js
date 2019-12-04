7daeb7ecc873b8112db6d1a03656de36e7b445e7
darkfusion90 <gauravsharmaa341@gmail.com>
2019-12-04 10:36:48 +0530

    Sends socket id on connection Added create room and join room event listeners

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,34 @@
 const io = require('socket.io')();
+const roomManager = require('./roomManager');
 
-const generateRoom = require('./roomGenerator');
-console.log(generateRoom());
+
 /*Consider using one of the following fonts: Ubuntu, Open-Sans, Roboto, Oxygen, Lato*/
 
-function handleMessageBroadcasts(client) {
-   
-}
-
 io.on('connection', (client) => {
-    const randomRoom = generateRoom();
-    client.join(randomRoom);
-    client.emit('ROOM_JOINED', randomRoom);
+    const permanentId = client.id;
+
+    client.emit("USER_ID_RECIEVE", permanentId);
 
     client.on('SEND_MESSAGE', (roomId, message) => {
         io.sockets.in(client.rooms[roomId]).emit("NEW_MESSAGE_RECIEVED", { message: message, senderId: client.id });
+    })
+
+    client.on('CREATE_ROOM', (roomName, roomType, roomOwner, callback) => {
+        const room = roomManager.createRoom(roomName, roomType, roomOwner);
+        callback({
+            status: 200,
+            room: room
+        });
+        console.log(room);
+    })
+
+    client.on('JOIN_ROOM', (roomName, roomType, roomOwner, callback) => {
+        const room = roomManager.createRoom(roomName, roomType, roomOwner);
+        callback({
+            status: 200,
+            room: room
+        });
+        console.log(room);
     })
 })
 
