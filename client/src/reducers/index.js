15f97c1ea88b65dfeaf8a89eadb0caed2ebd1cd1
darkfusion90 <gauravsharmaa341@gmail.com>
2019-12-04 10:33:17 +0530

    Added two new reducers: createRoomReducer and updateUserIdReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 
 import connectToServerReducer from './connectToServerReducer';
 import recieveMessageReducer from './recieveMessageReducer';
+import createRoomReducer from './createRoomReducer';
+import updateUserIdReducer from './updateUserIdReducer';
 
 export default combineReducers({
+    user: updateUserIdReducer,
     serverConnection: connectToServerReducer,
+    room: createRoomReducer,
     messages: recieveMessageReducer
 })
