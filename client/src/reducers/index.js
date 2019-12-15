7e0162bcc25e4f2c156ba0669b2cde0f9521b588
darkfusion90 <gauravsharmaa341@gmail.com>
2019-12-15 16:36:50 +0530

    Changed room->roomMetadata Refactored different room reducers into single roomReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux';
 
-import connectToServerReducer from './connectToServerReducer';
-import recieveMessageReducer from './recieveMessageReducer';
-import createRoomReducer from './createRoomReducer';
+import serverReducer from './serverReducer';
+import messageReducer from './messageReducer';
+import roomReducer from './roomReducer';
 import updateUserIdReducer from './updateUserIdReducer';
 
 export default combineReducers({
     user: updateUserIdReducer,
-    serverConnection: connectToServerReducer,
-    room: createRoomReducer,
-    messages: recieveMessageReducer
+    serverConnection: serverReducer,
+    roomMetadata: roomReducer,
+    messages: messageReducer
 })
