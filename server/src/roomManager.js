70150801d214d154adb8eb34efbc7e9997b9e355
darkfusion90 <gauravsharmaa341@gmail.com>
2019-12-15 16:41:07 +0530

    Added room object to RoomManager create Room now stores created room inside this.room Added fetchRoom function

--- server/src/roomManager.js
+++ server/src/roomManager.js
@@ -7,11 +7,25 @@ class Room {
         this.type = roomType;
         this.owner = roomOwner;
     }
+
+    isOwnedBy(userId){
+        return this.owner === userId;
+    }
 }
 
 class RoomManager {
+    //Each roomId (key) will map to the corresponding room (value)
+    //Easier to implement search and delete with roomId indexed records rather than iterating over arrays
+    rooms = {}
+
     createRoom(roomName, roomType, roomOwner) {
-        return new Room(generateRoomId(), roomName, roomType, roomOwner);
+        const room = new Room(generateRoomId(), roomName, roomType, roomOwner);
+        this.rooms[room.id] = room;
+        return room;
+    }
+
+    fetchRoom(roomId) {
+        return this.rooms[roomId];
     }
 }
 
