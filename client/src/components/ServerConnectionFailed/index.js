5f629105893b9957f8484811f038556853d05529
darkfusion90 <gauravsharmaa341@gmail.com>
2019-12-15 16:31:00 +0530

    Added Component to show when server connection fails

---
+++ client/src/components/ServerConnectionFailed/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ServerConnectionFailed = () => {
+    console.log('component connection failed')
+    return <div>Oops! Unable to connect to the server. Please visit later</div>;
+}
+
+export default ServerConnectionFailed;
