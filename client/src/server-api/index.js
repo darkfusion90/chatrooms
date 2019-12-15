1ee0fa3934cc0ee7a823b5b679c2bf7008765629
darkfusion90 <gauravsharmaa341@gmail.com>
2019-12-15 16:39:54 +0530

    Refactored socket to initialize to openSocket(url, actions) and connect only when required Added Room join permission and request methods Added Join Room method

--- client/src/server-api/index.js
+++ client/src/server-api/index.js
@@ -1,12 +1,23 @@
 import openSocket from 'socket.io-client';
 
-import { NEW_MESSAGE_RECIEVED, SEND_MESSAGE, ROOM_JOINED, CREATE_ROOM, USER_ID_RECIEVE } from './eventTypes';
+import {
+    NEW_MESSAGE_RECIEVED,
+    SEND_MESSAGE,
+    ROOM_JOINED,
+    CREATE_ROOM,
+    USER_ID_RECIEVE,
+    JOIN_ROOM,
+    ROOM_JOIN_REQUEST,
+    ROOM_JOIN_PERMISSION_RECIEVE
+} from '../constants/server_event_constants'
 
 class ServerApi {
-    socket = null;
+    socket = openSocket("http://localhost:8000", {reconnection: false, autoConnect: false});
 
-    connectToServer = (url) => {
-        this.socket = openSocket(url);
+    connectToServer = (url, onConnectionFailed) => {
+        this.socket.connect();
+        this.socket.on('connect_error', onConnectionFailed);
+        this.socket.on('connect', ()=>console.log("connected"))
     }
 
     sendMessage = (message) => {
@@ -21,12 +32,24 @@ class ServerApi {
         this.socket.on(ROOM_JOINED, callback)
     }
 
+    onRoomJoinRequestRecieved = (callback) => {
+        this.socket.on(ROOM_JOIN_REQUEST, callback);
+    }
+
+    onRoomJoinPermissionRecieved = (callback) => {
+        this.socket.on(ROOM_JOIN_PERMISSION_RECIEVE, callback);
+    }
+
     onUserIdRecieved = (callback) => {
         this.socket.on(USER_ID_RECIEVE, callback);
     }
 
     createRoom = (roomName, roomType, roomOwner, callback) => {
         return this.socket.emit(CREATE_ROOM, roomName, roomType, roomOwner, callback);
+    }
+
+    joinRoom = (roomId, callback) => {
+        return this.socket.emit(JOIN_ROOM, roomId, callback);
     }
 }
 
