d64a73581d6ac251c8a09b154ef3521a33fbb302
darkfusion90 <gauravsharmaa341@gmail.com>
2019-12-15 16:41:32 +0530

    Added implementation of various room joining scenarios

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,49 @@
 const io = require('socket.io')();
 const roomManager = require('./roomManager');
 
-
 /*Consider using one of the following fonts: Ubuntu, Open-Sans, Roboto, Oxygen, Lato*/
+/**
+ * 
+ * @param {Room} room
+ * @param {SocketIO.Socket} client 
+ * @param {function} callback
+ */
+function tryJoiningRoom(room, client, callback) {
+    switch (room.type) {
+        case "public":
+            console.log("public")
+        case "unlisted":
+            console.log("unlisted")
+            client.join(room.id, (data) => {
+                callback({
+                    status: "ok",
+                    room: room
+                })
+            });
+            break;
+        case "private":
+            console.log("private")
+            if (room.isOwnedBy(client.id)) {
+                console.log("same owner")
+                client.join(room.id, (data) => {
+                    callback({
+                        status: "ok",
+                        room: room
+                    })
+                });
+            }
+            else {
+                console.log("diff owner. will request to " + room.owner)
+                io.to(room.owner).emit("ROOM_JOIN_REQUEST", client.id);
+                callback({
+                    status: "permission_pending",
+                    reason: "Private room. Requesting access"
+                })
+            }
+    }
+}
 
+io.origins('http://localhost:3000')
 io.on('connection', (client) => {
     const permanentId = client.id;
 
@@ -16,19 +56,31 @@ io.on('connection', (client) => {
     client.on('CREATE_ROOM', (roomName, roomType, roomOwner, callback) => {
         const room = roomManager.createRoom(roomName, roomType, roomOwner);
         callback({
-            status: 200,
+            status: "ok",
             room: room
         });
         console.log(room);
     })
 
-    client.on('JOIN_ROOM', (roomName, roomType, roomOwner, callback) => {
-        const room = roomManager.createRoom(roomName, roomType, roomOwner);
-        callback({
-            status: 200,
-            room: room
-        });
+    client.on('JOIN_ROOM', (roomId, callback) => {
+        const room = roomManager.fetchRoom(roomId);
+        console.log("wants to join: ")
         console.log(room);
+
+        if (typeof room === "undefined" || room === null) {
+            console.log("REJECTED. Undefined or null room");
+            callback({
+                status: "error",
+                reason: "room not found"
+            });
+        }
+        else {
+            tryJoiningRoom(room, client, callback);
+        }
+    })
+
+    client.on("ROOM_JOIN_PERMISSION_SEND", (whoRequested, permissionStatus) => {
+        io.to(whoRequested).emit("ROOM_JOIN_PERMISSION_RECIEVE", permissionStatus);
     })
 })
 
