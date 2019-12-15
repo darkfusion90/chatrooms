9fc4c33415c8ff38ac42134d28a3b7bda01a3220
darkfusion90 <gauravsharmaa341@gmail.com>
2019-12-15 15:49:00 +0530

    Added onRoomJoinRequest socket.io event listener Added conditional rendering accounting to server connection failure

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -4,24 +4,55 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 
 import LandingPage from '../LandingPage';
+import ServerConnectionFailed from '../ServerConnectionFailed'
 import serverApi from '../../server-api';
 import connectToServer from '../../actions/connectToServer';
 import updateUserId from '../../actions/updateUserId';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from '../Header';
 
 class App extends React.Component {
+    state = {
+        serverConnectionFailed: false
+    }
+
     componentDidMount() {
-        this.props.connectToServer("http://localhost:8000");
+        this.props.connectToServer(this.onServerConnectionFailed);
         serverApi.onUserIdRecieved((userId) => this.props.updateUserId(userId));
+        serverApi.onRoomJoinRequestRecieved(whoSent => {
+            console.log(whoSent + " wants to join your room")
+            this.props.createNotification(
+                "Room join request",
+                whoSent + " wants to join your room",
+                null,
+                "")
+        })
+    }
+
+    onServerConnectionFailed = () => {
+        this.setState({ serverConnectionFailed: true })
+    }
+
+    renderAppBody() {
+        if(this.state.serverConnectionFailed){
+            return <ServerConnectionFailed />;
+        }
+
+        return (
+            <BrowserRouter>
+                <Route exact path="/" component={LandingPage} />
+            </BrowserRouter>
+        );
     }
 
     render() {
+        console.log(this.props)
         return (
             <div className="app">
-                <BrowserRouter>
-                    <Route exact path="/" component={LandingPage} />
-                </BrowserRouter>
+                <Header {...this.props} />
+                {this.renderAppBody()}
             </div>
         );
     }
