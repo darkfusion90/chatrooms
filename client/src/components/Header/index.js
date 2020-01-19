94cc127379e0cffea2f6d2e8725b31631056e04e
darkfusion90 <gauravsharmaa341@gmail.com>
2020-01-19 14:40:19 +0530

    Moved temporary credits comment to bottom

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,46 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavItem from 'react-bootstrap/NavItem';
+import Container from 'react-bootstrap/Container';
+import NotificationContainer from '../NotificationContainer';
+import './Header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+const style = { backgroundColor: "#23282d" }
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect style={style} expand="large" sticky="top">
+            <Container>
+                <Navbar.Brand>
+                    <Link to="/" className="logo"> ChatRooms </Link>
+                </Navbar.Brand>
+
+                <Nav className="justify-content-end flex-row">
+                    <Link to="/rooms" className="header-text">Rooms</Link>
+                    <NotificationContainer />
+                    <NavItem as={"span"} className="header-icon" >
+                        <FontAwesomeIcon icon={faUser} size="lg" />
+                    </NavItem>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
+
 /*
 Logo credits
 
 <div>
-    Icons made by 
+    Icons made by
     <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">
         Smashicons
-    </a> 
-    from 
+    </a>
+    from
     <a href="https://www.flaticon.com/" title="Flaticon">
         www.flaticon.com
     </a>
 </div>
 */
-
-import React from 'react';
-
-import NotificationContainer from './NotificationContainer';
-
-import logo from "../../assets/navbarLogo.svg"
-import './Header.css';
-
-
-class Header extends React.Component {
-    render() {
-        console.log("header " + this.props.notifications)
-        return (
-            <header>
-                <div className="ui orange inverted menu">
-                    <a className="navbar-logo-container" href="/" className="item">
-                        <img src={logo} />
-                        <span className="navbar-logo-text">Chatrooms</span>
-                    </a>
-                    <div className="right menu">
-                        <NotificationContainer notifications={this.props.notifications}/>
-                        <span className="item">
-                            <i className="big black user circle icon" />
-                        </span>
-                    </div>
-                </div>
-            </header>
-        );
-    }
-}
-
-export default Header;
