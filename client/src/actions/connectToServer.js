625e85a8b25722a0c7165dc7bb48ae098161b20a
darkfusion90 <gauravsharmaa341@gmail.com>
2020-01-19 13:52:00 +0530

    Recives server url as parameter Refactored import of constants

--- client/src/actions/connectToServer.js
+++ client/src/actions/connectToServer.js
@@ -1,8 +1,8 @@
 import serverApi from '../server-api';
 
-import { CONNECT_TO_SERVER } from '../constants/action_constants'
+import { CONNECT_TO_SERVER } from '../constants/actionConstants'
 
-export default (url, onConnectionFailed) => async (dispatch) => {
-    await serverApi.connectToServer(url, onConnectionFailed);
+export default (onConnectionFailed) => async (dispatch) => {
+    await serverApi.connectToServer(onConnectionFailed);
     dispatch({ type: CONNECT_TO_SERVER, payload: { connected: true } })
 }
