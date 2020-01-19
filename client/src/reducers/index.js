d08a2665de6386388fe6bd2b3c582350ba107de7
darkfusion90 <gauravsharmaa341@gmail.com>
2020-01-19 14:43:10 +0530

    Included notificationReducer in combineReducer()

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,10 +4,12 @@ import serverReducer from './serverReducer';
 import messageReducer from './messageReducer';
 import roomReducer from './roomReducer';
 import updateUserIdReducer from './updateUserIdReducer';
+import notificationReducer from './notificationReducer';
 
 export default combineReducers({
     user: updateUserIdReducer,
     serverConnection: serverReducer,
     roomMetadata: roomReducer,
-    messages: messageReducer
+    messages: messageReducer,
+    notifications: notificationReducer
 })
