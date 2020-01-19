e351ae82306696281ac53d66ff00e9cfa69cc1b1
darkfusion90 <gauravsharmaa341@gmail.com>
2020-01-19 14:41:46 +0530

    Removed

--- client/src/components/ServerConnectionFailed/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ServerConnectionFailed = () => {
-    console.log('component connection failed')
-    return <div>Oops! Unable to connect to the server. Please visit later</div>;
-}
-
-export default ServerConnectionFailed;
