866cb145156dea38d153c749f7a58e1f52615d5f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-01-23 19:31:44 +0530

    Central store for all event constants

---
+++ server/src/constants/socket_event_constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+    NEW_MESSAGE_RECIEVED: "new_message_recieved",
+    SEND_MESSAGE: "send_message",
+    CREATE_ROOM: "create_room",
+    JOIN_ROOM: "join_room", ROOM_JOINED: "room_joined",
+    USER_ID_RECIEVE: "user_id_recieve",
+    ROOM_JOIN_REQUEST: "room_join_request",
+    ROOM_JOIN_PERMISSION_SEND: "room_join_permission_send",
+    ROOM_JOIN_PERMISSION_RECIEVE: "room_join_permission_recieve"
+}
