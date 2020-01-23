87b60d28a9d104d34c1670e42e33d89998009029
darkfusion90 <gauravsharmaa341@gmail.com>
2020-01-23 21:05:26 +0530

    Seperating client and server to different repos

--- server/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const MONGO_USER_PW = "xPl09x0GtdoqzaS0";
-
-module.exports = {
-    MONGO_URI: `mongodb+srv://darkfusion90:${MONGO_USER_PW}@cluster0-bestn.mongodb.net/test?retryWrites=true&w=majority`,
-    MONGO_CLIENT_OPTS: {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-}
--- server/src/roomIdGenerator.js
+++
@@ -1,9 +0,0 @@
-const randomWordsGenerator = require('random-words');
-
-function createRoomId() {
-    return randomWordsGenerator({ exactly: 4, join: '-' })
-}
-
-module.exports = {
-    generate: createRoomId
-}
