c18948644084604799970d6a1edb0176623c1a61
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-01 19:26:50 +0530

    Re-arranged events with main event followed by subEvents

--- server/constants/socket_event_constants.js
+++ server/constants/socket_event_constants.js
@@ -1,9 +1,11 @@
 module.exports = {
-    NEW_MESSAGE_RECIEVED: "new_message_recieved",
-    SEND_MESSAGE: "send_message",
-    CREATE_ROOM: "create_room",
-    USER_ID_RECIEVE: "user_id_recieve",
+    ROOM_EVENT: "event:room",
     ROOM_JOIN_ATTEMPT: "room_join_attempt",
     ROOM_JOIN_PERMISSION_SEND: "room_join_permission_send",
-    ROOM_JOIN_PERMISSION_RECIEVE: "room_join_permission_recieve"
+    ROOM_JOIN_PERMISSION_RECIEVE: "room_join_permission_recieve",
+    CREATE_ROOM: "create_room",
+
+    NEW_MESSAGE_RECIEVED: "new_message_recieved",
+    SEND_MESSAGE: "send_message",
+    USER_ID_RECIEVE: "user_id_recieve",
 }
