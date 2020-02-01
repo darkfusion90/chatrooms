d0d68d935efd2887c16069c405d8866808706b4e
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-01 19:29:07 +0530

    Refactored to roomEventHandler.js

--- server/socket/eventHandlers/roomEventsHandler.js
+++
@@ -1,9 +0,0 @@
-const events = require('../../constants/socket_event_constants');
-
-function eventsHandler(client) {
-    client.on(events.CREATE_ROOM, (roomName, roomType, roomOwner, callback) => {
-        
-    });
-}
-
-module.exports = eventsHandler;
