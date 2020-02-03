bd39a089e7e9601807d1b88cc6442d035b1117b6
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-03 20:36:33 +0530

    Room owner determination from redux store removed Server is responsible to determine roomOwner from session data

--- client/src/actions/tryCreatingRoom.js
+++ client/src/actions/tryCreatingRoom.js
@@ -3,8 +3,8 @@ import { CREATE_ROOM } from '../constants/actionConstants';
 import serverApi from '../server-api';
 
 const tryCreatingRoom = (roomName, roomType, onSuccess, onFailure) =>
-    async (dispatch, getState) => {
-        await serverApi.createRoom(roomName, roomType, getState().user.id, (data) => {
+    async (dispatch) => {
+        await serverApi.createRoom(roomName, roomType, (data) => {
             if (data.status === "ok") {
                 console.log(data.room);
                 dispatch(createRoom(data.room));
