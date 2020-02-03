114940bf46931fff42097ee4390b82f4bbd399af
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-03 20:40:34 +0530

    Refactored event ROOM_JOIN_ATTEMPT to JOIN_ROOM

--- server/constants/socket_event_constants.js
+++ server/constants/socket_event_constants.js
@@ -1,9 +1,9 @@
 module.exports = {
     ROOM_EVENT: "event:room",
-    ROOM_JOIN_ATTEMPT: "room_join_attempt",
     ROOM_JOIN_PERMISSION_SEND: "room_join_permission_send",
     ROOM_JOIN_PERMISSION_RECIEVE: "room_join_permission_recieve",
     CREATE_ROOM: "create_room",
+    JOIN_ROOM: "join_room",
 
     NEW_MESSAGE_RECIEVED: "new_message_recieved",
     SEND_MESSAGE: "send_message",
