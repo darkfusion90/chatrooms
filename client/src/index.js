4ad80056511789c274132b0baf5eaa5a0331ac62
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-03 20:33:58 +0530

    Unchanged

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-
 import reducers from './reducers'
 import AppContainer from './containers/AppContainer';
 
@@ -11,8 +10,6 @@ const composeEnhanchers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducers, composeEnhanchers(
     applyMiddleware(thunk)
 ));
-
-
 ReactDOM.render(
     <Provider store={store}>
         <AppContainer />
