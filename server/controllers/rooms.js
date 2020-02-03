c1d6e47941c0e8e59ce051a9ba73862959160b14
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-03 20:41:01 +0530

    Removed crypto id-generation test code

--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -1,9 +1,6 @@
 const Room = require('../models/Room');
-const crypto = require('crypto')
 
 function createRoom(id, name, type, owner, callback) {
-    console.log("MY CRYPTO: ", crypto.randomBytes(16).toString('base64'))
-    console.log("ROOM ID: " + id)
     const room = new Room({ id, name, type, owner })
     room.save((err) => {
         if (err) {
