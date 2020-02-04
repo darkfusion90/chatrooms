a3d1fb3313423beb6ea51f0553e7db3eaeea894e
LibraryPC7 <gauravsharmaa341@gmail.com>
2020-02-04 09:26:46 +0530

    Moved MongoDb credentials outside config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,8 @@
-const MONGO_USER_PW = "xPl09x0GtdoqzaS0";
+const MONGO_DB_USERNAME = process.env.MONGO_DB_USERNAME;
+const MONGO_DB_USER_PW = process.env.MONGO_DB_USER_PW;
 
 module.exports = {
-    MONGO_URI: `mongodb+srv://darkfusion90:${MONGO_USER_PW}@cluster0-bestn.mongodb.net/test?retryWrites=true&w=majority`,
+    MONGO_URI: `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_USER_PW}@cluster0-bestn.mongodb.net/test?retryWrites=true&w=majority`,
     MONGO_CLIENT_OPTS: {
         useNewUrlParser: true,
         useUnifiedTopology: true
