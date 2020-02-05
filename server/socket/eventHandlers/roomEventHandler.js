d3eb69d9202733314771712b334afe26c4da14ed
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-05 06:00:36 +0530

    Room ID generation moved to room controller

--- server/socket/eventHandlers/roomEventHandler.js
+++ server/socket/eventHandlers/roomEventHandler.js
@@ -1,13 +1,11 @@
 const events = require('../../constants/socket_event_constants')
 const roomsController = require('../../controllers/rooms')
-const uniqueIdGenerator = require('../../utils/uniqueIdGenerator')
 
 function handleCreateRoom(client, args) {
     const [roomName, roomType, clientCallback] = args
     const roomOwner = client.request.session.userId
     console.log(roomName, roomType, roomOwner, clientCallback)
-    const roomId = uniqueIdGenerator.generateIdUsingRandomWords()
-    roomsController.createRoom(roomId, roomName, roomType, roomOwner, clientCallback)
+    roomsController.createRoom(roomName, roomType, roomOwner, clientCallback)
 }
 
 function eventHandler(io, client, subEvent, ...args) {
