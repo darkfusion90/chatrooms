abf8d180d891935965337c358ff279e3ba9bd3fb
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-05 05:58:09 +0530

    Added URI encoding to mongodb URI

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ const MONGO_DB_USERNAME = process.env.MONGO_DB_USERNAME;
 const MONGO_DB_USER_PW = process.env.MONGO_DB_USER_PW;
 
 module.exports = {
-    MONGO_URI: `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_USER_PW}@cluster0-bestn.mongodb.net/test?retryWrites=true&w=majority`,
+    MONGO_URI: encodeURI(`mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_USER_PW}@cluster0-bestn.mongodb.net/test?retryWrites=true&w=majority`),
     MONGO_CLIENT_OPTS: {
         useNewUrlParser: true,
         useUnifiedTopology: true
