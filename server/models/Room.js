0d10ae07364bfe37379db517a682cb698915e852
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-05 05:59:52 +0530

    Refactored id->roomId

--- server/models/Room.js
+++ server/models/Room.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RoomSchema = Schema({
-    id: String,
+    roomId: String,
     name: String,
     type: {
         type: String,
