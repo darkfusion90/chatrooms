ab853f2831b6a68af59d5a8a460c55cccd6cd602
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-07 18:46:41 +0530

    Moved routing to seperate module Removed cache-disabling headers Added /login GET and POST routes Added passport authentication (local) to /login POST

--- server/app.js
+++ server/app.js
@@ -8,53 +8,27 @@ const session = require('express-session')
 const sessionOptions = require('./config/sessionConfig')
 const sessionMiddleware = session(sessionOptions)
 require('./socket')(httpServer, sessionMiddleware)
+const routes = require('./routes')
+const passport = require('./config/passportConfig')
 
-const usersController = require('./controllers/users')
-
-const isProductionMode = process.env.NODE_ENV === 'production'
-if (isProductionMode) {
+if (process.env.NODE_ENV === 'production') {
     console.log("Production Mode")
     app.use(express.static(path.join(__dirname, '../client/build')))
 }
-else {
-    //Prevents caching
-    app.disable('etag');
-}
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 app.use(sessionMiddleware)
-app.use((_, res, next) => {
-    res.set({
-        'Cache-Control': 'no-store, no-cache, must-revalidate',
-        Pragma: 'no-cache'
-    })
-    next();
-})
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/*', (req, res) => {
-    console.log('GET /')
-    console.log('SessionId: ' + req.sessionID)
-    if (!req.session.userId) {
-        usersController.createUnregisteredUser(req.session.cookie.expires, (err, user) => {
-            if (err) {
-                console.log("Error creating user:\n", err)
-            }
-            else {
-                console.log("Callback: ", user)
-                req.session.userId = user.userId
-                req.session.save()
-            }
-        })
-    }
-    else {
-        console.log('Old User. UserId: ' + req.session.userId)
-    }
 
-    if (isProductionMode) {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'))
-    } else {
-        res.send('Hello from Development Server!')
-    }
-})
+app.get('/login', routes.loginGet)
+app.get('/', routes.index)
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+}), routes.loginPost)
 
 var serverPort = process.env.PORT || 8000
 httpServer.listen(serverPort, '0.0.0.0', () => {
