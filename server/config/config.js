9fbf2b30b01951acf48f9a85ce83c69f0531798b
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-07 18:49:01 +0530

    Switched to local mongodb server because of connection problems with atlas Added serverSelectionTimeoutMS, socketTimeoutMS and keepAlive opts to mongo client

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
-const MONGO_DB_USERNAME = process.env.MONGO_DB_USERNAME;
-const MONGO_DB_USER_PW = process.env.MONGO_DB_USER_PW;
-
 module.exports = {
-    MONGO_URI: encodeURI(`mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_USER_PW}@cluster0-bestn.mongodb.net/test?retryWrites=true&w=majority`),
+    MONGO_URI: 'mongodb://localhost:54000/test',
     MONGO_CLIENT_OPTS: {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 50000,
+        socketTimeoutMS: 50000,
+        keepAlive: true
     }
 }
