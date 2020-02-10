22b2b60a598214c7b7e46ff16c73df55cb5a0454
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-10 06:09:03 +0530

    Removing user cookie creation from route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,10 @@
 const path = require('path')
-const usersController = require('../controllers/users')
+const apiRouter = require('./api')
+
 
 const index = (req, res) => {
     if (!req.session.userId) {
-        usersController.createUnregisteredUser(req.session.cookie.expires, (err, user) => {
-            if (err) {
-                console.log("Error creating user:\n", err, '\n', user)
-            }
-            else {
-                console.log("Callback: ", user)
-                req.session.userId = user.userId
-                req.session.save()
-            }
-        })
+        
     }
     else {
         console.log('Old User. UserId: ' + req.session.userId)
@@ -25,29 +17,5 @@ const index = (req, res) => {
     }
 }
 
-const loginGet = (_, res) => {
-    console.log('Login Get')
-    res.sendFile(path.join(__dirname, '../', 'html', 'loginForm.html'))
-}
 
-const loginPost = (req, res) => {
-    console.log('Login Post')
-    console.log("Passport body: ", req.body)
-    res.redirect('/')
-}
-
-const registerGet = (_, res) => {
-    console.log('Register Get')
-    res.sendFile(path.join(__dirname, '../', 'html', 'registrationForm.html'))
-}
-
-const registerPost = (req, res) => {    
-    if(req.body.username != "cow"){
-        res.status(400).json({'cow': 'i don\'t like that'})
-    }
-    console.log('Register Post')
-    console.log("Passport body: ", req.body)
-    res.redirect('/')
-}
-
-module.exports = { index, loginGet, loginPost, registerGet, registerPost }
+module.exports = { index, apiRouter }
