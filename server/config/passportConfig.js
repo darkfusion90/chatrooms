ff7730d7e11ce144c995ceb260e9a8ca2d611e9c
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-10 22:00:32 +0530

    Improved logging by adding key for module

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -2,38 +2,39 @@ const config = require('./config')
 const passport = require('passport')
 const PassportLocalStrategy = require('passport-local').Strategy
 const User = require('../models/User')
+const logger = require('../utils/logger')('[PassportConfig] ')
 
 passport.serializeUser((user, done) => {
-    console.log("SERIALIZE ", user)
+    logger.debug("<Serialize> ", user)
     done(null, user.userId)
 })
 
 passport.deserializeUser((userId, done) => {
     User.findOne({ userId: userId }, (err, user) => {
-        console.log('Deserialize: ', user)
+        logger.debug("<Deserialize> ", user)
         done(err, user)
     })
 })
 
-passport.use(new PassportLocalStrategy({}, (userId, password, done) => {
-    User.findOne({ userId: userId }, (err, user) => {
+passport.use(new PassportLocalStrategy({}, (username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
         if (err) {
-            console.log('Strategy err: ', err)
+            logger.debug('<Strategy> err: ', err)
             return done(err)
         }
         if (!user) {
-            console.log('Strategy no userId: ', user)
+            logger.debug('<Strategy> no userId: ', user)
             return done(null, false, { message: 'Invalid userId' })
         }
         if (!user.verifyPassword(password)) {
-            console.log('Strategy wrong password: ', user, password)
+            logger.debug('<Strategy> wrong password: ', user, password)
             return done(null, false, { message: 'Invalid password' })
         }
-        console.log('Strategy success: ', user)
+        logger.debug('<Strategy> success: ', user)
         return done(null, user)
     })
 }))
 
 const passportAuthenticate = passport.authenticate('local', config.PASSPORT_AUTH_OPTS)
 
-module.exports = {passport, passportAuthenticate}
+module.exports = { passport, passportAuthenticate }
