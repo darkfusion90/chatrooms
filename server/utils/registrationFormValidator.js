526e90c90f38bce9279abb0d1b5c76672be84505
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-10 21:28:01 +0530

    Validates empty/password mismatch in registrationForm

--- server/utils/registrationFormValidator.js
+++ server/utils/registrationFormValidator.js
@@ -1,16 +1,41 @@
-const validator = require('validator')
+const validator = require('validator').default
 const isEmpty = require('is-empty')
-const logger = require('../utils/logger')('[RegistrationFormValidator] ')
+
 
 const convertEmptyFieldsToEmptyStrings = (data) => {
-    Object.keys(data).forEach(key => {
-        data[key] = isEmpty(data[key]) ? "" : data[key];
-    })
+    data.username = isEmpty(data.username) ? '' : data.username
+    data.password = isEmpty(data.password) ? '' : data.password
+    data.confirmPassword = isEmpty(data.confirmPassword) ? '' : data.confirmPassword
+}
+
+const ensureDataNotEmpty = (data) => {
+    const errors = {};
+    if (validator.isEmpty(data.username)) {
+        errors.usernameEmpty = 'Username field cannot be empty'
+    }
+
+    if (validator.isEmpty(data.password)) {
+        errors.passwordEmpty = 'Password field cannot be empty'
+    }
+
+    if (validator.isEmpty(data.confirmPassword)) {
+        errors.confirmPasswordEmpty = 'Confirm Password field cannot be empty'
+    }
+
+    return errors
 }
 
 module.exports = (data) => {
-    const errors = {}
-    logger.debug("Before: ", data)
     convertEmptyFieldsToEmptyStrings(data)
-    logger.debug("After: ", data)
+    const errors = { ...ensureDataNotEmpty(data) }
+    const { password, confirmPassword } = data
+
+    if (!validator.equals(password, confirmPassword)) {
+        errors.passwordsMismatch = 'Password and Confirm Password should match'
+    }
+
+    return {
+        errors: errors,
+        hasErrors: !isEmpty(errors)
+    }
 }
