4cc04078f8a8c7f1991e4e1946e5babc90251764
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-10 15:40:07 +0530

    set 'useCreateIndex' to true Removed on error handler

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,9 @@
 const config = require('./config');
 const mongoose = require('mongoose');
 
+//Solution to -> "DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead"
+mongoose.set('useCreateIndex', true)
 mongoose.connect(config.MONGO_URI, config.MONGO_CLIENT_OPTS);
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
-mongoose.connection.on('open', function cb(){
+mongoose.connection.on('open', function cb() {
     console.log("connected to db")
 })
