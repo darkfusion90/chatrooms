dace57c26c960d0e2643792077c1791a514bdf34
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-10 15:42:49 +0530

    Removed stale if-else statement

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,21 +1,12 @@
 const path = require('path')
 const apiRouter = require('./api')
 
-
-const index = (req, res) => {
-    if (!req.session.userId) {
-        
-    }
-    else {
-        console.log('Old User. UserId: ' + req.session.userId)
-    }
-
+const index = (_, res) => {
     if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, '../client/build/index.html'))
     } else {
         res.send('Hello from Development Server!')
     }
 }
-
 
 module.exports = { index, apiRouter }
