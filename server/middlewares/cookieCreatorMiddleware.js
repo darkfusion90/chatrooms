041c260c7b109210489e0d4248ddff8c07316b52
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-10 15:42:27 +0530

    Creates user cookie, if not created Added custom debug method

--- server/middlewares/cookieCreatorMiddleware.js
+++ server/middlewares/cookieCreatorMiddleware.js
@@ -1,11 +1,26 @@
 const usersController = require('../controllers/users')
 
+const debug = (...args) => {
+    console.debug('[CookieCreatorMiddleware] ', args)
+}
+
 const middleware = (req, res, next) => {
-    if(!req.session.userId){
-        console.log("No userid")
+    if (!req.session.userId) {
+        debug('New User')
+        usersController.createUnregisteredUser(req.session.cookie.expires, (err, user) => {
+            if (err || !user) {
+                debug('Error creating user: ', err)
+                const error = new Error('Internal Server Error')
+                next(error)
+            }
+
+            req.session.userId = user.userId
+            req.session.save()
+            debug('Created user: ', user.userId)
+        })
     }
-    else{
-        console.log("Middleware UserId: ", req.session.userId)
+    else {
+        debug('UserId: ', req.session.userId)
     }
     next()
 }
