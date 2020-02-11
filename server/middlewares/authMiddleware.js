4648f37b00bb1fd83326dfea0aefe100a8089e4b
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-11 20:31:54 +0530

    Added custom passport callback to respond to client accordingly

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,5 +1,21 @@
-const { passportAuthenticate } = require('../config/passportConfig')
+const passport = require('../config/passportConfig')
 const logger = require('../utils/logger')('[AuthMiddleware] ')
+
+const login = (user, req, res) => {
+    req.logIn(user, function (err) {
+        if (err) {
+            res.status(500).send('Oops! Got hurt real bad. Will be right back!')
+        }
+        else {
+            logger.debug('UserId before update: ', req.session.userId)
+            req.session.userId = user.userId
+            req.session.save()
+            res.redirect('/')
+            logger.debug('UserId updated to: ', req.session.userId)
+        }
+    });
+}
+
 
 
 const matchesLoginPath = (what) => {
@@ -9,7 +25,17 @@ const matchesLoginPath = (what) => {
 const middleware = (req, res, next) => {
     if (matchesLoginPath(req.path) && req.method === 'POST') {
         logger.debug('Will authenticate')
-        passportAuthenticate(req, res, next)
+        passport.authenticate('local', function (err, user, info) {
+            if (err) {
+                res.status(500).send('Oops! Got hurt real bad. Will be right back!')
+            }
+            else if (!user) {
+                res.status(401).json(info)
+            }
+            else {
+                login(user, req, res)
+            }
+        })(req, res, next)
     }
     else {
         logger.debug('Not authenticating')
