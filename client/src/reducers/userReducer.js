9cb862bac74cb03437c77667b1e08fa78d042e46
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-13 18:13:15 +0530

    Refactored Log(in)out actions. Modified state to include username

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,14 +1,15 @@
-import { LOGIN_SUCCESS, LOGIN_FAIL, UPDATE_USER_INFO } from '../constants/actionConstants';
+import { LOGIN, UPDATE_USER_INFO, LOGOUT } from '../constants/actionConstants';
 
 const INITIAL_STATE = {
     loggedIn: false,
+    username: null
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case LOGIN_SUCCESS:
-            return { loggedIn: true }
-        case LOGIN_FAIL:
+        case LOGIN:
+            return { loggedIn: true, username: action.payload.username }
+        case LOGOUT:
             return INITIAL_STATE
         case UPDATE_USER_INFO:
             return { ...action.payload }
