c70fc3771b55c1592888299a30196a1b43c4f621
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-13 14:06:44 +0530

    Conditionally dispatches LOGIN_SUCCESS or LOGIN_FAIL

---
+++ client/src/actions/loginUser.js
@@ -0,0 +1,53 @@
+import serverApi from '../server-api';
+
+import { LOGIN_FAIL, LOGIN_SUCCESS } from '../constants/actionConstants'
+
+const onRequestFulfilled = (dispatch, response) => {
+    console.log("onRequestFulfilled: ", response.data)
+    if (response.data.success === "true") {
+        dispatch(loginSuccessAction(response.data.userId))
+    }
+    else {
+        dispatch(loginFailAction(response.data.reason))
+    }
+}
+
+const onRequestRejected = (dispatch, { response }) => {
+    console.log("onRequestRejected: ", response.data)
+    dispatch(loginFailAction(response.data.message))
+}
+
+const loginFailAction = (reason) => {
+    return {
+        type: LOGIN_FAIL,
+        payload: {
+            status: "failed",
+            reason: reason
+        }
+    }
+}
+
+const loginSuccessAction = (userId) => {
+    return {
+        type: LOGIN_SUCCESS,
+        payload: {
+            status: "success",
+            userId: userId
+        }
+    }
+}
+
+export default (username, password, onSuccessCallback, onFailureCallback) => async (dispatch) => {
+    serverApi.loginUser(
+        username,
+        password,
+        (response) => {
+            onRequestFulfilled(dispatch, response)
+            onSuccessCallback()
+        },
+        (reason) => {
+            onRequestRejected(dispatch, reason)
+            onFailureCallback(reason)
+        }
+    );
+}
