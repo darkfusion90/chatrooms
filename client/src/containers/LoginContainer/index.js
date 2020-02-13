859b5c2df489760a5da676a101674fcbc69f95ca
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-13 13:58:28 +0530

    Added callbacks on login Added redirection to '/' for logged in users

--- client/src/containers/LoginContainer/index.js
+++ client/src/containers/LoginContainer/index.js
@@ -1,15 +1,56 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
-import LoginForm from '../../components/forms/LoginForm'
+import LoginForm from '../../components/forms/LoginForm';
+import loginUser from '../../actions/loginUser';
+
+import './LoginContainer.scss';
 
 class LoginContainer extends React.Component {
+    state = { loginAttemptStatus: "unknown" }
+
+    onLoginSuccess = () => {
+        this.setState({ loginAttemptStatus: "success" })
+    }
+
+    onLoginFailure = () => {
+        this.setState({ loginAttemptStatus: "failure" })
+    }
+
     onFormSubmit = (formValues) => {
         console.log("Login Container values: ", formValues)
+        this.props.loginUser(
+            formValues.username,
+            formValues.password,
+            this.onLoginSuccess,
+            this.onLoginFailure
+        );
     }
 
     render() {
-        return <LoginForm onFormSubmit={this.onFormSubmit} />
+        console.log("STATE: ", this.state)
+
+        if (this.props.isUserLoggedIn) {
+            return <Redirect to="/" />
+        }
+        else {
+            return (
+                <div className="login-page-container">
+                    <Container>
+                        <LoginForm onFormSubmit={this.onFormSubmit} />
+                    </Container>
+                </div>
+            );
+        }
     }
 }
 
-export default LoginContainer;
+const mapStateToProps = (state) => {
+    return {
+        isUserLoggedIn: state.user.loggedIn,
+    }
+}
+
+export default connect(mapStateToProps, { loginUser })(LoginContainer);
