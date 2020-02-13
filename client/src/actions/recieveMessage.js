b9558dca0b1a84e9af5e81f94f93363a272c988b
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-13 14:07:58 +0530

    Removed unnecessary action creators

--- client/src/actions/recieveMessage.js
+++
@@ -1,8 +0,0 @@
-import { MESSAGE_RECIEVED } from '../constants/actionConstants';
-
-export default (messageMeta) => {
-    return {
-        type: MESSAGE_RECIEVED,
-        payload: { messageMeta: messageMeta }
-    }
-}
