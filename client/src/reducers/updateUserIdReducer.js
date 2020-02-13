acf06c8b883866ddbc152972304735e5faf060df
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-13 13:56:47 +0530

    Removed reducers that weren't required

--- client/src/reducers/updateUserIdReducer.js
+++
@@ -1,8 +0,0 @@
-import { UPDATE_USER_ID } from '../constants/actionConstants';
-
-export default (state = {}, action) => {
-    if (action.type === UPDATE_USER_ID) {
-        return { ...state, id: action.payload };
-    }
-    return state;
-}
