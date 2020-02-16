48993fed1dd085319328e82524bbe33dd30a7e20
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-16 22:45:42 +0530

    Redirect if registered (to '/login' if not logged in after registration, else to '/') Registration Failed callback added

--- client/src/containers/RegisterContainer/index.js
+++ client/src/containers/RegisterContainer/index.js
@@ -4,21 +4,43 @@ import Container from 'react-bootstrap/Container';
 
 import RegistrationForm from '../../components/forms/RegistrationForm/RegistrationForm';
 import registerUser from '../../actions/registerUser';
+import { Redirect } from 'react-router-dom';
 
 class RegisterContainer extends React.Component {
+
+    onRegistrationFailed = ({ response }) => {
+        console.log("Registration failed: ", response)
+    }
 
     onFormSubmit = (formValues) => {
         console.log("Registration Form Values: ", formValues)
         this.props.registerUser(
             formValues.username,
             formValues.password,
-            formValues.confirmPassword
+            formValues.confirmPassword,
+            this.onRegistrationFailed
         )
     }
 
+    renderAppropriateRedirect = (isUserLoggedIn) => {
+        if (isUserLoggedIn) {
+            return <Redirect to="/" />
+        }
+
+        return <Redirect to={{
+            pathname: "/login",
+            state: { isRedirectFromSuccessfulRegister: true }
+        }} />
+    }
+
     render() {
+        console.log(this.props)
+        if (this.props.isUserRegistered) {
+            return this.renderAppropriateRedirect(this.props.isUserLoggedIn)
+        }
+
         return (
-            <div className="login-page-container">
+            <div className="centered-content">
                 <Container>
                     <RegistrationForm onFormSubmit={this.onFormSubmit} />
                 </Container>
@@ -27,4 +49,11 @@ class RegisterContainer extends React.Component {
     }
 }
 
-export default connect(null, { registerUser })(RegisterContainer);
+const mapStateToProps = (state) => {
+    return {
+        isUserRegistered: state.user.isRegistered,
+        isUserLoggedIn: state.user.loggedIn
+    }
+}
+
+export default connect(mapStateToProps, { registerUser })(RegisterContainer);
