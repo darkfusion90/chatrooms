349a128cc6e55a2d5d9d9aee4951b8e82259ba1d
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-16 22:41:52 +0530

    Callback only for request rejected Action Payload includes username

--- client/src/actions/registerUser.js
+++ client/src/actions/registerUser.js
@@ -2,17 +2,13 @@ import { REGISTER } from '../constants/actionConstants';
 import serverApi from '../server-api';
 
 const onRequestFulfilled = (response, dispatch) => {
-    console.log("Request fulfilled: ", response)
-    dispatch({ type: REGISTER })
+    console.log("Register Request fulfilled: ", response)
+    dispatch({ type: REGISTER, payload: { username: response.data.username } })
 }
 
-const onRequestRejected = ({ response }, dispatch) => {
-    console.log("Request rejected: ", response)
-}
-
-export default (username, password, confirmPassword) => async (dispatch) => {
+export default (username, password, confirmPassword, onRequestRejected) => async (dispatch) => {
     serverApi.registerUser(username, password, confirmPassword,
         (response) => onRequestFulfilled(response, dispatch),
-        (reason) => onRequestRejected(reason, dispatch)
+        onRequestRejected
     )
 }
