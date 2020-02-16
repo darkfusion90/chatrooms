629effd35076ceedd1b9848516c9fc3bb08e9300
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-16 22:50:32 +0530

    Added new property to user state: isRegistered Added REGISTER action reducer

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,18 +1,21 @@
-import { LOGIN, UPDATE_USER_INFO, LOGOUT } from '../constants/actionConstants';
+import { LOGIN, UPDATE_USER_INFO, LOGOUT, REGISTER } from '../constants/actionConstants';
 
 const INITIAL_STATE = {
     loggedIn: false,
+    isRegistered: false,
     username: null
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case LOGIN:
-            return { loggedIn: true, username: action.payload.username }
+            return { loggedIn: true, isRegistered: true, username: action.payload.username }
         case LOGOUT:
             return INITIAL_STATE
         case UPDATE_USER_INFO:
             return { ...action.payload }
+        case REGISTER:
+            return { loggedIn: false, isRegistered: true, username: action.payload.username }
         default:
             return state;
     }
