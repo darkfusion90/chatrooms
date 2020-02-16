d3e79a8264bd963bcf60243efa7c7d4c1ce5c377
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-16 13:11:29 +0530

    Work begin to centralize all response at one place

---
+++ server/config/apiResponses.js
@@ -0,0 +1,28 @@
+const reasons = {
+    ALREADY_LOGGED_IN: "Already logged in"
+}
+
+const responseCreator = (success, payload, error) => {
+    return {
+        success: success,
+        payload: payload,
+        error: error
+    }
+}
+
+const internalServerError = () => {
+    return responseCreator(false, {}, { code: 500 })
+}
+
+const loginSuccess = (user) => {
+    return responseCreator(true, { username: user.username }, null)
+}
+
+const loginFail = (reason) => {
+    return responseCreator(false, {}, { reason })
+}
+
+const registerSuccess = (user) => {
+    return responseCreator(true, { username: user.username }, null)
+}
+
