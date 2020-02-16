1e78764551064e7eac742f8b9ffc00d43557d297
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-16 13:10:44 +0530

    Attached form to container

--- client/src/containers/RegisterContainer/index.js
+++ client/src/containers/RegisterContainer/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Container from 'react-bootstrap/Container';
+
+import RegistrationForm from '../../components/forms/RegistrationForm/RegistrationForm';
+import registerUser from '../../actions/registerUser';
 
 class RegisterContainer extends React.Component {
-    render(){
-        return <div>Register Container</div>;
+
+    onFormSubmit = (formValues) => {
+        console.log("Registration Form Values: ", formValues)
+        this.props.registerUser(
+            formValues.username,
+            formValues.password,
+            formValues.confirmPassword
+        )
+    }
+
+    render() {
+        return (
+            <div className="login-page-container">
+                <Container>
+                    <RegistrationForm onFormSubmit={this.onFormSubmit} />
+                </Container>
+            </div>
+        );
     }
 }
 
-export default RegisterContainer;
+export default connect(null, { registerUser })(RegisterContainer);
