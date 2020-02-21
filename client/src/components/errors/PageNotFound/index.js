9c6bb38a6350bb025dc7ea84923afaec6a5b6e16
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-21 21:57:13 +0530

    Refactored css class(es) to 'error' Imports './style.scss'

--- client/src/components/errors/PageNotFound/index.js
+++ client/src/components/errors/PageNotFound/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
-import './PageNotFound.scss';
+import '../style.scss'
 
 const PageNotFound = () => {
     return (
-        <div className="error-404">
+        <div className="error">
             Error 404: Page Not Found.
             <Link to="/">
                 <Button>Home</Button>
--- client/src/components/errors/ServerConnectionFailed/index.js
+++ client/src/components/errors/ServerConnectionFailed/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import './ServerConnectionFailed.scss'
+import '../style.scss'
 
 const ServerConnectionFailed = () => {
     console.log('component connection failed')
     return (
-        <div className="connection-failed">
+        <div className="error">
             Oops! Unable to connect to the server. Please visit later
         </div>
     );
