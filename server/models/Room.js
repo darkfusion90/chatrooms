a1b11b7520e8af46e1bd5647f97895cf9f00a0b3
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-21 15:01:07 +0530

    Added auth validator before y findOneAndUpdate Refined definitions of each field

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,15 +1,40 @@
 const mongoose = require('mongoose');
+const UnauthorizedError = require('../errors/Unauthorized')
 
 const Schema = mongoose.Schema;
 
 const RoomSchema = Schema({
-    roomId: String,
-    name: String,
+    roomId: {
+        type: String,
+        required: true,
+        index: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+        index: true
+    },
     type: {
         type: String,
-        enum: ['private', 'public', 'unlisted']
+        enum: ['private', 'public', 'unlisted'],
+        required: true
     },
-    owner: String
+    owner: {
+        type: String,
+        required: true,
+        index: true
+    }
+})
+
+
+RoomSchema.pre('findOneAndUpdate', async function (next) {
+    const { roomId, owner } = this.getQuery()
+    const docToUpdate = await this.model.findOne({ roomId: roomId })
+    if (docToUpdate.owner != owner) {
+        next(new UnauthorizedError())
+    }
+    next()
 })
 
 const Room = mongoose.model("Room", RoomSchema);
