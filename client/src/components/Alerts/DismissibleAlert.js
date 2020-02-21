fa0717168feea3648701420719039c3a098d6bc3
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-21 14:52:48 +0530

    Stateless Component To represent a dismissible alert

--- client/src/components/Alerts/DismissibleAlert.js
+++ client/src/components/Alerts/DismissibleAlert.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Alert from 'react-bootstrap/Alert'
 
 export default (props) => {
+    console.log(props)
+    const shouldShowDismissIcon = !props.dismissAction;
     return (
-        <Alert show={props.show} variant={props.variant} onClose={props.onDismiss} dismissible>
+        <Alert show={props.show} variant={props.variant} onClose={props.onDismiss} dismissible={shouldShowDismissIcon}>
             <Alert.Heading>{props.heading}</Alert.Heading>
             <p>{props.body}</p>
+            <div className='d-flex justify-content-end'>
+                {props.dismissAction()}
+            </div>
         </Alert>
     )
 }
