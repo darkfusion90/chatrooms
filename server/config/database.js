b199a90165f4e18cd041d892f122a05e586798a6
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-21 14:56:00 +0530

    Set useFindAndModify to false for findOneAndUpdate

--- server/config/database.js
+++ server/config/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 //Solution to -> "DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead"
 mongoose.set('useCreateIndex', true)
+mongoose.set('useFindAndModify', false)
 mongoose.connect(config.MONGO_URI, config.MONGO_CLIENT_OPTS);
 mongoose.connection.on('open', function cb() {
     console.log("connected to db")
