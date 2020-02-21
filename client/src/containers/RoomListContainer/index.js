0051ecefb13e3acd770a8b1d0638172cde52da72
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-21 21:59:56 +0530

    Fetches all public rooms

--- client/src/containers/RoomListContainer/index.js
+++ client/src/containers/RoomListContainer/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
+import RoomList from '../../components/RoomList';
+import { getAllRooms } from '../../server-communication/httpServer'
+
 class RoomListContainer extends React.Component {
+    state = { rooms: [] }
+
+    componentDidMount() {
+        getAllRooms(this.onRoomFetchSuccess, this.onRoomFetchFail)
+    }
+
+    onRoomFetchFail = (reason) => {
+        console.log('RoomList fetch fail: ', reason.response)
+    }
+
+    onRoomFetchSuccess = (response) => {
+        console.log('RoomList fetch success: ', response)
+        this.setState({ rooms: response.data })
+    }
+
     render() {
-        return (
-            <div className="room-list-container">
-                RoomListContainer
-            </div>
-        );
+        return <RoomList rooms={this.state.rooms} />;
     }
 }
 
