1adcc2af044164d2af5dec574e1ca13c61574880
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-21 18:14:17 +0530

    Server communicator for http requests

---
+++ client/src/server-communication/httpServer.js
@@ -0,0 +1,39 @@
+import Axios from 'axios';
+
+const axios = Axios.create({ withCredentials: true })
+
+export const registerUser = (username, password, confirmPassword, onRequestFulfilled, onRequestRejected) => {
+    axios.post('/api/register', { username, password, confirmPassword }).then(onRequestFulfilled, onRequestRejected)
+}
+
+export const loginUser = (username, password, onRequestFulfilled, onRequestRejected) => {
+    axios.post('/api/login', { username, password }).then(onRequestFulfilled, onRequestRejected)
+}
+
+export const logoutUser = (onRequestFulfilled, onRequestRejected) => {
+    axios.post('/api/logout').then(onRequestFulfilled, onRequestRejected);
+}
+
+export const fetchUserInfo = (onRequestFulfilled, onRequestRejected) => {
+    axios.get('/api/user_info').then(onRequestFulfilled, onRequestRejected);
+}
+
+export const getRoom = (roomId, onRequestFulfilled, onRequestRejected) => {
+    axios.delete(`/api/rooms/${roomId}`).then(onRequestFulfilled, onRequestRejected)
+}
+
+export const getAllRooms = (onRequestFulfilled, onRequestRejected) => {
+    axios.delete('/api/rooms').then(onRequestFulfilled, onRequestRejected)
+}
+
+export const createRoom = (roomName, roomType, onRequestFulfilled, onRequestRejected) => {
+    axios.post('/api/rooms', { roomName, roomType }).then(onRequestFulfilled, onRequestRejected)
+}
+
+export const updateRoom = (roomId, update, onRequestFulfilled, onRequestRejected) => {
+    axios.patch(`/api/rooms/${roomId}`, update).then(onRequestFulfilled, onRequestRejected)
+}
+
+export const deleteRoom = (roomId, onRequestFulfilled, onRequestRejected) => {
+    axios.delete(`/api/rooms/${roomId}`).then(onRequestFulfilled, onRequestRejected)
+}
