94a29891f28bc8b94c39bb52e2bd621eb73be92f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-22 12:25:46 +0530

    Fetch users (owners of rooms) after successful rooms fetch

--- client/src/containers/RoomListContainer/index.js
+++ client/src/containers/RoomListContainer/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
 
 import RoomList from '../../components/RoomList';
-import { getAllRooms } from '../../server-communication/httpServer'
+import { getAllRooms, getUser } from '../../server-communication/httpServer'
 
 class RoomListContainer extends React.Component {
-    state = { rooms: [] }
+    state = { rooms: [], users: {} }
 
     componentDidMount() {
         getAllRooms(this.onRoomFetchSuccess, this.onRoomFetchFail)
     }
 
-    onRoomFetchFail = (reason) => {
-        console.log('RoomList fetch fail: ', reason.response)
+    onRoomFetchFail = ({ response }) => {
+        console.log('RoomList fetch fail: ', response)
     }
 
     onRoomFetchSuccess = (response) => {
         console.log('RoomList fetch success: ', response)
-        this.setState({ rooms: response.data })
+        const rooms = response.data
+        rooms.forEach(room => getUser(room.owner, this.onUserFetchSuccess, this.onUserFetchFail))
+        this.setState({ rooms: rooms })
+    }
+
+    onUserFetchFail = ({ response }) => {
+        console.log('User fetch fail: ', response)
+    }
+
+    onUserFetchSuccess = (response) => {
+        console.log('User fetch success', response)
+        const user = response.data
+        const usersMap = { [user.userId]: user }
+        console.log("user map: ", usersMap)
     }
 
     render() {
