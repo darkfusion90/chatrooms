b3e8873d0116533a0e5c66a72982b0915719e956
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-22 15:08:24 +0530

    Each class of tester has its  own base url

--- server/tests/room_requests.py
+++ server/tests/room_requests.py
@@ -6,32 +6,36 @@ BASE_URL = 'http://localhost:8000'
 class RoomRequests:
     def __init__(self, session=requests.session()):
         self.session = session
+        self.base_url = '{}/api/rooms'.format(BASE_URL)
 
     def get_room(self, id):
-        return self.session.get('{base}/api/rooms/{id}'.format(base=BASE_URL, id=id))
+        return self.session.get('{base}/{id}'.format(base=self.base_url, id=id))
 
     def get_all_rooms(self):
-        return self.session.get('{}/api/rooms'.format(BASE_URL))
+        return self.session.get(self.base_url)
 
     def create_room(self, roomName, roomType):
-        return self.session.post('{}/api/rooms'.format(BASE_URL), data={roomName: roomName, roomType: roomType})
+        data = {roomName, roomType}
+        return self.session.post(self.base_url, data=data)
 
 
 class UserRequests:
     def __init__(self, session=requests.session()):
         self.session = session
+        self.base_url = '{}/api/user'.format(BASE_URL)
 
     def create_user(self, username, password, confirmPassword):
-        return self.session.post('{}/api/user'.format(BASE_URL), data={username, password, confirmPassword})
+        data = {username, password, confirmPassword}
+        return self.session.post(self.base_url, data=data)
 
     def get_user(self, id):
-        return self.session.get('{base}/api/user/{id}'.format(base=BASE_URL, id=id))
+        return self.session.get('{base}/{id}'.format(base=self.base_url, id=id))
 
     def delete_user(self, id):
-        return self.session.delete('{base}/api/user/{id}'.format(base=BASE_URL, id=id))
+        return self.session.delete('{base}/{id}'.format(base=self.base_url, id=id))
 
     def update_user(self, id, to_update):
-        return self.session.patch('{base}/api/user/{id}'.format(base=BASE_URL, id=id), to_update)
+        return self.session.patch('{base}/{id}'.format(base=self.base_url, id=id), to_update)
 
 
 class Tester:
