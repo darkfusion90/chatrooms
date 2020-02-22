545c315a9fe60abe4c209a372ac528c5f474b464
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-22 20:35:06 +0530

    Migrated error reponse determiner to ../utils/errorResponseMetaCreator

--- server/routes/routeUtils.js
+++ server/routes/routeUtils.js
@@ -1,26 +1,5 @@
 const httpStatusCodes = require('../constants/httpStatusCodes')
-const UnauthorizedError = require('../errors/Unauthorized')
-const MongooseDuplicateKeyError = require('../errors/MongooseDuplicateKeyError')
-
-const determineResponseMetaFromError = (err) => {
-    const responseMeta = {}
-
-    if (err instanceof MongooseDuplicateKeyError) {
-        responseMeta.statusCode = httpStatusCodes.CONFLICT
-        responseMeta.data = err
-    }
-    else if (err instanceof UnauthorizedError) {
-        responseMeta.statusCode = httpStatusCodes.FORBIDDEN
-        responseMeta.data = {}
-    }
-    else {
-        console.log(err)
-        responseMeta.statusCode = httpStatusCodes.INTERNAL_SERVER_ERROR
-        responseMeta.data = {}
-    }
-
-    return responseMeta
-}
+const determineResponseMetaFromError = require('../utils/errorResponseMetaCreator')
 
 const determineResponseMeta = (err, resource) => {
     let responseMeta = {}
