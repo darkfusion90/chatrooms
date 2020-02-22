c421e1031f6ea47b2b8967ff566b6f028347614b
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-22 21:06:15 +0530

    Added simple function to print a line of colored stars for distinguishing b/w test components

--- server/tests/util.py
+++ server/tests/util.py
@@ -1,8 +1,14 @@
 from random import randint
 
+COLOR_RESET = '\033[0m'
+
 with open('names.txt') as file:
     name_list = file.readlines()
 
 
 def get_random_username():
     return name_list[randint(0, len(name_list)-1)].strip()
+
+
+def print_star_line(color=COLOR_RESET):
+    print(color, '*'*120, '\n', COLOR_RESET, sep='')
