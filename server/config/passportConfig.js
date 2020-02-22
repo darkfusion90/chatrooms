89dd1a686031c9bff731c80d8bd821242f280cb0
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-22 18:34:59 +0530

    Removed direct interactions with model Uses controller to get user

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -1,23 +1,22 @@
-const config = require('./config')
 const passport = require('passport')
 const PassportLocalStrategy = require('passport-local').Strategy
-const User = require('../models/User')
+const { getUser, getUserByUsername, filterUsingProjections } = require('../controllers/users')
 const logger = require('../utils/logger')('[PassportConfig] ')
 
+const getUserWithoutPasswordField = (user) => {
+    return filterUsingProjections(user)
+}
+
 passport.serializeUser((user, done) => {
-    logger.debug("<Serialize> ", user)
     done(null, user.userId)
 })
 
 passport.deserializeUser((userId, done) => {
-    User.findOne({ userId: userId }, (err, user) => {
-        logger.debug("<Deserialize> ", user)
-        done(err, user)
-    })
+    getUser(userId, done)
 })
 
 passport.use(new PassportLocalStrategy({}, (username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
+    getUserByUsername(username, true, (err, user) => {
         if (err) {
             logger.debug('<Strategy> err: ', err)
             return done(err)
@@ -31,7 +30,7 @@ passport.use(new PassportLocalStrategy({}, (username, password, done) => {
             return done(null, false, { reason: 'Incorrect username or password' })
         }
         logger.debug('<Strategy> success: ', user)
-        return done(null, user)
+        return done(null, getUserWithoutPasswordField(user))
     })
 }))
 
