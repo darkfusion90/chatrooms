227c36af1ab8790f41eb053e0d9eb276c96ced50
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-22 12:44:44 +0530

    Function determines both status code and response data based on properties of err and resource

--- server/routes/routeUtils.js
+++ server/routes/routeUtils.js
@@ -1,29 +1,50 @@
 const httpStatusCodes = require('../constants/httpStatusCodes')
 const UnauthorizedError = require('../errors/Unauthorized')
+const MongooseDuplicateKeyError = require('../errors/MongooseDuplicateKeyError')
 
-const determineStatusCode = (err, resource) => {
-    if (err) {
-        return err instanceof UnauthorizedError ?
-            httpStatusCodes.FORBIDDEN :
-            httpStatusCodes.INTERNAL_SERVER_ERROR
+const determineResponseMetaFromError = (err) => {
+    const responseMeta = {}
+
+    if (err instanceof MongooseDuplicateKeyError) {
+        responseMeta.statusCode = httpStatusCodes.CONFLICT
+        responseMeta.data = err
     }
-    else if (!resource) {
-        return httpStatusCodes.RESOURCE_NOT_FOUND
+    else if (err instanceof UnauthorizedError) {
+        responseMeta.statusCode = httpStatusCodes.FORBIDDEN
+        responseMeta.data = {}
     }
     else {
-        return httpStatusCodes.OK
+        responseMeta.statusCode = httpStatusCodes.INTERNAL_SERVER_ERROR
+        responseMeta.data = {}
     }
+
+    return responseMeta
+}
+
+const determineResponseMeta = (err, resource) => {
+    let responseMeta = {}
+
+    if (err) {
+        responseMeta = determineResponseMetaFromError(err)
+    } else if (!resource) {
+        responseMeta.statusCode = httpStatusCodes.RESOURCE_NOT_FOUND
+        responseMeta.data = {}
+    } else {
+        responseMeta.statusCode = httpStatusCodes.OK
+        responseMeta.data = resource
+    }
+
+    return responseMeta
 }
 
 genericHandlerCallback = (err, resource, response, returnResourceOnSuccess = false) => {
-    const statusCode = determineStatusCode(err, resource)
+    const { statusCode, data } = determineResponseMeta(err, resource)
+
     if (statusCode === httpStatusCodes.OK && returnResourceOnSuccess) {
         return resource
     }
-    else {
-        response.status(statusCode).json(resource ? resource : {})
-        return false
-    }
+
+    response.status(statusCode).json(data)
 }
 
 module.exports = { genericHandlerCallback }
