c0e45a0de5b09eae7cf2e7734c0d18b7e9dd88b8
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-22 12:43:34 +0530

    Redirects to /api/user with a flag in the session to indicate it came from here

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,29 +1,8 @@
-const registrationFormValidator = require('../utils/registrationFormValidator')
-const usersController = require('../controllers/users')
-const logger = require('../utils/logger')('[Router: Register] ')
+const httpStatusCodes = require('../constants/httpStatusCodes')
 
 module.exports = (req, res) => {
-    logger.debug('Register Post')
-    logger.debug('Req body: ', req.body)
-    const { errors, hasErrors } = registrationFormValidator(req.body)
-
-    if (hasErrors) {
-        res.status(400).json({ errors, hasErrors })
-        return
-    }
-
-    usersController.registerUser(req.session.userId, req.body.username, req.body.password, (err, user) => {
-        if (err) {
-            res.status(500).send('Oops! The server was punched really hard by the Hulk :(')
-        }
-        else if (!user) {
-            res.status(500).send('Oops! The server was punched really hard by the Hulk :(')
-        }
-        else {
-            logger.debug('Reg Post callback. User: ', user)
-            req.session.isRegistered = user.isRegistered
-            req.session.save()
-            res.json({ errors: false, username: user.username })
-        }
-    });
+    req.session.redirectedFromRegister = true
+    req.session.save()
+    //Using 307 Temporary Redirect to make sure the redirect is done using the same method as /api/register (i.e, POST)
+    res.redirect(httpStatusCodes.TEMPORARY_REDIRECT, '/api/user')
 }
