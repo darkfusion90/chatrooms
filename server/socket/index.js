52c6f520fa9c2e08faf6e0133adbd7659e7a7bfc
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-25 06:01:06 +0530

    New event: USER_EVENT: CHECK_USERNAME added Checks if username exists

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,6 +1,8 @@
 const socketio = require('socket.io')
 const roomEventHandler = require('./eventHandlers/roomEventHandler')
-const events = require('./../constants/socket_event_constants')
+const events = require('../constants/socket_event_constants')
+const httpStatusCodes = require('../constants/httpStatusCodes')
+const { getUserByUsername } = require('../controllers/users')
 
 function listen(server, sessionMiddleware) {
     const io = socketio.listen(server)
@@ -10,6 +12,24 @@ function listen(server, sessionMiddleware) {
     io.on('connection', (client) => {
         console.log("FROM SOCKET.IO, UserId: ", client.request.session.userId)
         client.on(events.ROOM_EVENT, (...args) => roomEventHandler(io, client, ...args));
+        client.on(events.USER_EVENT, (subEvent, data, callback) => {
+            console.log('event: ', subEvent, '\n', data)
+            switch (subEvent) {
+                case events.CHECK_USERNAME:
+                    getUserByUsername(data, false, (err, user) => {
+                        if (err) {
+                            console.log(err)
+                            callback({ status: httpStatusCodes.INTERNAL_SERVER_ERROR, })
+                        }
+                        else if (!user) {
+                            callback({ status: httpStatusCodes.RESOURCE_NOT_FOUND })
+                        }
+                        else {
+                            callback({ status: httpStatusCodes.OK, data: user })
+                        }
+                    })
+            }
+        })
     })
 }
 
