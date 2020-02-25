aa3d54df6654ddd9016c184138d3811489369bc2
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-25 05:50:00 +0530

    Functionality moved to updateUser.js

--- client/src/actions/updateUserStatus.js
+++
@@ -1,8 +0,0 @@
-import { UPDATE_USER_INFO } from '../constants/actionConstants'
-
-export default (valuesToUpdate) => {
-    return {
-        type: UPDATE_USER_INFO,
-        payload: valuesToUpdate
-    }
-}
