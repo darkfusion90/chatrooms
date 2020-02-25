ccc4df44503ae2649427ff54a035c10095c03abf
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-25 05:48:20 +0530

    Removed failure check in onSuccess callback (Yeah "what was he even thinking")

--- client/src/actions/loginUser.js
+++ client/src/actions/loginUser.js
@@ -1,33 +1,20 @@
-import {loginUser} from '../server-communication/httpServer'
+import { loginUser } from '../server-communication/httpServer'
 
 import { LOGIN } from '../constants/actionConstants'
 
-const onRequestFulfilled = (dispatch, response, onFailureCallback) => {
-    console.log("onRequestFulfilled: ", response.data)
-    if (response.data.error) {
-        onFailureCallback(response)
-    }
-    else {
-        dispatch(loginAction(response.data.username))
-    }
-}
-
-const loginAction = (username) => {
+const loginAction = (user) => {
     return {
         type: LOGIN,
         payload: {
-            username: username
+            user: user
         }
     }
 }
 
 export default (username, password, onFailureCallback) => async (dispatch) => {
-    loginUser(
-        username,
-        password,
-        (response) => {
-            onRequestFulfilled(dispatch, response, onFailureCallback)
-        },
-        (reason) => onFailureCallback(reason.response)
-    );
+    const dispatchLoginAction = (response) => {
+        dispatch(loginAction(response.data))
+    }
+
+    loginUser(username, password, dispatchLoginAction, onFailureCallback);
 }
