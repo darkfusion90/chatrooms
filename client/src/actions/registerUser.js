f4734a5a71aed5776c8bad0d5d6e3f5b3405d594
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-25 05:49:13 +0530

    Moved on success callback to anonymous arrow function

--- client/src/actions/registerUser.js
+++ client/src/actions/registerUser.js
@@ -1,14 +1,10 @@
 import { REGISTER } from '../constants/actionConstants';
-import {registerUser} from '../server-communication/httpServer'
+import { registerUser } from '../server-communication/httpServer'
 
-const onRequestFulfilled = (response, dispatch) => {
-    console.log("Register Request fulfilled: ", response)
-    dispatch({ type: REGISTER, payload: { username: response.data.username } })
-}
 
 export default (username, password, confirmPassword, onRequestRejected) => async (dispatch) => {
     registerUser(username, password, confirmPassword,
-        (response) => onRequestFulfilled(response, dispatch),
+        (response) => dispatch({ type: REGISTER, payload: response.data }),
         onRequestRejected
     )
 }
