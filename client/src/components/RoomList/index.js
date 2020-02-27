6bf74782141f5a7f299d1c80b58b85cd54c4b482
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-27 06:48:03 +0530

    Adds created by ${username} to room meta display

--- client/src/components/RoomList/index.js
+++ client/src/components/RoomList/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, ListGroup } from 'react-bootstrap';
 
-const renderRoomList = (rooms) => {
-    return rooms.map(({ roomId, name, owner }) => {
+const renderRoomList = (props) => {
+    const getUserById = (id) => {
+        return props.users[id]
+    }
+
+    return props.rooms.map((room) => {
+        const roomOwner = getUserById(room.owner)
         return (
-            <ListGroup.Item key={roomId}>
+            <ListGroup.Item key={room.roomId}>
                 <Card>
                     <Card.Body>
                         <Card.Title>
-                            <Link to={`/rooms/${roomId}`}>{name}</Link>
+                            <Link to={`/rooms/${room.roomId}`}>{room.name}</Link>
                         </Card.Title>
                         <Card.Text>
-                            Created by: {owner}
+                            {roomOwner ? `Created by: ${roomOwner.username}` : ''}
                         </Card.Text>
                     </Card.Body>
                 </Card>
@@ -23,7 +28,7 @@ const renderRoomList = (rooms) => {
 }
 
 const RoomList = (props) => {
-    return <ListGroup variant='flush'>{renderRoomList(props.rooms)}</ListGroup>
+    return <ListGroup variant='flush'>{renderRoomList(props)}</ListGroup>
 }
 
 export default RoomList;
