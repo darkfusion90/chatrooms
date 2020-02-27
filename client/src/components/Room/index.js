1b026015460196498f4e063f747a15b002f2312a
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-27 06:46:55 +0530

    Room now contains a chat window and a textarea to help type message to send

--- client/src/components/Room/index.js
+++ client/src/components/Room/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Container, ListGroup } from 'react-bootstrap';
 
-const Room = (props) => {
+import ChatMessageForm from '../../components/forms/ChatMessageForm';
+import MessageContainer from '../../containers/MessageContainer';
+
+import './style.scss'
+
+const renderMessageList = (room) => {
+    if (!room) {
+        return null
+    }
+
     return (
-        <div>
-            Hi! I am a room
-            Name: {props.room.name}
-            Created: {props.room.createdAt}
-        </div>
+        <ListGroup className='chat-container pre-scrollable'>
+            {
+                room.messages.map(messageId => {
+                    return <MessageContainer messageId={messageId} key={messageId} />
+                })
+            }
+        </ListGroup>
+    )
+}
+
+const Room = ({ room, onSendMessageButtonClick }) => {
+    console.log('room: ', room)
+    return (
+        <Container style={{ border: '1px solid red' }}>
+            <Container>
+                <p>Hey! I am the chat container of room {room ? room.name : ''}</p>
+                <br />
+                {renderMessageList(room)}
+            </Container>
+            <ChatMessageForm onFormSubmit={onSendMessageButtonClick} />
+        </Container>
     );
 }
 
