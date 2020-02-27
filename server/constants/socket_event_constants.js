bb506ba0e2094bf7351857488d44c851598f4450
darkfusion90 <gauravsharmaa341@gmail.com>
2020-02-27 06:52:14 +0530

    Minor refactoring

--- server/constants/socket_event_constants.js
+++ server/constants/socket_event_constants.js
@@ -1,14 +1,14 @@
 module.exports = {
-    ROOM_EVENT: "event:room",
-    ROOM_JOIN_PERMISSION_SEND: "room_join_permission_send",
-    ROOM_JOIN_PERMISSION_RECIEVE: "room_join_permission_recieve",
-    CREATE_ROOM: "create_room",
-    JOIN_ROOM: "join_room",
+    ROOM_EVENT: 'event:room',
+    ROOM_JOIN_PERMISSION_SEND: 'room_join_permission_send',
+    ROOM_JOIN_PERMISSION_RECIEVE: 'room_join_permission_recieve',
+    CREATE_ROOM: 'create_room',
+    JOIN_ROOM: 'join_room',
 
-    NEW_MESSAGE_RECIEVED: "new_message_recieved",
-    SEND_MESSAGE: "send_message",
-    USER_ID_RECIEVE: "user_id_recieve",
+    MESSAGE_EVENT: 'event:message',
+    SEND_MESSAGE: 'send_message',
+    RECEIVE_MESSAGE: 'receive-message',
 
-    USER_EVENT: "event:user",
-    CHECK_USERNAME: "CHECK_USERNAME"
+    USER_EVENT: 'event:user',
+    CHECK_USERNAME: 'check_username'
 }
