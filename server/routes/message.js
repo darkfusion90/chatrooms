fbb1a78dc514d7f185d328ccf90f920d195e9e2c
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-01 18:27:12 +0530

    Refactored to roomMessages

--- server/routes/message.js
+++
@@ -1,8 +0,0 @@
-const { getMessage } = require('../controllers/messages')
-const { genericHandlerCallback } = require('./routeUtils')
-
-const get = (req, res) => {
-    getMessage(req.params.id, (err, message) => genericHandlerCallback(err, message, res))
-}
-
-module.exports = {get}
