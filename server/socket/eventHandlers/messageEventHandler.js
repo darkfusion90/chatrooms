02b0b449b36118b0f776a4b1ace115bd13999c00
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-01 18:28:30 +0530

    Resource creation left to http server. Only emits messageId now

--- server/socket/eventHandlers/messageEventHandler.js
+++ server/socket/eventHandlers/messageEventHandler.js
@@ -6,37 +6,14 @@ const logger = require('../../utils/logger')('[Socket: MessageEventHandler] ')
 
 
 const handleSendMessage = (io, client, ...args) => {
-    const userId = client.request.session.userId
-    const roomId = args[0], message = args[1], clientCallback = args[2]
-    logger.debug('roomId: ' + roomId)
-    logger.debug('message: ' + message)
-    messagesController.createMessage(userId, roomId, message, (err, messageDoc) => {
-        if (err) {
-            clientCallback({ status: httpStatusCodes.INTERNAL_SERVER_ERROR })
-        }
-        else {
-            roomsController.addNewMessage(roomId, userId, messageDoc.id, (err, updatedRoom) => {
-                if (err) {
-                    clientCallback({ status: httpStatusCodes.INTERNAL_SERVER_ERROR })
-                }
-                else {
-                    clientCallback({ status: httpStatusCodes.OK, data: updatedRoom })
-                }
-            })
-        }
+    const [roomId, messageId] = args
+    client.broadcast.to(roomId).emit(events.NEW_MESSAGE, {
+        author: client.request.session.userId,
+        messageId: messageId
     })
 }
 
 const eventHandler = (io, client, subEvent, ...args) => {
-    const userId = client.request.session.userId
-    logger.debug("Message Event fired by: " + userId)
-    logger.debug("SubEvent: " + subEvent)
-    logger.debug("Other args: ", args)
-    const [roomId, message, clientCallback] = args
-    logger.debug('unpack roomId: ', roomId)
-    logger.debug('unpack message: ', message)
-    logger.debug('unpack clientCallback: ', clientCallback)
-
     switch (subEvent) {
         case events.SEND_MESSAGE:
             handleSendMessage(io, client, ...args)
