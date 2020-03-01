a9fc283ed7fa1f6c08c7bc54d8a8ff31165576b5
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-01 18:21:29 +0530

    Added/Modified events

--- client/src/constants/serverEventConstants.js
+++ client/src/constants/serverEventConstants.js
@@ -1,13 +1,13 @@
 export default {
-    ROOM_EVENT: "event:room",
-    ROOM_JOIN_PERMISSION_SEND: "room_join_permission_send",
-    ROOM_JOIN_PERMISSION_RECIEVE: "room_join_permission_recieve",
-    CREATE_ROOM: "create_room",
-    JOIN_ROOM: "join_room",
+    ROOM_EVENT: 'event:room',
+    ROOM_JOIN_PERMISSION_SEND: 'room_join_permission_send',
+    ROOM_JOIN_PERMISSION_RECIEVE: 'room_join_permission_recieve',
+    CONNECT_TO_ROOM: 'connect_to_room',
+    JOIN_ROOM: 'join_room',
+    NEW_USER_IN_ROOM: 'new_user_in_room',
 
     MESSAGE_EVENT: 'event:message',
-    SEND_MESSAGE: 'send_message',
-    RECEIVE_MESSAGE: 'receive-message',
+    NEW_MESSAGE: 'send_message',
 
     USER_EVENT: 'event:user',
     CHECK_USERNAME: 'check_username'
