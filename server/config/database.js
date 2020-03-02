6c5f1bc9352689ab1c741d9cd532d7121efbe45b
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-02 18:00:36 +0530

    Added log to database config

--- server/config/database.js
+++ server/config/database.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 //Solution to -> "DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead"
 mongoose.set('useCreateIndex', true)
 mongoose.set('useFindAndModify', false)
+console.log("Mongodb Connection URI: ", config.MONGO_URI)
 mongoose.connect(config.MONGO_URI, config.MONGO_CLIENT_OPTS);
 mongoose.connection.on('open', function cb() {
     console.log("connected to db")
