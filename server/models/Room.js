d09959b17a811dd8ed96c40d3205aad0774f0f6f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-02 19:59:43 +0530

    Added members field to track members of room. Will be populated

--- server/models/Room.js
+++ server/models/Room.js
@@ -25,19 +25,33 @@ const RoomSchema = Schema({
         required: true,
         index: true
     },
-    messages: [String]
+    messages: [String],
+    members: [{
+        type: Schema.Types.ObjectId,
+        ref: 'RoomMember'
+    }]
 })
 
+function resolveOperations(update) {
+    console.log(update["$push"])
+}
+
+function hasPermissionForOperations(userId, operations) {
+
+}
 
 async function modificationPermissionValidator(next) {
     const { roomId, owner } = this.getQuery()
     const docToUpdate = await this.model.findOne({ roomId: roomId })
 
+    console.log('Query: ', this.getUpdate())
+    //hasPermissionForOperations({ owner:})
     //Check owner only if valid document
     //If not valid, 404 Not Found will be sent to client by the callback given to the respective controller
-    if (docToUpdate && docToUpdate.owner != owner) {
+    /*if (docToUpdate && docToUpdate.owner != owner) {
+        console.log('not authorized')
         next(new UnauthorizedError())
-    }
+    }*/
     next()
 }
 
