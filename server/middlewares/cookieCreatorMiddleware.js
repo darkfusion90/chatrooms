30e5c479371b339ad03d218a5b9a43cf45d0afca
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-04 21:27:38 +0530

    Creating user and adding to session moved to util function

--- server/middlewares/cookieCreatorMiddleware.js
+++ server/middlewares/cookieCreatorMiddleware.js
@@ -1,4 +1,5 @@
 const { createUnregisteredUser } = require('../controllers/users')
+const addUserToSession = require('../utils/addUserToSession')
 
 const isRegisterPath = (path) => {
     return /^\/api\/register(\/){0,1}$/.test(path)
@@ -8,19 +9,8 @@ const middleware = (req, res, next) => {
     if (isRegisterPath(req.path) || req.session.redirectedFromRegister) {
         next()
     } else if (!req.session.userId) {
-        createUnregisteredUser(req.session.cookie.expires, (err, user) => {
-            //In this case, unable to create user is the server's fault because the client doesn't send any payload
-            //Hence, there is no way for the client to screw up
-            //Failure will generally occur due to duplicate userId (automatically generated in the users controller)
-            if (err || !user) {
-                return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({})
-            }
-
-            req.session.userId = user.userId
-            req.session.isRegistered = false
-            req.session.save()
-            next()
-        })
+        if (addUserToSession(req)) next()
+        else return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({})
     }
     else {
         next()
