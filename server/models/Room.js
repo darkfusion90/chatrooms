b72e3df0fd465fe1ea0154214202e6e28639cbe8
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-04 20:25:34 +0530

    Replaced field owner with createdBy Removed permission validators

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('./User')
 const UnauthorizedError = require('../errors/Unauthorized')
 
 const Schema = mongoose.Schema;
@@ -20,43 +21,13 @@ const RoomSchema = Schema({
         enum: ['private', 'public', 'unlisted'],
         required: true
     },
-    owner: {
-        type: String,
-        required: true,
-        index: true
-    },
+    createdBy: User,
     messages: [String],
     members: [{
         type: Schema.Types.ObjectId,
         ref: 'RoomMember'
     }]
 })
-
-function resolveOperations(update) {
-    console.log(update["$push"])
-}
-
-function hasPermissionForOperations(userId, operations) {
-
-}
-
-async function modificationPermissionValidator(next) {
-    const { roomId, owner } = this.getQuery()
-    const docToUpdate = await this.model.findOne({ roomId: roomId })
-
-    console.log('Query: ', this.getUpdate())
-    //hasPermissionForOperations({ owner:})
-    //Check owner only if valid document
-    //If not valid, 404 Not Found will be sent to client by the callback given to the respective controller
-    /*if (docToUpdate && docToUpdate.owner != owner) {
-        console.log('not authorized')
-        next(new UnauthorizedError())
-    }*/
-    next()
-}
-
-RoomSchema.pre('findOneAndUpdate', modificationPermissionValidator)
-RoomSchema.pre('findOneAndDelete', modificationPermissionValidator)
 
 RoomSchema.methods.userHasPermission = function (userId) {
     if (this.type === 'private') {
