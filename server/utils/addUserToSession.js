c44f73bf0a572374387a3d1ae3221779f5d4f17e
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-06 16:51:02 +0530

    Transformed to promise based function

--- server/utils/addUserToSession.js
+++ server/utils/addUserToSession.js
@@ -1,16 +1,18 @@
-const {createUnregisteredUser} = require('../controllers/users')
+const { createUnregisteredUser } = require('../controllers/users')
 module.exports = (req) => {
-    createUnregisteredUser(req.session.cookie.expires, (err, user) => {
-        //In this case, unable to create user is the server's fault because the client doesn't send any payload
-        //Hence, there is no way for the client to screw up
-        //Failure will generally occur due to duplicate userId (automatically generated in the users controller)
-        if (err || !user) {
-            return null
-        }
-
-        req.session.userId = user._id
-        req.session.isRegistered = false
-        req.session.save()
-        return user
+    return new Promise((resolve, reject) => {
+        createUnregisteredUser(req.session.cookie.expires, (err, user) => {
+            if (err) {
+                reject(err)
+            } else if (!user) {
+                console.log('here')
+                reject(new Error('User could not be created'))
+            } else {
+                req.session.userId = user._id
+                req.session.isRegistered = false
+                req.session.save()
+                resolve(user)
+            }
+        })
     })
 }
