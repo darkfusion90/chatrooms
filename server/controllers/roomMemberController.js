cf3ed635c92701f1c2812637b366416a7d145850
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-06 14:30:11 +0530

    Added room in roomMember creatore

--- server/controllers/roomMemberController.js
+++ server/controllers/roomMemberController.js
@@ -1,17 +1,17 @@
 const RoomMember = require('../models/RoomMember')
 
-const _createRoomMember = (user, memberType, onFulfilled, onRejected) => {
-    const roomMember = new RoomMember({ user, memberType })
+const _createRoomMember = (room, user, memberType, onFulfilled, onRejected) => {
+    const roomMember = new RoomMember({ room, user, memberType })
     roomMember.save().then(onFulfilled).catch(onRejected)
 }
 
-exports.createRoomMember = (user, memberType, callback) => {
+exports.createRoomMember = (roomObjectId, userId, memberType, callback) => {
     if (callback) {
-        _createRoomMember(user, memberType, roomMember => callback(null, roomMember), err => callback(err, null))
+        _createRoomMember(roomObjectId, userId, memberType, roomMember => callback(null, roomMember), err => callback(err, null))
     }
     else {
         return new Promise((resolve, reject) => {
-            _createRoomMember(user, memberType, resolve, reject)
+            _createRoomMember(roomObjectId, userId, memberType, resolve, reject)
         })
     }
 }
