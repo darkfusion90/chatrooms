f7b846ddae8dba11809f8724be69eff45ad7ba43
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-06 16:49:23 +0530

    Added logs

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,10 +1,14 @@
 const determineResponseMetaFromError = require('../utils/errorResponseMetaCreator')
+const logger = require('../utils/logger')('[ExpressErrorHandler] ')
 
 module.exports = (err, _, res, next) => {
+    logger.debug('Error encountered: ', err)
     if (res.headersSent) {
+        logger.debug('Headers sent. Calling next middleware: ')
         next(err)
     }
 
+    logger.debug('Will determine response meta from error: ')
     const { statusCode, data } = determineResponseMetaFromError(err)
     res.status(statusCode).json(data)
 }
