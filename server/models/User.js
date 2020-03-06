60e31081cd974daf4c7f2b096bcc88c14fb3b663
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-06 16:50:25 +0530

    Comma removed. Seriously -_-

--- server/models/User.js
+++ server/models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         index: true,
-        unique: true,
+        unique: true
     },
     password: String,
     createdAt: {
