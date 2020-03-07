5323389bf6ab009d62b3da069b7482d1f8c86f09
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-07 18:22:53 +0530

    Added Populate and projections config Added 'members' field operations

--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -3,26 +3,49 @@ const messagesController = require('./messages')
 const { createRoomMember, MEMBER_TYPE_ADMIN } = require('./roomMemberController')
 const uniqueIdGenerator = require('../utils/uniqueIdGenerator')
 
-function generateRoomId() {
-    return uniqueIdGenerator.generateIdUsingRandomWords()
+const DEFAULT_PROJECTIONS = {
+    '_id': 0,
+    '__v': 0
 }
 
-function populateApplicableFields(query) {
-    return query.populate('createdBy').populate('messages').populate({
+const POPULATE_CONFIG = {
+    createdBy: {
+        path: 'createdBy',
+        model: 'User',
+        select: 'username isRegistered'
+    },
+    messages: {
+        path: 'messages',
+        model: 'Message',
+        select: 'author atRoom data'
+    },
+    members: {
         path: 'members',
         model: 'RoomMember',
-        select: 'user memberType -_id',
+        select: 'user memberType',
         populate: {
             path: 'user',
             model: 'User',
             select: 'username isRegistered -_id',
         }
-    })
+    }
+}
+
+function generateRoomId() {
+    return uniqueIdGenerator.generateIdUsingRandomWords()
+}
+
+function populateApplicableFields(query) {
+    const { createdBy, members, messages } = POPULATE_CONFIG
+    return query.populate(members).populate(messages).populate(createdBy)
 }
 
 function addMemberToRoom(room, roomMember, callback) {
-    Room.findByIdAndUpdate(room._id, { $push: { members: roomMember } }, { returnOriginal: false })
-        .populate('createdBy').exec(callback)
+    populateApplicableFields(Room.findByIdAndUpdate(
+        room._id,
+        { $push: { members: roomMember } },
+        { returnOriginal: false, fields: DEFAULT_PROJECTIONS })
+    ).exec(callback)
 }
 
 function createRoom(name, type, creatorUserId, callback) {
@@ -50,50 +73,70 @@ function deleteRoom(roomId, callback) {
 }
 
 function getAllPublicRooms(callback) {
-    populateApplicableFields(Room.find({ type: 'public' })).exec(callback)
+    populateApplicableFields(Room.find({ type: 'public' }, DEFAULT_PROJECTIONS)).exec(callback)
 }
 
 function getRoomByRoomId(roomId, callback) {
-    populateApplicableFields(Room.findOne({ roomId: roomId })).exec(callback)
+    populateApplicableFields(Room.findOne({ roomId: roomId }, DEFAULT_PROJECTIONS)).exec(callback)
 }
 
-function updateRoomByRoomId(roomId, updatedValues, callback) {
-    Room.findOneAndUpdate({ roomId: roomId }, updatedValues, { returnOriginal: false }, callback)
+function updateRoomByRoomId(roomId, updates, callback) {
+    populateApplicableFields(Room.findOneAndUpdate(
+        { roomId: roomId },
+        updates,
+        { returnOriginal: false, fields: DEFAULT_PROJECTIONS })
+    ).exec(callback)
 }
 
 function addMessageToRoom(roomId, author, message, callback) {
     Room.findOne({ roomId }, (err, room) => {
-        if (!room || err) callback(err, room)
-        messagesController.createMessage(author, roomId, message, (err, messageDoc) => {
-            if (!messageDoc || err) callback(err, messageDoc)
-            room.update({ $push: { 'messages': messageDoc } })
-        })
+        if (!room || err) return callback(err, room)
+        messagesController.createMessage(author, roomId, message).then((messageDoc) => {
+            updateRoomByRoomId(roomId, { $push: { messages: messageDoc } }, callback)
+        }).catch(callback)
     })
 }
 
 function getRoomMessage(roomId, messageId, callback) {
-    Room.findOne({ roomId: roomId, messages: messageId }, (err, room) => {
-        if (!err && room) {
-            return messagesController.getMessage(messageId, callback)
-        }
-        callback(err, room)
-    })
+    Room.findOne({ roomId: roomId, messages: messageId }, 'messages -_id')
+        .populate(POPULATE_CONFIG.messages)
+        .exec((err, room) => {
+            if (room) {
+                return callback(null, room.messages[0])
+            }
+            callback(err, room)
+        })
 }
 
 function getAllMessagesInRoom(roomId, callback) {
-    Room.findOne({ roomId: roomId }, async (err, room) => {
-        if (room) {
-            const messages = await Promise.all(room.messages.map(messageId => {
-                return messagesController.getMessage(messageId)
-            }))
-            callback(null, messages)
-        }
-        else {
-            callback(err, null)
-        }
-    })
+    Room.findOne({ roomId: roomId }, 'messages -_id').populate(POPULATE_CONFIG.messages).exec(callback)
 }
 
+function deleteMessageFromRoom(roomId, messageId, callback) {
+    populateApplicableFields(Room.findOneAndUpdate(roomId, { $pull: { messages: messageId } })).exec(callback)
+}
+
+function getRoomMember(roomId, memberId, callback) {
+    Room.findOne({ roomId, members: memberId }, DEFAULT_PROJECTIONS)
+        .populate(POPULATE_CONFIG.members)
+        .exec((err, room) => callback(err, room ? room.members[0] : room))
+}
+
+function getAllRoomMembersOfRoom(roomId, callback) {
+    Room.findOne({ roomId }, DEFAULT_PROJECTIONS)
+        .populate(POPULATE_CONFIG.members)
+        .exec((err, room) => callback(err, room ? room.members : room))
+}
+
+function addMemberToRoomByRoomId(roomId, memberData, callback) {
+    Room.findOne({ roomId }, (err, room) => {
+        if (err || !room) return callback(err, room)
+        createRoomMember(room._id, memberData.userId, memberData.memberType)
+            .then((roomMember) => {
+                addMemberToRoom(room, roomMember, callback)
+            }).catch(callback)
+    })
+}
 
 module.exports = {
     createRoom,
@@ -104,4 +147,8 @@ module.exports = {
     getRoomMessage,
     getAllMessagesInRoom,
     addMessageToRoom,
+    deleteMessageFromRoom,
+    getRoomMember,
+    getAllRoomMembersOfRoom,
+    addMemberToRoomByRoomId
 }
