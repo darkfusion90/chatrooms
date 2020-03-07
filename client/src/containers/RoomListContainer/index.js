02adeff38083302a2acc67594385cd58acdf6598
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-07 18:57:31 +0530

    Removed user fetching since now server sends that itself

--- client/src/containers/RoomListContainer/index.js
+++ client/src/containers/RoomListContainer/index.js
@@ -1,33 +1,24 @@
 import React from 'react';
 
 import RoomList from '../../components/RoomList';
-import { getAllRooms, getUser } from '../../server-communication/httpServer'
+import { getAllRooms } from '../../server-communication/httpServer'
+
+const logger = require('../../helpers/logger')('[RoomListContainer]')
 
 class RoomListContainer extends React.Component {
-    state = { rooms: [], users: {} }
+    state = { rooms: [] }
 
     componentDidMount() {
         getAllRooms(this.onRoomFetchSuccess, this.onRoomFetchFail)
     }
 
     onRoomFetchFail = ({ response }) => {
-        console.log('RoomList fetch fail: ', response)
+        logger.debug('RoomList fetch fail: ', response)
     }
 
     onRoomFetchSuccess = (response) => {
-        console.log('RoomList fetch success: ', response)
-        const rooms = response.data
-        rooms.forEach(room => getUser(room.owner, this.onUserFetchSuccess, this.onUserFetchFail))
-        this.setState({ rooms: rooms })
-    }
-
-    onUserFetchFail = ({ response }) => {
-        console.log('User fetch fail: ', response)
-    }
-
-    onUserFetchSuccess = (response) => {
-        const user = response.data
-        this.setState({ users: { ...this.state.users, [user.userId]: user } })
+        logger.debug('RoomList fetch success: ', response)
+        this.setState({ rooms: response.data })
     }
 
     render() {
