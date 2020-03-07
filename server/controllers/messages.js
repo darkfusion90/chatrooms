a5d364a6dfdc2885b9c023ac83fc62206e7821d8
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-07 18:18:14 +0530

    Fixed promise based functions Added delete message

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -2,38 +2,41 @@ const Message = require('../models/Message')
 const logger = require('../utils/logger')('[MessagesController] ')
 
 exports.createMessage = (author, roomId, messageTxt, callback) => {
-    const message = new Message({
-        author: author,
-        atRoom: roomId,
-        data: messageTxt
-    })
-    message.save(function (err) {
-        if (err) {
+    const promise = new Promise((resolve, reject) => {
+        const message = new Message({
+            author: author,
+            atRoom: roomId,
+            data: messageTxt
+        })
+        message.save().then((message) => {
+            logger.debug('Successfully created message: ', message)
+            resolve(message)
+        }).catch((err) => {
             logger.debug('Error creating message: ', message)
             logger.debug('Error: ', err)
-            callback(err, null)
-        }
-        else {
-            logger.debug('Successfully created message: ', message)
-            callback(null, message)
-        }
+            reject(err)
+        })
     })
+
+    if (callback && typeof (callback) === 'function') {
+        promise.then((message) => callback(null, message)).catch((err) => callback(err, null))
+    }
+
+    return promise
 }
 
 exports.getMessage = (messageId, callback) => {
-    if (callback) {
-        Message.findById(messageId, callback)
+    const promise = new Promise((resolve, reject) => {
+        Message.findById(messageId).then(resolve).catch(reject)
+    })
+
+    if (callback && typeof (callback) === 'function') {
+        promise.then((message) => callback(null, message)).catch((err) => callback(err, null))
     }
-    else {
-        return new Promise((resolve, reject) => {
-            Message.findById(messageId, (err, message) => {
-                if (err) {
-                    reject(err)
-                }
-                else {
-                    resolve(message)
-                }
-            })
-        })
-    }
+
+    return promise
+}
+
+exports.deleteMessage = (messageId, callback) => {
+    Message.findByIdAndDelete(messageId, callback)
 }
