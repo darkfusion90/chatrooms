030fd8dbc2d1564c5f6fd52f6d608e0bbd8752e4
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-08 13:48:41 +0530

    Added logs

--- server/middlewares/cookieCreatorMiddleware.js
+++ server/middlewares/cookieCreatorMiddleware.js
@@ -1,5 +1,6 @@
 const httpStatusCodes = require('../constants/httpStatusCodes')
 const addUserToSession = require('../utils/addUserToSession')
+const logger = require('../utils/logger')('[CookieCreatorMiddleware]')
 
 const isRegisterPath = (path) => {
     return /^\/api\/register(\/){0,1}$/.test(path)
@@ -7,14 +8,20 @@ const isRegisterPath = (path) => {
 
 const middleware = (req, res, next) => {
     if (isRegisterPath(req.path) || req.session.redirectedFromRegister) {
+        logger.debug('Either register path or redirected from register')
         next()
     } else if (!req.session.userId) {
-        addUserToSession(req).then(() => next()).catch(err => {
+        logger.debug('Session doesn\'t contain user. Creating one. Session: ', req.session)
+        addUserToSession(req).then((user) => {
+            logger.debug('User successfully added to session. Session: ', req.session, '\nUser: ', user)
+            next()
+        }).catch(err => {
+            logger.debug('Error adding user to session. Session: ', req.session, '\nErr: ', err)
             res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({})
-            console.log('bababababab: ', err)
         })
     }
     else {
+        logger.debug('Session already contains user. Session: ', req.session)
         next()
     }
 }
