f8e77a3a295d7946d1b5a0f4a7d5287070ff6fbb
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-08 15:56:45 +0530

    Added refining to actions Converted to class component

--- client/src/containers/RoomListItemContainer/index.js
+++ client/src/containers/RoomListItemContainer/index.js
@@ -1,27 +1,75 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Button, ButtonGroup, Card, Row, Col } from 'react-bootstrap';
+import { Button, ButtonGroup, Card, Row, Col, Spinner } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
+import { joinRoom } from '../../server-communication/httpServer'
 import convertISODateToReadableString from '../../helpers/convertISODateToReadableString';
 import RoomListItem from '../../components/RoomListItem'
 
-const renderRoomMetaData = (room) => {
-    const roomOwner = room.createdBy
-    const roomCreatedBy = roomOwner ? roomOwner.username : '<unknown>'
-    const roomCreatedAt = convertISODateToReadableString(room.createdAt)
+class RoomListItemContainer extends React.Component {
+    state = { joinRoomStatus: 'initial', joinRoomErrorMessage: null }
 
-    return `Created ${roomCreatedAt} by ${roomCreatedBy}`
-}
-
-const renderRoomActions = (room, currentUser) => {
-    const adminActions = <Button variant='danger'>Delete</Button>
-
-    const isRoomAdmin = (roomMember) => {
-        return roomMember && roomMember.memberType === 'admin'
+    onJoinButtonClick = () => {
+        const { roomId } = this.props.room
+        joinRoom(roomId,
+            () => this.setState({ joinRoomStatus: 'success' }),
+            ({ response }) => this.setState({ joinRoomStatus: 'fail', joinRoomErrorMessage: response.message })
+        )
+        this.setState({ joinRoomStatus: 'pending' })
     }
 
-    const getActionsForRoomMember = (roomMember, roomId) => {
+    disabledJoinButtonGridStyleTwoColsTemplate(leftCol, rightCol, variant) {
+        return (
+            <Button variant={variant} disabled={true}>
+                <Row>
+                    <Col>{leftCol}</Col>
+                    <Col style={{ paddingLeft: 0, whiteSpace: 'nowrap' }}>
+                        {rightCol}
+                    </Col>
+                </Row>
+            </Button>
+        )
+    }
+
+    determineJoinButtonMetadata() {
+        let leftCol, rightCol, btnVariant = 'primary';
+
+        if (this.state.joinRoomStatus === 'pending') {
+            leftCol = <Spinner animation='border' size='sm' />
+            rightCol = 'Joining...'
+        } else if (this.state.joinRoomStatus === 'success') {
+            leftCol = <FontAwesomeIcon icon={faCheckCircle} />
+            rightCol = 'Joined'
+            btnVariant = 'success'
+        } else if (this.state.joinRoomStatus === 'fail') {
+            leftCol = <FontAwesomeIcon icon={faTimesCircle} />
+            rightCol = 'Join Failed'
+            btnVariant = 'danger'
+        }
+
+        return { leftCol, rightCol, btnVariant }
+    }
+
+    getActionsForNonRoomMember() {
+        console.log('status: ', this.state.joinRoomStatus)
+        if (this.state.joinRoomStatus === 'initial') {
+            return <Button onClick={this.onJoinButtonClick}>Join</Button>
+        }
+
+        const { leftCol, rightCol, btnVariant } = this.determineJoinButtonMetadata()
+        return this.disabledJoinButtonGridStyleTwoColsTemplate(leftCol, rightCol, btnVariant)
+    }
+
+    getActionsForRoomMember(roomMember, roomId) {
+        const adminActions = <Button variant='danger'>Delete</Button>
+
+        const isRoomAdmin = (roomMember) => {
+            return roomMember && roomMember.memberType === 'admin'
+        }
+
         return (
             <ButtonGroup>
                 <Link to={`/rooms/${roomId}`} component={Button}>Open</Link>
@@ -30,50 +78,73 @@ const renderRoomActions = (room, currentUser) => {
         )
     }
 
-    const getActionsForNonRoomMember = () => {
-        return <Button>Join</Button>
-    }
+    renderRoomActions() {
+        const { room, currentUser } = this.props
+        const getMemberRefInRoom = (room, queryUserId) => {
+            return room.members.filter(roomMember => roomMember.user._id === queryUserId)
+        }
 
-
-    const getMemberRefInRoom = (room, queryUserId) => {
-        return room.members.filter(roomMember => roomMember.user._id === queryUserId)
-    }
-
-
-    if (currentUser) {
-        const currentUserRoomMemberReference = getMemberRefInRoom(room, currentUser._id)[0]
-        if (!currentUserRoomMemberReference) {
-            return getActionsForNonRoomMember()
-        } else {
-            return getActionsForRoomMember(currentUserRoomMemberReference, room.roomId)
+        if (currentUser) {
+            const currentUserRoomMemberReference = getMemberRefInRoom(room, currentUser._id)[0]
+            if (!currentUserRoomMemberReference) {
+                return this.getActionsForNonRoomMember()
+            } else {
+                return this.getActionsForRoomMember(currentUserRoomMemberReference, room.roomId)
+            }
         }
     }
-}
 
-const renderComponentTitle = (room) => {
-    return <Link to={`/rooms/${room.roomId}`}>{room.name}</Link>
-}
+    renderRoomMetaData() {
+        const { room } = this.props
+        const roomOwner = room.createdBy
+        const roomCreatedBy = roomOwner ? roomOwner.username : '<unknown>'
+        const roomCreatedAt = convertISODateToReadableString(room.createdAt)
 
-const renderComponentContent = (room, currentUser) => {
-    return (
-        <Row>
-            <Col className='d-flex align-items-center'>
-                <Card.Text>{renderRoomMetaData(room)}</Card.Text>
-            </Col>
-            <Col>
-                {renderRoomActions(room, currentUser)}
-            </Col>
-        </Row>
-    )
-}
+        return `Created ${roomCreatedAt} by ${roomCreatedBy}`
+    }
 
-const RoomListItemContainer = ({ room, currentUser }) => {
-    return (
-        <RoomListItem
-            title={renderComponentTitle(room)}
-            content={renderComponentContent(room, currentUser)}
-        />
-    )
+    renderComponentContent() {
+        return (
+            <Row>
+                <Col className='d-flex align-items-center'>
+                    <Card.Text>{this.renderRoomMetaData()}</Card.Text>
+                </Col>
+                <Col>
+                    {this.renderRoomActions()}
+                </Col>
+            </Row>
+        )
+    }
+
+    renderComponentTitle() {
+        const { room } = this.props
+        return <Link to={`/rooms/${room.roomId}`}>{room.name}</Link>
+    }
+
+    renderComponentFooter() {
+        const errMsg = this.state.joinRoomErrorMessage
+        if (!errMsg) {
+            return null
+        }
+        return (
+            <Row>
+                <Col style={{ color: 'red' }}>
+                    Room join failed {errMsg ? `:${errMsg}` : ''}
+                </Col>
+            </Row>
+        )
+    }
+
+    render() {
+        this.renderComponentFooter()
+        return (
+            <RoomListItem
+                title={this.renderComponentTitle()}
+                content={this.renderComponentContent()}
+                footer={this.renderComponentFooter()}
+            />
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
