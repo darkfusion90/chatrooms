9f619bd8774cfaded80c8cbe52166bc99f1038a9
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-08 19:41:45 +0530

    Actions moved to their own component

---
+++ client/src/containers/RoomListItemContainer/RoomListItemActionsContainer/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Button, ButtonGroup, Spinner } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
+import ButtonWithLeadingIcon from '../../../components/ButtonWithLeadingIcon'
+import { joinRoom } from '../../../server-communication/httpServer'
+
+class RoomListItemActionsContainer extends React.Component {
+    state = { joinRoomStatus: 'initial' }
+
+    determineJoinButtonMetadata() {
+        let icon, content, variant = 'primary';
+
+        if (this.state.joinRoomStatus === 'pending') {
+            icon = <Spinner animation='border' size='sm' />
+            content = 'Joining...'
+        } else if (this.state.joinRoomStatus === 'success') {
+            icon = <FontAwesomeIcon icon={faCheckCircle} />
+            content = 'Joined'
+            variant = 'success'
+        } else if (this.state.joinRoomStatus === 'fail') {
+            icon = <FontAwesomeIcon icon={faTimesCircle} />
+            content = 'Join Failed'
+            variant = 'danger'
+        }
+
+        return { icon, content, variant }
+    }
+
+    onJoinButtonClick = () => {
+        const onSuccess = () => {
+            this.setState({ joinRoomStatus: 'success' })
+        }
+
+        const onFailure = ({ response }) => {
+            this.setState({ joinRoomStatus: 'fail' })
+            this.props.onError(response.message)
+        }
+
+        const { roomId } = this.props.room
+        joinRoom(roomId, onSuccess, onFailure)
+        this.setState({ joinRoomStatus: 'pending' })
+    }
+
+    getActionsForRoomMember(roomMember, roomId) {
+        const adminActions = <Button variant='danger'>Delete</Button>
+
+        const isRoomAdmin = (roomMember) => {
+            return roomMember && roomMember.memberType === 'admin'
+        }
+
+        return (
+            <ButtonGroup>
+                <Link to={`/rooms/${roomId}`} component={Button}>Open</Link>
+                {this.props.isUserLoggedIn && isRoomAdmin(roomMember) ? adminActions : null}
+            </ButtonGroup>
+        )
+    }
+
+    getActionsForNonRoomMember() {
+        console.log('status: ', this.state.joinRoomStatus)
+        if (this.state.joinRoomStatus === 'initial') {
+            return <Button onClick={this.onJoinButtonClick}>Join</Button>
+        }
+
+        const { icon, content, variant } = this.determineJoinButtonMetadata()
+        const buttonProps = { variant, disabled: true }
+        return <ButtonWithLeadingIcon icon={icon} content={content} buttonProps={buttonProps} />
+    }
+
+    render() {
+        const { room, currentUser } = this.props
+        const getMemberRefInRoom = (room, queryUserId) => {
+            return room.members.filter(roomMember => roomMember.user._id === queryUserId)
+        }
+
+        if (currentUser) {
+            const currentUserRoomMemberReference = getMemberRefInRoom(room, currentUser._id)[0]
+            if (!currentUserRoomMemberReference) {
+                return this.getActionsForNonRoomMember()
+            } else {
+                return this.getActionsForRoomMember(currentUserRoomMemberReference, room.roomId)
+            }
+        }
+
+        return <div>Hello World!</div>;
+    }
+}
+
+const mapStateToProps = (state) => {
+    return { currentUser: state.user.user, isUserLoggedIn: state.user.isLoggedIn }
+}
+
+export default connect(mapStateToProps)(RoomListItemActionsContainer);
--- client/src/containers/RoomListItemContainer/index.js
+++ client/src/containers/RoomListItemContainer/index.js
@@ -1,98 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Button, ButtonGroup, Card, Row, Col, Spinner } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { Card, Row, Col } from 'react-bootstrap';
 
-import { joinRoom } from '../../server-communication/httpServer'
 import convertISODateToReadableString from '../../helpers/convertISODateToReadableString';
 import RoomListItem from '../../components/RoomListItem'
+import RoomListItemActionsContainer from './RoomListItemActionsContainer';
 
 class RoomListItemContainer extends React.Component {
-    state = { joinRoomStatus: 'initial', joinRoomErrorMessage: null }
-
-    onJoinButtonClick = () => {
-        const { roomId } = this.props.room
-        joinRoom(roomId,
-            () => this.setState({ joinRoomStatus: 'success' }),
-            ({ response }) => this.setState({ joinRoomStatus: 'fail', joinRoomErrorMessage: response.message })
-        )
-        this.setState({ joinRoomStatus: 'pending' })
-    }
-
-    disabledJoinButtonGridStyleTwoColsTemplate(leftCol, rightCol, variant) {
-        return (
-            <Button variant={variant} disabled={true}>
-                <Row>
-                    <Col>{leftCol}</Col>
-                    <Col style={{ paddingLeft: 0, whiteSpace: 'nowrap' }}>
-                        {rightCol}
-                    </Col>
-                </Row>
-            </Button>
-        )
-    }
-
-    determineJoinButtonMetadata() {
-        let leftCol, rightCol, btnVariant = 'primary';
-
-        if (this.state.joinRoomStatus === 'pending') {
-            leftCol = <Spinner animation='border' size='sm' />
-            rightCol = 'Joining...'
-        } else if (this.state.joinRoomStatus === 'success') {
-            leftCol = <FontAwesomeIcon icon={faCheckCircle} />
-            rightCol = 'Joined'
-            btnVariant = 'success'
-        } else if (this.state.joinRoomStatus === 'fail') {
-            leftCol = <FontAwesomeIcon icon={faTimesCircle} />
-            rightCol = 'Join Failed'
-            btnVariant = 'danger'
-        }
-
-        return { leftCol, rightCol, btnVariant }
-    }
-
-    getActionsForNonRoomMember() {
-        console.log('status: ', this.state.joinRoomStatus)
-        if (this.state.joinRoomStatus === 'initial') {
-            return <Button onClick={this.onJoinButtonClick}>Join</Button>
-        }
-
-        const { leftCol, rightCol, btnVariant } = this.determineJoinButtonMetadata()
-        return this.disabledJoinButtonGridStyleTwoColsTemplate(leftCol, rightCol, btnVariant)
-    }
-
-    getActionsForRoomMember(roomMember, roomId) {
-        const adminActions = <Button variant='danger'>Delete</Button>
-
-        const isRoomAdmin = (roomMember) => {
-            return roomMember && roomMember.memberType === 'admin'
-        }
-
-        return (
-            <ButtonGroup>
-                <Link to={`/rooms/${roomId}`} component={Button}>Open</Link>
-                {isRoomAdmin(roomMember) ? adminActions : null}
-            </ButtonGroup>
-        )
-    }
-
-    renderRoomActions() {
-        const { room, currentUser } = this.props
-        const getMemberRefInRoom = (room, queryUserId) => {
-            return room.members.filter(roomMember => roomMember.user._id === queryUserId)
-        }
-
-        if (currentUser) {
-            const currentUserRoomMemberReference = getMemberRefInRoom(room, currentUser._id)[0]
-            if (!currentUserRoomMemberReference) {
-                return this.getActionsForNonRoomMember()
-            } else {
-                return this.getActionsForRoomMember(currentUserRoomMemberReference, room.roomId)
-            }
-        }
-    }
+    state = { error: null, deleteModalShow: false }
 
     renderRoomMetaData() {
         const { room } = this.props
@@ -110,7 +25,10 @@ class RoomListItemContainer extends React.Component {
                     <Card.Text>{this.renderRoomMetaData()}</Card.Text>
                 </Col>
                 <Col>
-                    {this.renderRoomActions()}
+                    <RoomListItemActionsContainer
+                        room={this.props.room}
+                        onError={(error) => this.setState({ error })}
+                    />
                 </Col>
             </Row>
         )
@@ -122,21 +40,18 @@ class RoomListItemContainer extends React.Component {
     }
 
     renderComponentFooter() {
-        const errMsg = this.state.joinRoomErrorMessage
-        if (!errMsg) {
+        const { error } = this.state
+        if (!error) {
             return null
         }
         return (
             <Row>
-                <Col style={{ color: 'red' }}>
-                    Room join failed {errMsg ? `:${errMsg}` : ''}
-                </Col>
+                <Col style={{ color: 'red' }}>{error}</Col>
             </Row>
         )
     }
 
     render() {
-        this.renderComponentFooter()
         return (
             <RoomListItem
                 title={this.renderComponentTitle()}
@@ -147,8 +62,4 @@ class RoomListItemContainer extends React.Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return { currentUser: state.user.user }
-}
-
-export default connect(mapStateToProps)(RoomListItemContainer);
+export default RoomListItemContainer;
