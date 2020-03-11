706e566c7d35f077fdcf045c772e0ccb9133f2b1
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-11 18:04:10 +0530

    Message recieved as prop so fetching the same and state removed

--- client/src/containers/MessageContainer/index.js
+++ client/src/containers/MessageContainer/index.js
@@ -2,35 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 import Message from '../../components/Message'
-import { getRoomMessage } from '../../server-communication/httpServer'
-
 const logger = require('../../helpers/logger')('[MessageContainer]')
 
 class MessageContainer extends React.Component {
-    state = { message: null }
-
-    componentDidMount() {
-        const { roomId, messageId } = this.props
-        getRoomMessage(roomId, messageId, this.onMessageFetchSuccess, this.onMessageFetchFailure)
-    }
-
-    onMessageFetchSuccess = (response) => {
-        this.setState({ message: response.data })
-    }
-
-    onMessageFetchFailure = ({ response }) => {
-        logger.debug('Message fetch failure: ', response)
-    }
-
     isCurrentUserMessageAuthor = () => {
-        const { message } = this.state
-        logger.debug('message author: ', message ? message.author : '')
-        logger.debug('userid: ', this.props.userId)
+        const { message } = this.props
         return message && message.author === this.props.userId
     }
 
     render() {
-        const { message } = this.state
+        const { message } = this.props
         if (!message) {
             return null
         }
