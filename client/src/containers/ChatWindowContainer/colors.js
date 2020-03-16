e794cc6652a502b7cae0f48f988b28572021ddb0
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 07:59:14 +0530

    Assigns random color to each 'key' (here, userId)

---
+++ client/src/containers/ChatWindowContainer/colors.js
@@ -0,0 +1,40 @@
+const colors = [
+    '#fff07c',
+    '#80ff72',
+    '#7ee8fa',
+    '#eec0c6',
+    '#e58c8a'
+]
+
+const colorChoicesExhausted = (usedColors) => {
+    return colors.length === usedColors.length
+}
+
+const isColorUsed = (usedColors, randomColor) => {
+    return usedColors.indexOf(randomColor) !== -1
+}
+
+const getRandomColor = (usedColors) => {
+    let randomColor;
+    do {
+        let randomIndex = Math.floor(Math.random() * colors.length)
+        randomColor = colors[randomIndex]
+    } while (isColorUsed(usedColors, randomColor) && !colorChoicesExhausted(usedColors));
+    return randomColor
+}
+
+const assignRandomly = (keys) => {
+    const assignedKeys = {}
+    const usedColors = []
+    keys.forEach(key => {
+        if (!assignedKeys[key]) {
+            const randomColor = getRandomColor(usedColors)
+            assignedKeys[key] = randomColor
+            if (!colorChoicesExhausted(usedColors)) usedColors.push(randomColor)
+        }
+    })
+
+    return assignedKeys
+}
+
+export default assignRandomly
