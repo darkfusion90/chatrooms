0078450f2af52e9a82ed3283989e9a71e97e09ad
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 07:57:02 +0530

    Removed chatList ref and refactored to a functional component

--- client/src/components/Room/index.js
+++ client/src/components/Room/index.js
@@ -1,59 +1,31 @@
 import React from 'react';
-import { Container, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 import ChatMessageForm from '../../components/forms/ChatMessageForm';
-import MessageContainer from '../../containers/MessageContainer';
+import ChatWindowContainer from '../../containers/ChatWindowContainer'
 
-import './style.scss'
+import RoomInfoHeader from '../RoomInfoHeader';
 
-class Room extends React.Component {
-    constructor() {
-        super()
-        this.chatListRef = React.createRef()
-    }
+const Room = () => {
+    const { room, onSendMessageButtonClick } = this.props
+    return (
+        <div style={{ border: '1px solid red', marginTop: '1vh', height: '100%' }}>
+            <RoomInfoHeader room={room} />
+            <Container fluid className='d-flex flex-column justify-content-center' style={{ height: '100%' }}>
+                <Row>
+                    <Col>
+                        <ChatWindowContainer room={room} />
+                    </Col>
+                </Row>
 
-    componentDidUpdate() {
-        this.scrollChatListToBottom()
-    }
-
-    scrollChatListToBottom() {
-        const chatList = this.chatListRef.current
-        chatList.scrollTop = chatList.scrollHeight + 120
-    }
-
-    renderMessageList = (room) => {
-        if (!room) {
-            return null
-        }
-
-        return (
-            <ListGroup className='chat-container pre-scrollable' ref={this.chatListRef}>
-                {
-                    room.messages.map(message => {
-                        return <MessageContainer
-                            roomId={room.roomId}
-                            message={message}
-                            key={message && message._id}
-                        />
-                    })
-                }
-            </ListGroup>
-        )
-    }
-
-    render() {
-        const { room, onSendMessageButtonClick } = this.props
-        return (
-            <Container style={{ border: '1px solid red', marginTop: '1vh' }}>
-                <Container>
-                    <p>Hey! I am the chat container of room {room ? room.name : ''}</p>
-                    <br />
-                    {this.renderMessageList(room)}
-                </Container>
-                <ChatMessageForm onFormSubmit={onSendMessageButtonClick} />
+                <Row>
+                    <Col style={{ backgroundColor: '#ededed' }}>
+                        <ChatMessageForm onFormSubmit={onSendMessageButtonClick} />
+                    </Col>
+                </Row>
             </Container>
-        );
-    }
+        </div>
+    );
 }
 
 
