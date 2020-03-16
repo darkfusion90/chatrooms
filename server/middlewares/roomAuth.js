b595af10921ea604e20bb0462284f53925905993
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 14:51:33 +0530

    Middleware to authenticate requests to /api/rooms/*

---
+++ server/middlewares/roomAuth.js
@@ -0,0 +1,17 @@
+const { getRoomByRoomId } = require('../controllers/rooms')
+
+const ensureRoomNotPrivate = (room) => {
+
+}
+
+const middleware = async (req, res, next) => {
+    
+    switch (req.method) {
+        case 'GET':
+            
+            break;
+
+        default:
+            break;
+    }
+}
