6fc429edd846be31c84bb2f0d32903017287b8aa
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 21:08:54 +0530

    Component to display when no public rooms are available

---
+++ client/src/components/RoomList/EmptyRoomList/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+
+import CreateRoomButton from '../../CreateRoomButton'
+import JoinRoomButton from '../../JoinRoomButton'
+
+const EmptyRoomList = () => {
+    return (
+        <Container className='centered-content compensate-header'>
+            <h1 className='title'>There are no public rooms available</h1>
+            <Container style={{textAlign: 'center'}} className='subtitle'>
+                <span>You could</span>
+                <Container style={{ display: 'inline' }}>
+                    <CreateRoomButton size='sm' />
+                </Container>
+                <span>or</span>
+                <Container style={{ display: 'inline' }}>
+                    <JoinRoomButton label='Join a Room using ID' size='sm' />
+                </Container>
+            </Container>
+        </Container>
+    )
+}
+
+export default EmptyRoomList
