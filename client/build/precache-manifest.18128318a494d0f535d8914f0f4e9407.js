3cda19be4562a58f03c4d983ee1868eecb3f88fe
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 09:27:47 +0530

    New react build

---
+++ client/build/precache-manifest.18128318a494d0f535d8914f0f4e9407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf935d836107fc406c40610084b44c34",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f75eb3be0c1f410efb5",
+    "url": "./static/css/main.26707cfd.chunk.css"
+  },
+  {
+    "revision": "98ba10edc18a9b2dab55",
+    "url": "./static/js/2.bf6f3b14.chunk.js"
+  },
+  {
+    "revision": "83f62cbe13b567104de8849cf7df32be",
+    "url": "./static/js/2.bf6f3b14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f75eb3be0c1f410efb5",
+    "url": "./static/js/main.cc9be7d1.chunk.js"
+  },
+  {
+    "revision": "489c207e28bb5e666801",
+    "url": "./static/js/runtime-main.bdb4c24e.js"
+  },
+  {
+    "revision": "2527e1097e242943be2b8aa0962c43a2",
+    "url": "./static/media/input-loading.2527e109.svg"
+  }
+]);
