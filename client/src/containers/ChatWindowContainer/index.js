1a550b5faeb8e7428bb7a559c0c28efad61a8ba9
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 08:00:17 +0530

    Removed hello world boilerplate. Render actual content

--- client/src/containers/ChatWindowContainer/index.js
+++ client/src/containers/ChatWindowContainer/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+
+import MessageContainer from '../../containers/MessageContainer';
+import assignRandomColors from './colors'
+import './style.scss';
 
 class ChatWindowContainer extends React.Component {
-    render(){
-        return <div>Hello World!</div>;
+    state = { randomAuthorColors: [] }
+
+    componentDidMount() {
+        const { room } = this.props
+        if (room) {
+            this.setState({
+                randomAuthorColors: this.assignRandomColorsToMessageAuthors(room)
+            })
+        }
+    }
+
+    assignRandomColorsToMessageAuthors(room) {
+        const messageAuthors = room.messages.map(message => message.author && message.author._id)
+        return assignRandomColors(messageAuthors)
+    }
+
+    render() {
+        const { room } = this.props
+
+        if (!room) {
+            return null
+        }
+
+        return (
+            <ListGroup className='chat-container pre-scrollable' variant="flush">
+                {
+                    room.messages.map(message => {
+                        const messageAuthorColor = this.state.randomAuthorColors[message.author && message.author._id]
+                        return <MessageContainer
+                            color={messageAuthorColor}
+                            roomId={room.roomId}
+                            message={message}
+                            key={message && message._id}
+                        />
+                    })
+                }
+            </ListGroup>
+        )
     }
 }
 
