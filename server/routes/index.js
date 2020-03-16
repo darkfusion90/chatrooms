a28b55f94c911f862985629bd7f3ab658f527291
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 08:11:48 +0530

    Dirname to log

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,7 +10,7 @@ const logger = require('../utils/logger')('[Routes: index]')
 
 const index = (_, res) => {
     if (process.env.NODE_ENV === 'production') {
-       logger.debug('Production build')
+        logger.debug('Production build , dirname: ', __dirname)
         res.sendFile(path.join(__dirname, '../client/build/index.html'))
     } else {
         logger.debug('Development build')
