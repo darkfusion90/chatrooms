3d2af4723587b4534059b60fed284db32fcfa4c1
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 08:02:11 +0530

    Added logs

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,14 @@ const register = require('./register')
 const rooms = require('./rooms')
 const user = require('./user')
 
+const logger = require('../utils/logger')('[Routes: index]')
+
 const index = (_, res) => {
     if (process.env.NODE_ENV === 'production') {
+       logger.debug('Production build')
         res.sendFile(path.join(__dirname, '../client/build/index.html'))
     } else {
+        logger.debug('Development build')
         res.send('Hello from Development Server!')
     }
 }
