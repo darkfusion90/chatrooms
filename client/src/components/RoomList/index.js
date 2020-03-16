14d6387c24cf898072c2f1223b9a20ae53343474
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 21:09:22 +0530

    Added empty room list implementation

--- client/src/components/RoomList/index.js
+++ client/src/components/RoomList/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import RoomListItemContainer from '../../containers/RoomListItemContainer'
 import ListGroup from 'react-bootstrap/ListGroup'
 
-const renderRoomList = (props) => {
-    return props.rooms.map((room) => <RoomListItemContainer room={room} key={room.roomId}/>)
+import RoomListItemContainer from '../../containers/RoomListItemContainer'
+import EmptyRoomList from './EmptyRoomList'
+
+const renderRoomList = (rooms) => {
+    return rooms.map((room) => <RoomListItemContainer room={room} key={room.roomId} />)
 }
 
-const RoomList = (props) => {
-    return <ListGroup variant='flush'>{renderRoomList(props)}</ListGroup>
+const RoomList = ({ rooms }) => {
+    if (!rooms || rooms.length === 0) {
+        return <EmptyRoomList />
+    }
+
+    return <ListGroup variant='flush' style={{marginLeft: 0, marginRight: 0}}>{renderRoomList(rooms)}</ListGroup>
 }
 
 export default RoomList;
