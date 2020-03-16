48bd584a7b7962f1767a9f80472d4763a59e1f3b
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-16 07:58:01 +0530

    Component displayed on top of room container having room info

---
+++ client/src/components/RoomInfoHeader/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt, faPlus} from '@fortawesome/free-solid-svg-icons'
+import { Container, Col, Row } from 'react-bootstrap/';
+
+import './style.scss'
+
+const RoomInfoHeader = ({ room }) => {
+    if (!room) return null
+    console.log('room: ', room)
+    return (
+        <Container fluid>
+            <Row style={{ backgroundColor: '#ededed', height: '3.5vw' }}>
+                <Col>
+                    <span className='room-name'><strong>{room.name}</strong></span>
+                </Col>
+                <Col sm={2} lg={2} md={2} xl={2}>
+                    <Row>
+                        <Col sm={4} lg={4} md={4} xl={4} className="col-4">
+                            <FontAwesomeIcon icon={faSignOutAlt} />
+                        </Col>
+                        <Col sm={4} lg={4} md={4} xl={4} className="col-4">
+                            <FontAwesomeIcon icon={faPlus} />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default RoomInfoHeader
