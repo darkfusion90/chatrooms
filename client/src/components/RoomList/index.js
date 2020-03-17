9ef68f159506428baf3348297223c854ac025dfe
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-17 08:33:11 +0530

    Added RoomListControlsHeader

--- client/src/components/RoomList/index.js
+++ client/src/components/RoomList/index.js
@@ -3,17 +3,25 @@ import ListGroup from 'react-bootstrap/ListGroup'
 
 import RoomListItemContainer from '../../containers/RoomListItemContainer'
 import EmptyRoomList from './EmptyRoomList'
+import RoomListControlsHeader from './RoomListControlsHeader'
 
 const renderRoomList = (rooms) => {
     return rooms.map((room) => <RoomListItemContainer room={room} key={room.roomId} />)
 }
 
-const RoomList = ({ rooms }) => {
+const RoomList = ({ rooms, onRoomControlsChange }) => {
     if (!rooms || rooms.length === 0) {
         return <EmptyRoomList />
     }
 
-    return <ListGroup variant='flush' style={{marginLeft: 0, marginRight: 0}}>{renderRoomList(rooms)}</ListGroup>
+    return (
+        <>
+            <RoomListControlsHeader onChange={onRoomControlsChange} />
+            <ListGroup variant='flush' style={{ marginLeft: 0, marginRight: 0 }}>
+                {renderRoomList(rooms)}
+            </ListGroup>
+        </>
+    )
 }
 
 export default RoomList;
