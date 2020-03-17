149bf0971864df76e6e9739958d93c203436f275
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-17 14:15:20 +0530

    Added controls header Removed component level state for sort technique. Moved to redux

--- client/src/containers/RoomListContainer/index.js
+++ client/src/containers/RoomListContainer/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 import RoomList from '../../components/RoomList';
+import RoomListControlsHeader from './RoomListControlsHeader'
 import fetchPublicRooms from '../../actions/fetchPublicRooms'
-import { byName } from './sort'
+import setRoomListSortTechnique from '../../actions/setRoomListSortTechnique'
+import { byName, byDate } from './sort'
 
 import loggerInit from '../../helpers/logger'
 
 const logger = loggerInit('[RoomListContainer] ')
 
 class RoomListContainer extends React.Component {
-    state = { sortRoomListBy: 'name' }
-
     componentDidMount() {
         this.props.fetchPublicRooms(this.onRoomFetchFail)
     }
@@ -21,26 +21,42 @@ class RoomListContainer extends React.Component {
     }
 
     sortRoomList = () => {
-        const { rooms } = this.props
+        const { rooms, roomListSortTechnique } = this.props
         if (!rooms) {
             return []
         }
 
-        switch (this.state.sortRoomListBy) {
-            case 'name':
-                return rooms.sort(byName)
+        switch (roomListSortTechnique) {
             default:
-                return rooms
+            case 'name-asc':
+                return rooms.sort(byName)
+            case 'name-des':
+                return rooms.sort(byName).reverse()
+            case 'date-asc':
+                return rooms.sort(byDate)
+            case 'date-des':
+                return rooms.sort(byDate).reverse()
         }
     }
 
+    onSortFormSubmit = (formValues) => {
+        this.props.setRoomListSortTechnique(formValues.sortBy)
+    }
+
     render() {
-        return <RoomList rooms={this.sortRoomList()} />;
+        const { rooms } = this.props
+        return (
+            <>
+                {rooms && rooms.length !== 0 &&
+                    <RoomListControlsHeader onSortFormSubmit={this.onSortFormSubmit} />}
+                <RoomList rooms={this.sortRoomList()} />
+            </>
+        )
     }
 }
 
 const mapStateToProps = (state) => {
-    return { rooms: state.rooms }
+    return { rooms: state.rooms, roomListSortTechnique: state.roomListSortTechnique }
 }
 
-export default connect(mapStateToProps, { fetchPublicRooms })(RoomListContainer);
+export default connect(mapStateToProps, { fetchPublicRooms, setRoomListSortTechnique })(RoomListContainer);
