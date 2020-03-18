4fc338a1aca0d6ea7b913004964a7a8c30830c46
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-18 14:22:07 +0530

    Removed gray-ish bgcolor

--- client/src/components/RoomListItem/index.js
+++ client/src/components/RoomListItem/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Card, ListGroupItem } from 'react-bootstrap';
 
 const RoomListItem = ({ title, content, footer, key }) => {
-    const style = { backgroundColor: '#ededed' }
-
     return (
         <ListGroupItem key={key} className='px-0'>
-            <Card className='border-0 mx-0 px-0' style={style}>
+            <Card className='border-0 mx-0 px-0'>
                 <Card.Body>
                     <Card.Title>
                         {title}
