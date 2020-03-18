fde625f9b00a073a3b67cb3bb47c4f0348784a8a
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-18 14:11:01 +0530

    Modified POST auth. Added DELETE auth

--- server/middlewares/roomAuth/room.members.js
+++ server/middlewares/roomAuth/room.members.js
@@ -1,20 +1,49 @@
 const { getRoomByRoomId } = require('../../controllers/rooms')
-const { ensureIsRoomMember, ensureIsRoomAdmin } = require('./helper')
+const { ensureIsRoomMember, ensureIsRoomAdmin, isPrivateRoom, roomAuthFailedError } = require('./helper')
+
+const handleDeleteRoomMemberAuth = (room, memberId, userId, next) => {
+    const isUserAttemptingSelfDelete = () => {
+        let memberDocOfUser;
+        room.members.forEach(member => {
+            if (member && member.user && member.user._id.equals(userId)) {
+                memberDocOfUser = member
+            }
+        })
+
+        return memberDocOfUser && memberDocOfUser._id.equals(memberId)
+    }
+
+    if (isUserAttemptingSelfDelete()) {
+        return next()
+    }
+
+    ensureIsRoomAdmin(userId, room, next)
+}
+
+const handlePostRoomMemberAuth = (room, next) => {
+    if (isPrivateRoom(room)) {
+        next(roomAuthFailedError)
+    } else {
+        next()
+    }
+}
 
 const middleware = (req, _, next) => {
     const { userId } = req.session
+    const { roomId, memberId } = req.params
 
-    getRoomByRoomId(req.params.roomId).then(room => {
+    getRoomByRoomId(roomId).then(room => {
         if (!room) {
             return next()
         }
-        console.log(room.name)
 
         switch (req.method) {
             case 'GET':
                 return ensureIsRoomMember(userId, room, next)
             case 'POST':
-                return ensureIsRoomAdmin(userId, room, next)
+                return handlePostRoomMemberAuth(room, next)
+            case 'DELETE':
+                return handleDeleteRoomMemberAuth(room, memberId, userId, next)
             default:
                 next()
         }
