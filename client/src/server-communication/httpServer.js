384b14f5fedc261131d44fd875db3c8757124ff5
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-18 14:14:47 +0530

    Added method: leaveRoom

--- client/src/server-communication/httpServer.js
+++ client/src/server-communication/httpServer.js
@@ -50,6 +50,10 @@ export const joinRoom = (roomId, onRequestFulfilled, onRequestRejected) => {
     return axios.post(`/api/rooms/${roomId}/members`).then(onRequestFulfilled, onRequestRejected)
 }
 
+export const leaveRoom = (roomId, memberId, onRequestFulfilled, onRequestRejected) => {
+    return axios.delete(`/api/rooms/${roomId}/members/${memberId}`).then(onRequestFulfilled, onRequestRejected)
+}
+
 export const getRoomMessage = (roomId, messageId, onRequestFulfilled, onRequestRejected) => {
     return axios.get(`/api/rooms/${roomId}/messages/${messageId}`).then(onRequestFulfilled, onRequestRejected)
 }
