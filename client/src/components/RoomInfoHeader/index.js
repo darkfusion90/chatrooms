06dcb32df52676afe17287becbe4cd6730e1c6ac
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-18 14:23:29 +0530

    Styling changed from bootstrap grid -> custom Flex Added icons' tooltip and actions

--- client/src/components/RoomInfoHeader/index.js
+++ client/src/components/RoomInfoHeader/index.js
@@ -1,33 +1,55 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSignOutAlt, faPlus} from '@fortawesome/free-solid-svg-icons'
-import { Container, Col, Row } from 'react-bootstrap/';
+import { faSignOutAlt, faPlus, faWindowClose } from '@fortawesome/free-solid-svg-icons'
+import { Container } from 'react-bootstrap/';
 
+import { withTooltip } from '../Tooltip'
+import LinkIcon from '../LinkIcon'
+import showModalAction from '../../actions/showModal'
 import './style.scss'
 
-const RoomInfoHeader = ({ room }) => {
+const RoomInfoHeader = ({ room, className, showModal }) => {
     if (!room) return null
-    console.log('room: ', room)
+
+    const onLeaveRoomIconClick = () => {
+        showModal('LeaveRoom', { room })
+    }
+
+    const renderRoomActionIcons = () => {
+        const renderIcon = ({ icon, id, onClick }) => {
+            if (id === 'tooltip-close-room') {
+                return <LinkIcon to='/rooms' icon={icon} replace key={id} className='cursor-pointer' />
+            }
+            return <FontAwesomeIcon icon={icon} key={id} className='cursor-pointer' onClick={onClick} />
+        }
+
+        return [
+            { icon: faSignOutAlt, label: 'Leave Room', id: 'tooltip-leave-room', onClick: onLeaveRoomIconClick },
+            { icon: faPlus, label: 'Invite User', id: 'tooltip-invite-room' },
+            { icon: faWindowClose, label: 'Close Room Window', id: 'tooltip-close-room' }
+        ].map(({ icon, label, id, onClick }) => {
+            return withTooltip(
+                renderIcon({ icon, id, onClick }),
+                { label, id }
+            )
+        })
+    }
+
     return (
-        <Container fluid>
-            <Row style={{ backgroundColor: '#ededed', height: '40px' }}>
-                <Col>
-                    <span className='room-name'><strong>{room.name}</strong></span>
-                </Col>
-                <Col sm={2} lg={2} md={2} xl={2}>
-                    <Row>
-                        <Col sm={4} lg={4} md={4} xl={4} className="col-4">
-                            <FontAwesomeIcon icon={faSignOutAlt} />
-                        </Col>
-                        <Col sm={4} lg={4} md={4} xl={4} className="col-4">
-                            <FontAwesomeIcon icon={faPlus} />
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
+        <Container fluid className={`room-page-sub-header ${className}`}>
+            <div className='d-flex align-items-center h-100'>
+                <div className='room-name-container'>
+                    <strong>{room.name}</strong>
+                </div>
+                <div className='room-actions-container'>
+                    <div className='d-flex justify-content-around'>
+                        {renderRoomActionIcons()}
+                    </div>
+                </div>
+            </div>
         </Container>
     )
 }
 
-export default RoomInfoHeader
+export default connect(null, { showModal: showModalAction })(RoomInfoHeader)
