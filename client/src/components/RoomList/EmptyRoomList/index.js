711dfda2fcb3cb358c2e3ac16affec046e4b8f10
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-20 06:57:25 +0530

    Removed non-existent components and respective imports

--- client/src/components/RoomList/EmptyRoomList/index.js
+++ client/src/components/RoomList/EmptyRoomList/index.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 
-import CreateRoomButton from '../../CreateRoomButton'
-import JoinRoomButton from '../../JoinRoomButton'
-
 const EmptyRoomList = () => {
     return (
         <Container className='centered-content compensate-header'>
             <h1 className='title'>There are no public rooms available</h1>
-            <Container style={{textAlign: 'center'}} className='subtitle'>
+            <Container style={{ textAlign: 'center' }} className='subtitle'>
                 <span>You could</span>
                 <Container style={{ display: 'inline' }}>
-                    <CreateRoomButton size='sm' variant='outline-primary'/>
                 </Container>
                 <span>or</span>
                 <Container style={{ display: 'inline' }}>
-                    <JoinRoomButton label='Join a Room using ID' size='sm' variant='outline-primary'/>
                 </Container>
             </Container>
         </Container>
