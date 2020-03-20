84d3573a4bddbc5f0d552737ce000fadbf82f806
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-20 14:47:21 +0530

    Added standalone component: Dropdown

--- client/src/components-beta/standalone/index.js
+++ client/src/components-beta/standalone/index.js
@@ -1,4 +1,5 @@
+import Dropdown from './Dropdown'
 import TooltipWrapper from './TooltipWrapper'
 import TextInputWordWrap from './TextInputWordWrap'
 
-export { TooltipWrapper, TextInputWordWrap }
+export { Dropdown, TextInputWordWrap, TooltipWrapper }
