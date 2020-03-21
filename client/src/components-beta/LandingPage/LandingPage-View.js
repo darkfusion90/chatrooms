942c8671b11c918046690cbaf141700c8aede51c
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-21 15:44:46 +0530

    LandingPage re-structured

---
+++ client/src/components-beta/LandingPage/LandingPage-View.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+
+import ModalTrigger from '../modals/ModalTrigger'
+import './LandingPage-Style.scss';
+
+
+const LandingPage = () => {
+    return (
+        <div className="landing-page compensate-header">
+            <div className="inner-wrapper">
+                <p>Welcome to ChatRooms!</p>
+
+                <div className="buttons-container">
+                    <ModalTrigger
+                        component={Button}
+                        content='Create Room'
+                        modalName='CreateRoom'
+                    />
+                    <ModalTrigger
+                        component={Button}
+                        componentProps={{ variant: 'outline-primary' }}
+                        content='Join Room By Id'
+                        modalName='JoinRoom'
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
---
+++ client/src/components-beta/LandingPage/index.js
@@ -0,0 +1,3 @@
+import LandingPage from './LandingPage-View'
+
+export default LandingPage
