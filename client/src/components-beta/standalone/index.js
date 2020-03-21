d570a1a100b3eb84e4e528fef661b57c861f2612
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-21 15:25:10 +0530

    Added ProgressButton

--- client/src/components-beta/standalone/index.js
+++ client/src/components-beta/standalone/index.js
@@ -3,5 +3,6 @@ import Dropdown from './Dropdown'
 import TooltipWrapper from './TooltipWrapper'
 import TextInputWordWrap from './TextInputWordWrap'
 import ToggleablePasswordField from './ToggleablePasswordField'
+import ProgressButton from './ProgressButton'
 
-export { DismissibleAlert, Dropdown, TextInputWordWrap, ToggleablePasswordField, TooltipWrapper }
+export { DismissibleAlert, Dropdown, TextInputWordWrap, ToggleablePasswordField, TooltipWrapper, ProgressButton }
