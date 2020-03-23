e24977645e682f28b43cf18da6cd8e10b6aa40f5
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-23 16:27:20 +0530

    Action creator to join a room

--- client/src/redux/actions/room-actions/joinRoom.js
+++ client/src/redux/actions/room-actions/joinRoom.js
@@ -1,5 +1,9 @@
-export default (roomId, onSuccess, onFailure) => {
-    return {
-        type: 'bah'
-    }
+import { joinRoom } from '../../../server-communication/httpServer'
+import { JOIN_ROOM } from '../../action-constants'
+
+export default (roomId, onSuccess, onFailure) => dispatch => {
+    joinRoom(roomId).then(({ data }) => {
+        dispatch({ type: JOIN_ROOM, payload: { roomId, updatedMembers: data.members } })
+        onSuccess()
+    }).catch(onFailure)
 }
