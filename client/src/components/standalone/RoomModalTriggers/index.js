81e4653b00fa87523a2064c71924b72b29ff1268
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-23 16:28:18 +0530

    Simple modal triggers for each possible room action

---
+++ client/src/components/standalone/RoomModalTriggers/index.js
@@ -0,0 +1,11 @@
+import CreateRoomModalTrigger from './CreateRoomModalTrigger'
+import JoinRoomModalTrigger from './JoinRoomModalTrigger'
+import DeleteRoomModalTrigger from './DeleteRoomModalTrigger'
+import LeaveRoomModalTrigger from './LeaveRoomModalTrigger'
+
+export {
+    CreateRoomModalTrigger,
+    JoinRoomModalTrigger,
+    DeleteRoomModalTrigger,
+    LeaveRoomModalTrigger
+}
