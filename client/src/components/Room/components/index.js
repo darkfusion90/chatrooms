1ae784cc39f431d1ea7a144f9203f1b4d89c6782
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-24 13:52:40 +0530

    Index added for RoomNotJoined component

--- client/src/components/Room/components/index.js
+++ client/src/components/Room/components/index.js
@@ -2,5 +2,6 @@ import ChatMessageForm from './ChatMessageForm'
 import ChatWindow from './ChatWindow'
 import RoomHeader from './RoomHeader'
 import Message from './Message'
+import RoomNotJoined from './RoomNotJoined'
 
-export { ChatMessageForm, ChatWindow, RoomHeader, Message }
+export { ChatMessageForm, ChatWindow, RoomHeader, Message, RoomNotJoined }
