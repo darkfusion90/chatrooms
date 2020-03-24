2373ede3556ac3b3f6e57bfc60d3dcf4779a0ca3
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-24 13:46:14 +0530

    Using helper Room action buttons, added joinRoom action and functionality

--- client/src/components/RoomList/components/RoomListItemActions/RoomListItemActions-View.js
+++ client/src/components/RoomList/components/RoomListItemActions/RoomListItemActions-View.js
@@ -1,39 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import { ButtonGroup } from 'react-bootstrap';
 
-import { Button, ButtonGroup } from 'react-bootstrap';
-import { WithModalTrigger, WithLink } from '../../../hoc'
+import { DeleteRoomModalTrigger } from '../../../standalone/RoomModalTriggers'
+import { JoinRoomButton } from '../../../standalone/RoomActionButtons'
+
+const getJoinRoomAction = (joinRoomProgress, onClick) => {
+    return <JoinRoomButton progress={joinRoomProgress} onClick={onClick} />
+}
+
+const getDeleteRoomAction = (room) => {
+    return <DeleteRoomModalTrigger room={room}>Delete</DeleteRoomModalTrigger>
+}
+
+const getOpenRoomAction = (room) => {
+    const roomId = room.roomId ? room.roomId : ''
+    return (
+        <Link to={`/rooms/${roomId}`} className='btn btn-outline-primary'>
+            Open
+        </Link>
+    )
+}
 
 const RoomListItemActions = (props) => {
-    const { room, isCurrentUserRoomMember, isCurrentUserRoomAdmin } = props
+    const { room, isCurrentUserRoomMember, isCurrentUserRoomAdmin, ...restProps } = props
 
     const getActionsForRoomMember = () => {
-        const ActionDeleteRoom = (
-            <WithModalTrigger
-                modalName='DeleteRoom'
-                modalProps={{ room }}
-                component={Button}
-                componentProps={{ variant: 'danger' }}
-                content='Delete Room'
-            />
-        )
-
-        const ActionOpenRoom = (
-            <WithLink
-                to={`/test/rooms/${room.roomId}`}
-                component={Button}
-            >Open Room</WithLink>
-        )
-
         return (
             <ButtonGroup>
-                {ActionOpenRoom}
-                {isCurrentUserRoomAdmin ? ActionDeleteRoom : null}
+                {getOpenRoomAction(room)}
+                {isCurrentUserRoomAdmin ? getDeleteRoomAction(room) : null}
             </ButtonGroup>
         )
     }
 
     const getActionsForNonRoomMember = () => {
-        return <div className='btn btn-outline-primary'>Join Room</div>
+        const { joinRoomProgress, onJoinRoomButtonClick } = restProps
+        const onClick = () => onJoinRoomButtonClick(room ? room.roomId : '')
+        return getJoinRoomAction(joinRoomProgress, onClick)
     }
 
     return isCurrentUserRoomMember ? getActionsForRoomMember() : getActionsForNonRoomMember()
