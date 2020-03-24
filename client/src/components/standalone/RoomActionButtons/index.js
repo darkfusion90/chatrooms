419a7e71ac1737e819416a9f9e71b82347768f8f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-24 13:27:38 +0530

    Index added for less noisy imports by other component(s)

---
+++ client/src/components/standalone/RoomActionButtons/index.js
@@ -0,0 +1,6 @@
+import CreateRoomButton from './CreateRoomButton'
+import DeleteRoomButton from './DeleteRoomButton'
+import JoinRoomButton from './JoinRoomButton'
+import LeaveRoomButton from './LeaveRoomButton'
+
+export { CreateRoomButton, DeleteRoomButton, JoinRoomButton, LeaveRoomButton }
