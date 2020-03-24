a1739b6fcd585e7b0c8f1b2e50eea35022429461
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-24 13:29:29 +0530

    Index added for component ProgressButton

---
+++ client/src/components/standalone/ProgressButton/index.js
@@ -0,0 +1,14 @@
+import ProgressButton, {
+    PROGRESS_INITIAL,
+    PROGRESS_PENDING,
+    PROGRESS_SUCCESS,
+    PROGRESS_FAIL
+} from './ProgressButton'
+
+export {
+    PROGRESS_INITIAL,
+    PROGRESS_PENDING,
+    PROGRESS_SUCCESS,
+    PROGRESS_FAIL
+}
+export default ProgressButton
