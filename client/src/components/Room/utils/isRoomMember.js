bdb86777cbdf38deb3a4a1b54ddac70bf296a11a
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-24 13:48:57 +0530

    Util to check if a user is room member of a certain room

---
+++ client/src/components/Room/utils/isRoomMember.js
@@ -0,0 +1,12 @@
+export default (room, queryUserId) => {
+    if (!room || !room.members || !queryUserId) {
+        return false
+    }
+
+    const memberDocIndex = room.members.findIndex(member => {
+        console.log(member)
+        return member.user && member.user._id === queryUserId
+    })
+
+    return memberDocIndex !== -1
+}
