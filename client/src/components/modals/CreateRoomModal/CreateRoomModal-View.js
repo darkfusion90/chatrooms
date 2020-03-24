906e1295db150b047e0354295a148b5c23b77119
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-24 13:42:56 +0530

    Using CreateRoomButton and removed alert from modal

--- client/src/components/modals/CreateRoomModal/CreateRoomModal-View.js
+++ client/src/components/modals/CreateRoomModal/CreateRoomModal-View.js
@@ -4,62 +4,38 @@ import Button from 'react-bootstrap/Button';
 import GenericModal from '../GenericModal';
 import { CreateRoomForm } from './components'
 
-import { DismissibleAlert } from '../../standalone'
+import { CreateRoomButton } from '../../standalone/RoomActionButtons'
 
 const CreateRoomModalView = ({
     isModalVisible,
-    alertProps,
-    hideAlert,
     hideModal,
     createRoomFormData,
+    createRoomProgress,
     onFormSubmit
 }) => {
-    const hideAlertAndModal = () => {
-        hideAlert()
-        hideModal()
-    }
-
     const getModalActions = () => {
         const hasErrors = createRoomFormData && createRoomFormData.syncErrors
         return (
             <>
-                <Button variant='secondary' onClick={hideAlertAndModal}>Cancel</Button>
-                <Button
+                <Button variant='secondary' onClick={hideModal}>Cancel</Button>
+                <CreateRoomButton
+                    progress={createRoomProgress}
                     form='create-room-form'
                     type='submit'
                     disabled={hasErrors}
                     className={`cursor-${hasErrors ? 'not-allowed' : 'pointer'}`}
-                >CreateRoom</Button>
+                />
             </>
         );
     }
 
     const getModalContent = () => {
-        const getAlertDismissAction = () => {
-            if (alertProps.show && alertProps.variant === 'success') {
-                return (
-                    <Button variant='outline-success' onClick={hideAlertAndModal}>
-                        Back to home
-                    </Button>
-                )
-            }
-        }
-
-        return (
-            <>
-                <DismissibleAlert
-                    {...alertProps}
-                    onDismiss={hideAlert}
-                    dismissAction={getAlertDismissAction}
-                />
-                <CreateRoomForm onFormSubmit={onFormSubmit} />
-            </>
-        )
+        return <CreateRoomForm onFormSubmit={onFormSubmit} />
     }
-
+    
     return (
         <GenericModal
-            header="Create Room"
+            header='Create Room'
             actions={getModalActions()}
             content={getModalContent()}
             visible={isModalVisible}
