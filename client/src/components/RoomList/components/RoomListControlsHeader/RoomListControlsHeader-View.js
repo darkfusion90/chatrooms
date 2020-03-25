3c93fc338531c84452a470e6de24caec9bc06b6e
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-25 16:57:52 +0530

    Sorting logic moved to an independent component

--- client/src/components/RoomList/components/RoomListControlsHeader/RoomListControlsHeader-View.js
+++ client/src/components/RoomList/components/RoomListControlsHeader/RoomListControlsHeader-View.js
@@ -1,48 +1,14 @@
 import React from 'react';
 import { Container } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSort } from '@fortawesome/free-solid-svg-icons'
 
-import { Dropdown } from '../../../standalone'
-import RoomListControlsForm from '../RoomListControlsForm'
+import SortRoomDropdownButton from './SortRoomDropdownButton'
 
-class RoomListControlsHeader extends React.Component {
-    hideDropdown = () => {
-        //Since rootClose is set to true in OverlayTrigger in Dropdown,
-        //clicking anywhere on body dismisses the dropdown
-        document.body.click()
-    }
-
-    getTriggerComponent() {
-        return (
-            <div className='d-inline cursor-pointer'>
-                <FontAwesomeIcon icon={faSort} />
-                <span style={{ marginLeft: '0.5vw' }}>Sort By</span>
-            </div>
-        )
-    }
-
-    getDropdownMenu() {
-        const onFormSubmit = (formValues) => {
-            document.body.click()
-            this.props.onSortFormSubmit(formValues)
-        }
-        return <RoomListControlsForm onFormSubmit={onFormSubmit} />
-    }
-
-    render() {
-        const dropdownProps = {
-            triggerComponent: this.getTriggerComponent(),
-            menu: this.getDropdownMenu(),
-            show: this.props.showDropdown
-        }
-
-        return (
-            <Container fluid className='room-page-sub-header'>
-                <Dropdown {...dropdownProps} />
-            </Container>
-        )
-    }
+const RoomListControlsHeader = ({ onSortFormSubmit }) => {
+    return (
+        <Container fluid className='room-page-sub-header d-flex align-items-center'>
+            <SortRoomDropdownButton onSortFormSubmit={onSortFormSubmit} />
+        </Container>
+    )
 }
 
 export default RoomListControlsHeader;
