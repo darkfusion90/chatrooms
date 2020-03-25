feaf0de1c3b5198918f24328c4405b36885d136b
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-25 17:01:57 +0530

    Added dispatch: fetchRoom Added: new mapStateToProps - room corresponding to roomId in ownProps

--- client/src/components/Room/Room-Redux.js
+++ client/src/components/Room/Room-Redux.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 import RoomContainer from './Room-Container'
+import { fetchRoom } from '../../redux/actions/room-actions'
 
 const RoomRedux = (props) => {
     return <RoomContainer {...props} />;
 }
 
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
     return {
-        currentUser: state.user && state.user.user
+        currentUser: state.user && state.user.user,
+        room: state.rooms[ownProps.roomId]
     }
 }
 
-export default connect(mapStateToProps)(RoomRedux)
+export default connect(mapStateToProps, { fetchRoom })(RoomRedux)
