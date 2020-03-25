85114543baa5c5b3bab3e353e9cc7e4e8d21d473
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-25 16:54:38 +0530

    Added custom field level validator and an "invalid" feedback

--- client/src/components/modals/JoinRoomModal/components/JoinRoomForm/JoinRoomForm-View.js
+++ client/src/components/modals/JoinRoomModal/components/JoinRoomForm/JoinRoomForm-View.js
@@ -1,25 +1,39 @@
 import React from 'react'
+import isEmpty from 'is-empty'
 import { Field } from 'redux-form'
 import { Form, FormControl, FormGroup, FormLabel } from 'react-bootstrap'
 
-const renderJoinRoomField = ({ input, label, ...formProps }) => {
+const renderJoinRoomField = ({ input, label, meta, ...formProps }) => {
+    const { touched, error, pristine } = meta
+    const hasErrors = touched && error
+
     return (
         <FormGroup controlId='join-room'>
             <FormLabel>{label}</FormLabel>
-            <FormControl {...input} {...formProps} />
+            <FormControl
+                isValid={(!pristine && !hasErrors)}
+                isInvalid={hasErrors}
+                {...input}
+                {...formProps}
+            />
+            <FormControl.Feedback type='invalid'>{error}</FormControl.Feedback>
         </FormGroup>
     )
 }
 
+const ensureNotEmpty = value => {
+    return isEmpty(value) ? 'Room Id is required' : undefined
+}
+
 const JoinRoomFormView = ({ onFormSubmit }) => {
     return (
-        <Form id='join-room-form' onSubmit={onFormSubmit} noValidate>
+        <Form id='join-room-form' onSubmit={onFormSubmit} >
             <Field
                 component={renderJoinRoomField}
-                name='join-room'
+                name='roomId'
                 label='Enter Room Id'
                 placeholder='X-X-X-X'
-                required
+                validate={ensureNotEmpty}
             />
         </Form>
     );
