354e52ec3c1892353e6485ccfc80dc74b81def11
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-25 16:53:36 +0530

    Room nested property update using spread operator instead of copy by reference Added FETCH_ROOM

--- client/src/redux/reducers/roomReducer.js
+++ client/src/redux/reducers/roomReducer.js
@@ -1,23 +1,31 @@
 import _ from 'lodash'
 
-import { DELETE_ROOM, FETCH_PUBLIC_ROOMS, LEAVE_ROOM, JOIN_ROOM } from '../action-constants';
+import {
+    DELETE_ROOM,
+    FETCH_PUBLIC_ROOMS,
+    LEAVE_ROOM,
+    JOIN_ROOM,
+    FETCH_ROOM
+} from '../action-constants';
 
 const handleRoomMemberUpdate = (state, payload) => {
     const { roomId, updatedMembers } = payload
-    //TODO: copy by value, not reference
-    const roomToUpdate = state[roomId]
-    if (!roomToUpdate) {
-        return state
+    return {
+        ...state,
+        [roomId]: {
+            ...state[roomId],
+            members: updatedMembers
+        }
     }
-
-    roomToUpdate.members = updatedMembers
-    return { ...state, [roomId]: roomToUpdate }
 }
 
 export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_PUBLIC_ROOMS:
             return { ...state, ..._.mapKeys(action.payload, 'roomId') }
+        case FETCH_ROOM:
+            const room = action.payload
+            return { ...state, [room.roomId]: room }
         case DELETE_ROOM:
             return _.omit(state, action.payload)
         case LEAVE_ROOM:
