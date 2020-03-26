4157d3730dc61ae46982f9f0b3f6b6713d30820f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-26 09:15:07 +0530

    Padding applied on either side depending on Icon position

--- client/src/components/standalone/IconButton.js
+++ client/src/components/standalone/IconButton.js
@@ -4,12 +4,16 @@ import { Button, Row, Col } from 'react-bootstrap';
 const ICON_POS_LEFT = 0;
 const ICON_POS_RIGHT = 1;
 
-const IconButton = ({ icon, iconPos, children, ...rest }) => {
+const IconButton = ({ icon, iconPos, children, childrenProps, ...rest }) => {
+    const isIconLeft = iconPos === ICON_POS_LEFT
     return (
         <Button {...rest}>
             <Row>
-                {iconPos === ICON_POS_LEFT ? <Col>{icon}</Col> : null}
-                <Col style={{ paddingLeft: 0, whiteSpace: 'nowrap' }}>
+                {isIconLeft ? <Col>{icon}</Col> : null}
+                <Col
+                    className={isIconLeft ? 'pl-0' : 'pr-0'}
+                    style={{ whiteSpace: 'nowrap' }}
+                >
                     {children}
                 </Col>
                 {iconPos === ICON_POS_RIGHT ? <Col>{icon}</Col> : null}
