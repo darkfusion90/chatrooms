ebea2505cd7107855ecdf16c52fac5fc4c11bb21
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-26 14:38:15 +0530

    Added Pagination and necessary styling changes (mostly bottom padding/margins)

--- client/src/components/RoomList/RoomList-View.js
+++ client/src/components/RoomList/RoomList-View.js
@@ -1,25 +1,42 @@
 import React from 'react'
 import { Container, ListGroup } from 'react-bootstrap'
 
-import { EmptyRoomList, RoomListControlsHeader, RoomListItem } from './components/'
+import {
+    EmptyRoomList,
+    RoomListControlsHeader,
+    RoomListItem,
+    RoomListPagination
+} from './components/'
 
-const renderRoomList = (rooms) => {
-    return rooms.map((room) => <RoomListItem room={room} key={room.roomId} />)
-}
 
-const RoomList = ({ rooms, onSortFormSubmit }) => {
-    if (!rooms || rooms.length === 0) {
+const RoomList = ({
+    roomsToDisplay,
+    onSortFormSubmit,
+    setCurrentPageNumber,
+    ...otherProps
+}) => {
+    if (!roomsToDisplay || roomsToDisplay.length === 0) {
         return <EmptyRoomList />
+    }
+
+    
+    const renderRoomList = (roomsToDisplay) => {
+        return roomsToDisplay.map((room) => <RoomListItem room={room} key={room.roomId} />)
+    }
+
+    const updatePageNumber = (pageNumber) => {
+        setCurrentPageNumber(pageNumber)
     }
 
     //TODO: Add custom sort style where users can drag a room up and down 
     //according to their convenience
     return (
-        <Container fluid>
+        <Container fluid className='pb-5 mb-5' >
             <RoomListControlsHeader onSortFormSubmit={onSortFormSubmit} />
             <ListGroup variant='flush' className='mx-0 mt-3'>
-                {renderRoomList(rooms)}
+                {renderRoomList(roomsToDisplay)}
             </ListGroup>
+            <RoomListPagination {...otherProps} setCurrentPageNumber={updatePageNumber} />
         </Container>
     )
 }
