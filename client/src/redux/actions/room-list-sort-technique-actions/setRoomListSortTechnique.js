375bc815ab0222d0f1c6cec0490d1ddfc162290a
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-26 14:32:47 +0530

    Moved to room-list-display-settings-actions dir

--- client/src/redux/actions/room-list-sort-technique-actions/setRoomListSortTechnique.js
+++
@@ -1,8 +0,0 @@
-import { SET_ROOM_LIST_SORT_TECHNIQUE } from "../../action-constants"
-
-export default (technique) => {
-    return {
-        type: SET_ROOM_LIST_SORT_TECHNIQUE,
-        payload: technique
-    }
-}
