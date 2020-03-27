26b42cc371384c7e8c8116be939f436fb6b32a70
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-27 20:30:31 +0530

    Fixed import of EmptyRoomList (Should not make any difference but React won't compile without that forward slash)

--- client/src/components/RoomList/components/index.js
+++ client/src/components/RoomList/components/index.js
@@ -1,4 +1,4 @@
-import EmptyRoomList from './EmptyRoomList'
+import EmptyRoomList from './EmptyRoomList/'
 import RoomListItem from './RoomListItem'
 import RoomListControlsHeader from './RoomListControlsHeader'
 import RoomListPagination from './RoomListPagination'
