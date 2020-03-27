64011f2e38ed322f3c5fbecd4eb2389a55ef3a08
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-27 20:32:17 +0530

    Search functionality added. Changes to dispatch accordingly

--- client/src/components/RoomList/RoomList-Redux.js
+++ client/src/components/RoomList/RoomList-Redux.js
@@ -5,11 +5,13 @@ import RoomList from './RoomList-View'
 import { fetchPublicRooms } from '../../redux/actions/room-actions'
 import {
     setSortTechnique,
-    setCurrentPageNumber
+    setCurrentPageNumber,
+    setSearchQuery
 } from '../../redux/actions/room-list-display-settings-actions'
 import { sort as roomSortingTechniques } from './utils'
 
 import loggerInit from '../../helpers/logger'
+import isEmpty from 'is-empty';
 
 const logger = loggerInit('[RoomListContainer] ')
 
@@ -22,30 +24,32 @@ class RoomListContainer extends React.Component {
         logger.debug('RoomList fetch fail: ', response)
     }
 
-    sortRoomList = () => {
+    getSortedRoomList = (rooms) => {
         const { roomListDisplaySettings: { sortBy } } = this.props
-        //Copying by value since directly altering the props can create problems in future
-        const rooms = [...this.props.rooms]
 
         if (!rooms) {
             return []
         }
 
         switch (sortBy) {
-            default:
-            case 'name-asc':
-                return rooms.sort(roomSortingTechniques.byName)
             case 'name-des':
                 return rooms.sort(roomSortingTechniques.byName).reverse()
             case 'date-asc':
                 return rooms.sort(roomSortingTechniques.byDate)
             case 'date-des':
                 return rooms.sort(roomSortingTechniques.byDate).reverse()
+            case 'name-asc':
+            default:
+                return rooms.sort(roomSortingTechniques.byName)
         }
     }
 
     onSortFormSubmit = (formValues) => {
         this.props.setSortTechnique(formValues.sortBy)
+    }
+
+    onSearchFieldInputChange = (e) => {
+        this.props.setSearchQuery(e.target.value)
     }
 
     getPaginatedRoomList = (sortedRooms) => {
@@ -62,16 +66,44 @@ class RoomListContainer extends React.Component {
         return sortedRooms.slice(offset, to)
     }
 
+    getSearchQuery = () => {
+        const { roomListDisplaySettings: { searchQuery } } = this.props
+        return searchQuery && searchQuery.toLowerCase().trim()
+    }
+
+    filterRoomListBySearchQuery = (roomList, searchQuery) => {
+        if (searchQuery) {
+            return roomList.filter(room => {
+                const roomName = room && room.name
+                return roomName && roomName.toLowerCase().includes(searchQuery)
+            })
+        }
+
+        return roomList
+    }
+
+    getRooms = (searchQuery) => {
+        const rooms = [...this.props.rooms]
+        return this.getSortedRoomList(this.filterRoomListBySearchQuery(rooms, searchQuery))
+    }
+
     render() {
-        const { roomListDisplaySettings, setCurrentPageNumber } = this.props
-        const sortedRooms = this.sortRoomList()
-        const paginatedRooms = this.getPaginatedRoomList(sortedRooms)
+        const {
+            roomListDisplaySettings,
+            setCurrentPageNumber
+        } = this.props
+
+        const searchQuery = this.getSearchQuery()
+        const rooms = this.getRooms(searchQuery)
+        const paginatedRooms = this.getPaginatedRoomList(rooms)
 
         return (
             <RoomList
-                totalRooms={sortedRooms}
+                totalRooms={rooms}
                 roomsToDisplay={paginatedRooms}
+                isSearchResult={!isEmpty(searchQuery)}
                 onSortFormSubmit={this.onSortFormSubmit}
+                onSearchFieldInputChange={this.onSearchFieldInputChange}
                 setCurrentPageNumber={setCurrentPageNumber}
                 {...roomListDisplaySettings}
             />
@@ -82,11 +114,11 @@ class RoomListContainer extends React.Component {
 const mapStateToProps = (state) => {
     return {
         rooms: Object.values(state.rooms),
-        roomListDisplaySettings: state.roomListDisplaySettings
+        roomListDisplaySettings: state.roomListDisplaySettings,
     }
 }
 
 export default connect(
     mapStateToProps,
-    { fetchPublicRooms, setSortTechnique, setCurrentPageNumber }
+    { fetchPublicRooms, setSortTechnique, setCurrentPageNumber, setSearchQuery }
 )(RoomListContainer);
