8f4bbeeef4187f6ad5a10936df837ce29acae2b9
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-27 20:28:24 +0530

    Added Search field to header. Adjusted some styling accordingly

--- client/src/components/RoomList/components/RoomListControlsHeader/RoomListControlsHeader-View.js
+++ client/src/components/RoomList/components/RoomListControlsHeader/RoomListControlsHeader-View.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 
 import SortRoomDropdownButton from './SortRoomDropdownButton'
+import RoomListSearchField from '../RoomListSearchField'
 
-const RoomListControlsHeader = ({ onSortFormSubmit }) => {
+
+const RoomListControlsHeader = ({ onSortFormSubmit, onSearchFieldInputChange }) => {
     return (
-        <Container fluid className='room-page-sub-header d-flex align-items-center'>
-            <SortRoomDropdownButton onSortFormSubmit={onSortFormSubmit} />
+        <Container fluid className='room-page-sub-header d-flex align-items-center justify-content-between px-sm-0 mx-sm-0'>
+            <Container fluid>
+                <SortRoomDropdownButton onSortFormSubmit={onSortFormSubmit} />
+            </Container>
+            <Container fluid>
+                <RoomListSearchField onSearchFieldInputChange={onSearchFieldInputChange} />
+            </Container>
         </Container>
     )
 }
