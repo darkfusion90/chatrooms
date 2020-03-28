258faa0572d8d7011082a59bb7f48948a50268c8
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-28 17:27:01 +0530

    Sends errors meta for all kinds of errors

--- server/utils/errorResponseMetaCreator.js
+++ server/utils/errorResponseMetaCreator.js
@@ -3,19 +3,16 @@ const UnauthorizedError = require('../errors/Unauthorized')
 const MongooseDuplicateKeyError = require('../errors/MongooseDuplicateKeyError')
 
 const determineResponseMetaFromError = (err) => {
-    const responseMeta = {}
+    const responseMeta = { data: err }
 
     if (err instanceof MongooseDuplicateKeyError) {
         responseMeta.statusCode = httpStatusCodes.CONFLICT
-        responseMeta.data = err
     }
     else if (err instanceof UnauthorizedError) {
         responseMeta.statusCode = httpStatusCodes.FORBIDDEN
-        responseMeta.data = {}
     }
     else {
         responseMeta.statusCode = httpStatusCodes.INTERNAL_SERVER_ERROR
-        responseMeta.data = {}
     }
 
     return responseMeta
