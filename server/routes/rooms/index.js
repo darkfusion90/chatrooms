6348652d133e557dbdf9eda4ddbab66bbb41a256
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-30 18:53:39 +0530

    Formatted export statement to 1 export per line

--- server/routes/rooms/index.js
+++ server/routes/rooms/index.js
@@ -2,4 +2,11 @@ const { get, post, patch, _delete } = require('./rooms')
 const roomMessages = require('./roomMessages')
 const roomMembers = require('./roomMembers')
 
-module.exports = { get, post, patch, _delete, messages: roomMessages, members: roomMembers }
+module.exports = {
+    get,
+    post,
+    patch,
+    _delete,
+    messages: roomMessages,
+    members: roomMembers
+}
