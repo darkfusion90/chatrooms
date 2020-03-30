ab13854c3e4ff11192e1acdbd65301c956521af8
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-30 12:20:06 +0530

    Controller for model RoomInvitation

---
+++ server/controllers/roomInvitations.js
@@ -0,0 +1,27 @@
+const RoomInvitation = require('../models/RoomInvitation')
+const isFunction = require('../utils/isFunction')
+
+exports.createInvitation = (invitee, inviter, room, callback) => {
+    const promise = new Promise((resolve, reject) => {
+        const roomInvitation = new RoomInvitation({ invitee, inviter, room })
+        roomInvitation.save().then(resolve).catch(reject)
+    })
+
+    if (isFunction(callback)) {
+        promise.then(val => callback(null, val)).catch(callback)
+    }
+
+    return promise
+}
+
+exports.deleteInvitation = (invitationId, callback) => {
+    const promise = new Promise((resolve, reject) => {
+        RoomInvitation.findById(invitationId).then(resolve).catch(reject)
+    })
+
+    if (isFunction(callback)) {
+        promise.then(val => callback(null, val)).catch(callback)
+    }
+
+    return promise
+}
