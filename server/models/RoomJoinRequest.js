ecdd07c55a7abec5f73d153e509b7a6552960660
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-30 12:21:06 +0530

    Model for a request to join a room

---
+++ server/models/RoomJoinRequest.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const { UserSchema } = require('./User')
+const { RoomSchema } = require('./Room')
+
+const RoomJoinRequestSchema = new mongoose.Schema({
+    requestedBy: {
+        type: UserSchema,
+        required: true
+    },
+    room: {
+        type: RoomSchema,
+        required: true
+    }
+})
+
+const RoomJoinRequest = mongoose.model('RoomJoinRequest', RoomJoinRequestSchema)
+
+module.exports = RoomJoinRequest
