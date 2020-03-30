0571ecc3624c71354a9d8b2e83bf0db1954d9c32
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-30 18:58:29 +0530

    Added get for room invitations based on invitee userId

--- server/controllers/roomInvitations.js
+++ server/controllers/roomInvitations.js
@@ -14,6 +14,34 @@ exports.createInvitation = (invitee, inviter, room, callback) => {
     return promise
 }
 
+exports.getInvitationOfInvitee = (invitationId, invitee, callback) => {
+    const promise = new Promise((resolve, reject) => {
+        RoomInvitation.findOne({ _id: invitationId, invitee })
+            .then(resolve)
+            .catch(reject)
+    })
+
+    if (isFunction(callback)) {
+        promise.then(val => callback(null, val)).catch(callback)
+    }
+
+    return promise
+}
+
+exports.getAllInvitationsOfInvitee = (invitee, callback) => {
+    const promise = new Promise((resolve, reject) => {
+        RoomInvitation.find({ invitee })
+            .then(resolve)
+            .catch(reject)
+    })
+
+    if (isFunction(callback)) {
+        promise.then(val => callback(null, val)).catch(callback)
+    }
+
+    return promise
+}
+
 exports.deleteInvitation = (invitationId, callback) => {
     const promise = new Promise((resolve, reject) => {
         RoomInvitation.findById(invitationId).then(resolve).catch(reject)
