ff453ca618d5cf3c39dd6516399c3c00d9d62805
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-31 12:59:42 +0530

    Controller for model Notification. Methods as of now: create

---
+++ server/controllers/notifications.js
@@ -0,0 +1,25 @@
+const { Notification } = require('../models/Notification')
+const createPromiseCallbackFunction = require('../utils/promiseCallbackFunction')
+
+const getNotificationDoc = (user, meta) => {
+    switch (meta.type) {
+        case 'room_invitation':
+            return { user, roomInvitation: meta.payload }
+        default:
+            return { user }
+    }
+}
+
+exports.createNotification = (user, meta, callback) => {
+    return createPromiseCallbackFunction((resolve, reject) => {
+        const notification = new Notification(getNotificationDoc(user, meta))
+        notification.save().then(resolve).catch(reject)
+    }, callback)
+}
+
+exports.getNotification = (notificationId, callback) => {
+    return createPromiseCallbackFunction((resolve, reject) => {
+        Notification.findById(notificationId).then(resolve).catch(reject)
+    }, callback)
+}
+
