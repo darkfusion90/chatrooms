84f2774317f73d9c27362c37dd6c98e02cc65880
darkfusion90 <gauravsharmaa341@gmail.com>
2020-03-31 12:58:56 +0530

    Util to create function that takes an optional callback returns a promise with callback bound if applicable

---
+++ server/utils/promiseCallbackFunction.js
@@ -0,0 +1,18 @@
+const isFunction = require('./isFunction')
+
+/**
+ * 
+ * @param {Function} executor The executor to be passed to the promise being created
+ * @param {any} callback An optional callback which will be called iff it is a function
+ * @returns {Promise} The promise created using *executor* and *callback* attached to it if applicable
+ */
+const createPromiseCallbackFunction = (executor, callback) => {
+    const promise = new Promise(executor)
+
+    if (isFunction(callback)) {
+        promise.then(v => callback(null, v)).catch(callback)
+    }
+    return promise
+}
+
+module.exports = createPromiseCallbackFunction
