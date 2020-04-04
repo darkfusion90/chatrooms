43e4f2372c83b748fa4ba5a77f2dd6f5d273ec43
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-04 17:27:27 +0530

    Added redux prop: inviteUserForm Added redux dispatch: selectUsername

--- client/src/components/modals/InviteUserModal/InviteUserModal-Redux.js
+++ client/src/components/modals/InviteUserModal/InviteUserModal-Redux.js
@@ -3,18 +3,19 @@ import { connect } from 'react-redux'
 
 import InviteUserModalContainer from './InviteUserModal-Container'
 import { hideModal } from '../../../redux/actions/modal-actions';
-
+import { selectUsername } from './utils'
 
 const InviteUserModalRedux = (props) => {
     return <InviteUserModalContainer {...props} />
 }
 
 const mapStateToProps = (state) => {
-    const { modal: { isModalOpen, modalName, modalProps } } = state
+    const { modal: { isModalOpen, modalName, modalProps }, form } = state
     return {
         isModalVisible: isModalOpen && modalName === 'InviteUser',
-        modalProps
+        modalProps,
+        inviteUserForm: form['invite-user-form'],
     }
 }
 
-export default connect(mapStateToProps, { hideModal })(InviteUserModalRedux)
+export default connect(mapStateToProps, { hideModal, selectUsername })(InviteUserModalRedux)
