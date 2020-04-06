a3e81e88ae0db45e7242f25d95b48df44c58f7c9
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-06 16:33:55 +0530

    Added method to accept room invitation

--- client/src/server-communication/httpServer.js
+++ client/src/server-communication/httpServer.js
@@ -89,3 +89,8 @@ export const sendRoomInvitation = (inviteeUserId, roomId, onRequestFulfilled, on
         { invitee: inviteeUserId, roomId }
     ).then(onRequestFulfilled).catch(onRequestRejected)
 }
+
+export const acceptRoomInvitation = (invitationId, onRequestFulfilled, onRequestRejected) => {
+    return axios.post(`/api/room_invitations/${invitationId}/accept`)
+        .then(onRequestFulfilled).catch(onRequestRejected)
+}
