cfafefd57fe4948ea90106176a60717db10124b5
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-07 20:13:23 +0530

    Added TODO to diagnose problem when on direct visit to a room's url which the user is not a member of, the room component doesn't  get updated

--- client/src/components/Room/Room-Redux.js
+++ client/src/components/Room/Room-Redux.js
@@ -5,6 +5,12 @@ import RoomContainer from './Room-Container'
 import { fetchRoom } from '../../redux/actions/room-actions'
 
 const RoomRedux = (props) => {
+    //TODO: When directly visiting a room's url, the store gets confusingly malfunctioned
+    //Example: On clicking join, the component is not updated and it remains same
+    //i.e., with the "Joined Room" button instead of reloading and showing the room window
+    //Upon inspecting, the redux store seems to consider the user as the member of the room
+    //However, as stated, the component doesn't seems to do the same and still shows the 
+    //Room not joined screen with the green (successful) "Room Joined" button
     return <RoomContainer {...props} />;
 }
 
