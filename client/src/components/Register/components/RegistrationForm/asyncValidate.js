b4b9332b1c4b8366cb8f369127e68dc9065b1835
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-07 20:14:03 +0530

    Checking username validity handled by http server requests compared to previous socketIO events

--- client/src/components/Register/components/RegistrationForm/asyncValidate.js
+++ client/src/components/Register/components/RegistrationForm/asyncValidate.js
@@ -1,10 +1,10 @@
-import { checkUsername } from '../../../../server-communication/socketServer'
+import { getUserByUsername } from '../../../../server-communication/httpServer'
 
 export const USERNAME_TAKEN_ERROR_MESSAGE = 'That username is already taken'
 
 export const checkUsernameExists = (values, _) => {
     return new Promise((resolve, reject) =>
-        checkUsername(values.username, (response) => {
+        getUserByUsername(values.username, (response) => {
             if (response.status === 200) {
                 reject({ 'username': USERNAME_TAKEN_ERROR_MESSAGE })
             }
