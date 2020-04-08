2d4e6511e99249b5cb33f621c1da33cccea51efc
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-08 10:25:46 +0530

    Refactored to handle rejection of the promise. Had missed the refactoring after changing the checking username from socketio(using callback) to https method(using promise)

--- client/src/components/Register/components/RegistrationForm/asyncValidate.js
+++ client/src/components/Register/components/RegistrationForm/asyncValidate.js
@@ -3,14 +3,10 @@ import { getUserByUsername } from '../../../../server-communication/httpServer'
 export const USERNAME_TAKEN_ERROR_MESSAGE = 'That username is already taken'
 
 export const checkUsernameExists = (values, _) => {
-    return new Promise((resolve, reject) =>
-        getUserByUsername(values.username, (response) => {
-            if (response.status === 200) {
-                reject({ 'username': USERNAME_TAKEN_ERROR_MESSAGE })
-            }
-            else {
-                resolve()
-            }
-        })
-    )
+    return new Promise((resolve, reject) => {
+        const onUsernameFound = () => {
+            reject({ 'username': USERNAME_TAKEN_ERROR_MESSAGE })
+        }
+        getUserByUsername(values.username, onUsernameFound, resolve)
+    })
 }
