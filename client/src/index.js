8b276025007f54fcf9f0a10e3c7d42376c68c95d
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-08 08:13:48 +0530

    Added ToastContainer to global level (like ModalContainer)

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk';
 import reducers from './redux/reducers'
 import App from './components/App'
 import ModalContainer from './components/modals/ModalContainer'
+import ToastContainer from './components/toasts/ToastContainer/'
 
 const composeEnhanchers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhanchers(
@@ -16,6 +17,7 @@ ReactDOM.render(
     <Provider store={store}>
         <App />
         <ModalContainer />
+        <ToastContainer />
     </Provider>,
     document.querySelector("#root")
 );
