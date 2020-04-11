3663bb553eb292f55c935a3e6f695533b617d085
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-11 20:09:13 +0530

    Changes to reflect the removal of paths 'members' from model Room Checks are done using roomMembers controller now

--- server/middlewares/roomAuth/helper.js
+++ server/middlewares/roomAuth/helper.js
@@ -1,18 +1,32 @@
 const UnauthorizedError = require('../../errors/Unauthorized')
 const { REASON } = require('../../constants/apiResponseConstants')
+const { getRoomMemberHavingUserId } = require('../../controllers/roomMembers')
 
-const ensureIsRoomAdmin = (userId, room, next) => {
-    if (room.isAdmin(userId)) {
-        next()
-    } else {
+const ensureIsRoomAdmin = async (userId, room, next) => {
+    let roomMemberReference;
+    try {
+        roomMemberReference = await getRoomMemberHavingUserId(room._id, userId)
+    } catch (error) {
+        next(error)
+    } finally {
+        if (roomMemberReference && roomMemberReference.isAdmin()) {
+            return next()
+        }
+
         next(new UnauthorizedError(REASON.NOT_A_ROOM_ADMIN))
     }
 }
 
-const ensureIsRoomMember = (userId, room, next) => {
-    if (room.isMember(userId)) {
-        next()
-    } else {
+const ensureIsRoomMember = async (userId, room, next) => {
+    let roomMemberReference;
+    try {
+        roomMemberReference = await getRoomMemberHavingUserId(room._id, userId)
+    } catch (error) {
+        next(error)
+    } finally {
+        if (roomMemberReference) {
+            return next()
+        }
         next(new UnauthorizedError(REASON.NOT_A_ROOM_MEMBER))
     }
 }
