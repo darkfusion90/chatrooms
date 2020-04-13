4dd8ba8655a24a34a6450c11e6a102411f956984
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-13 20:47:44 +0530

    Added pagination by taking pagination data from the state

--- client/src/redux/actions/room-actions/fetchPublicRooms.js
+++ client/src/redux/actions/room-actions/fetchPublicRooms.js
@@ -1,8 +1,12 @@
 import { FETCH_PUBLIC_ROOMS } from '../../action-constants'
 import { rooms } from '../../../api/http';
 
-export default (onFailure) => async dispatch => {
-    rooms.getAllRooms((response) => {
-        dispatch({ type: FETCH_PUBLIC_ROOMS, payload: response.data })
-    }, onFailure)
+
+export default (onFailure) => async (dispatch, getState) => {
+    const { roomListDisplaySettings: { currentPageNumber, itemsPerPage } } = getState()
+    const offset = itemsPerPage * (currentPageNumber - 1)
+
+    rooms.getRooms(itemsPerPage, offset)
+        .then(({ data }) => dispatch({ type: FETCH_PUBLIC_ROOMS, payload: data }))
+        .catch(onFailure)
 }
