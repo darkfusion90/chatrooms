bbbb253b43cf2caf62021bcef0ad2b0524ee32fd
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-13 20:51:24 +0530

    Removed pagination, sort and search filtering from the component. Now handled in the server side

--- client/src/components/RoomList/RoomList-Redux.js
+++ client/src/components/RoomList/RoomList-Redux.js
@@ -1,71 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import _ from 'lodash'
 
 import RoomList from './RoomList-View'
-import { fetchPublicRooms } from '../../redux/actions/room-actions'
+import {
+    fetchPublicRooms,
+    updateTotalPublicRoomsCount
+} from '../../redux/actions/room-actions'
 import {
     setSortTechnique,
     setCurrentPageNumber,
     setSearchQuery,
     setItemsPerPage
 } from '../../redux/actions/room-list-display-settings-actions'
-import { sort as roomSortingTechniques } from './utils'
-
-import loggerInit from '../../helpers/logger'
 import isEmpty from 'is-empty';
 
-const logger = loggerInit('[RoomListContainer] ')
 
-class RoomListContainer extends React.Component {
+class RoomListRedux extends React.Component {
     componentDidMount() {
+        this.refreshStore()
+    }
+
+    componentDidUpdate(prevProps) {
+        const { roomListDisplaySettings: prevDisplaySettings } = prevProps
+        const { roomListDisplaySettings: currentDisplaySettings } = this.props
+
+        if (!_.isEqual(prevDisplaySettings, currentDisplaySettings)) {
+            this.refreshStore()
+        }
+    }
+
+    refreshStore = () => {
+        this.props.updateTotalPublicRoomsCount()
         this.props.fetchPublicRooms(this.onRoomFetchFail)
     }
 
     onRoomFetchFail = ({ response }) => {
-        logger.debug('RoomList fetch fail: ', response)
+        console.log('RoomList fetch fail: ', response)
     }
 
-    getSortedRoomList = (rooms) => {
-        const { roomListDisplaySettings: { sortBy } } = this.props
-
-        if (!rooms) {
-            return []
-        }
-
-        switch (sortBy) {
-            case 'name-des':
-                return rooms.sort(roomSortingTechniques.byName).reverse()
-            case 'date-asc':
-                return rooms.sort(roomSortingTechniques.byDate)
-            case 'date-des':
-                return rooms.sort(roomSortingTechniques.byDate).reverse()
-            case 'name-asc':
-            default:
-                return rooms.sort(roomSortingTechniques.byName)
-        }
-    }
-
-    onDisplayControlsFormSubmit = ({sortBy, itemsPerPage}) => {
+    onDisplayControlsFormSubmit = ({ sortBy, itemsPerPage }) => {
         this.props.setSortTechnique(sortBy)
-        this.props.setItemsPerPage(itemsPerPage)
+        this.props.setItemsPerPage(Math.min(itemsPerPage, this.props.countTotalPublicRooms))
     }
 
     onSearchFieldInputChange = (e) => {
         this.props.setSearchQuery(e.target.value)
-    }
-
-    getPaginatedRoomList = (sortedRooms) => {
-        const { roomListDisplaySettings: { itemsPerPage, currentPageNumber } } = this.props
-        if (sortedRooms.length <= itemsPerPage) {
-            return sortedRooms
-        }
-
-        //Page numbers are 1-indexed (1, 2, 3...) whereas array is 0-indexed (0, 1, 2...)
-        //So to make 1-indexed page number applicable while indexing array, 
-        //we subtract 1 from the page number
-        const offset = itemsPerPage * (currentPageNumber - 1);
-        const to = Math.min(offset + itemsPerPage, sortedRooms.length)
-        return sortedRooms.slice(offset, to)
     }
 
     getSearchQuery = () => {
@@ -73,36 +53,20 @@ class RoomListContainer extends React.Component {
         return searchQuery && searchQuery.toLowerCase().trim()
     }
 
-    filterRoomListBySearchQuery = (roomList, searchQuery) => {
-        if (searchQuery) {
-            return roomList.filter(room => {
-                const roomName = room && room.name
-                return roomName && roomName.toLowerCase().includes(searchQuery)
-            })
-        }
-
-        return roomList
-    }
-
-    getRooms = (searchQuery) => {
-        const rooms = [...this.props.rooms]
-        return this.getSortedRoomList(this.filterRoomListBySearchQuery(rooms, searchQuery))
-    }
-
     render() {
         const {
+            rooms,
+            countTotalPublicRooms,
             roomListDisplaySettings,
             setCurrentPageNumber
         } = this.props
 
         const searchQuery = this.getSearchQuery()
-        const rooms = this.getRooms(searchQuery)
-        const paginatedRooms = this.getPaginatedRoomList(rooms)
 
         return (
             <RoomList
-                totalRooms={rooms}
-                roomsToDisplay={paginatedRooms}
+                countTotalPublicRooms={countTotalPublicRooms}
+                rooms={rooms}
                 isSearchResult={!isEmpty(searchQuery)}
                 onDisplayControlsFormSubmit={this.onDisplayControlsFormSubmit}
                 onSearchFieldInputChange={this.onSearchFieldInputChange}
@@ -114,18 +78,25 @@ class RoomListContainer extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+    const {
+        rooms: { rooms, countTotalPublicRooms },
+        roomListDisplaySettings
+    } = state
+
     return {
-        rooms: Object.values(state.rooms),
-        roomListDisplaySettings: state.roomListDisplaySettings,
+        rooms: Object.values(rooms),
+        countTotalPublicRooms,
+        roomListDisplaySettings,
     }
 }
 
 const mapDispatchToProps = ({
     fetchPublicRooms,
+    updateTotalPublicRoomsCount,
     setSortTechnique,
     setCurrentPageNumber,
     setSearchQuery,
     setItemsPerPage
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(RoomListContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(RoomListRedux);
