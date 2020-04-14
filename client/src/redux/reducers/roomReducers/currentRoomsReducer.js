fd9c184350044c512bf65a83d0d93ec9524a216c
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-14 12:06:47 +0530

    Handling changes related to membership (Leave room, join room, etc) removed from the reducer FETCH_ROOM no longer handled by this reducer

--- client/src/redux/reducers/roomReducers/currentRoomsReducer.js
+++ client/src/redux/reducers/roomReducers/currentRoomsReducer.js
@@ -3,42 +3,23 @@ import _ from 'lodash'
 import {
     DELETE_ROOM,
     FETCH_PUBLIC_ROOMS,
-    LEAVE_ROOM,
-    JOIN_ROOM,
     FETCH_ROOM,
     UPDATE_ROOM
 } from '../../action-constants';
 
 
-const handleRoomMemberUpdate = (state, payload) => {
-    const { roomId, updatedMembers } = payload
-    return {
-        ...state,
-        [roomId]: {
-            ...state.rooms[roomId],
-            members: updatedMembers
-        }
-    }
-}
-
-const addRoomToState = (state, room) => {
+const handleUpdateRoom = (state, room) => {
     return { ...state, [room.roomId]: room }
 }
 
-export default (state={}, action) => {
+export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_PUBLIC_ROOMS:
-            return { ...state, ..._.mapKeys(action.payload, '_id'), }
-        case FETCH_ROOM:
-            return addRoomToState(state, action.payload)
+            return _.mapKeys(action.payload, '_id')
         case DELETE_ROOM:
             return _.omit(state, action.payload)
-        case LEAVE_ROOM:
-            return handleRoomMemberUpdate(state, action.payload)
-        case JOIN_ROOM:
-            return handleRoomMemberUpdate(state, action.payload)
         case UPDATE_ROOM:
-            return addRoomToState(state, action.payload)
+            return handleUpdateRoom(state, action.payload)
         default:
             return state;
     }
