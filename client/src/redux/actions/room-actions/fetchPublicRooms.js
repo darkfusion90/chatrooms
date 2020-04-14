651d41bc3c2b2263336a0ccbc18771c01c386d79
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-14 08:39:05 +0530

    Added onSuccess callback

--- client/src/redux/actions/room-actions/fetchPublicRooms.js
+++ client/src/redux/actions/room-actions/fetchPublicRooms.js
@@ -2,11 +2,14 @@ import { FETCH_PUBLIC_ROOMS } from '../../action-constants'
 import { rooms } from '../../../api/http';
 
 
-export default (onFailure) => async (dispatch, getState) => {
+export default (onSuccess, onFailure) => async (dispatch, getState) => {
     const { roomListDisplaySettings: { currentPageNumber, itemsPerPage } } = getState()
     const offset = itemsPerPage * (currentPageNumber - 1)
 
     rooms.getRooms(itemsPerPage, offset)
-        .then(({ data }) => dispatch({ type: FETCH_PUBLIC_ROOMS, payload: data }))
+        .then(response => {
+            onSuccess(response)
+            dispatch({ type: FETCH_PUBLIC_ROOMS, payload: response.data })
+        })
         .catch(onFailure)
 }
