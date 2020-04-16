e7cc9f0ac34a086d2464d036cb6c1ae17cf56e37
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-16 13:20:05 +0530

    Current page adjusted everytime itemsPerPage is updated

--- client/src/redux/reducers/roomListDisplaySettingsReducer.js
+++ client/src/redux/reducers/roomListDisplaySettingsReducer.js
@@ -12,12 +12,32 @@ const INITIAL_STATE = {
     searchQuery: null
 }
 
+const getOffset = (itemsPerPage, pageNumber) => {
+    return itemsPerPage * (pageNumber - 1)
+}
+
+const getPageNumber = (itemsPerPage, offset) => {
+    return Math.floor((offset + itemsPerPage) / itemsPerPage)
+}
+
+const adjustCurrentPageNumber = (state, newItemsPerPage) => {
+    const offset = getOffset(state.itemsPerPage, state.currentPageNumber)
+    const updatedPageNumber = getPageNumber(newItemsPerPage, offset)
+    return updatedPageNumber
+}
+
+
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case SET_ROOM_LIST_SORT_TECHNIQUE:
             return { ...state, sortBy: action.payload }
         case SET_ROOM_LIST_ITEMS_PER_PAGE:
-            return { ...state, itemsPerPage: action.payload }
+            const newItemsPerPage = action.payload
+            return {
+                ...state,
+                itemsPerPage: newItemsPerPage,
+                currentPageNumber: adjustCurrentPageNumber(state, newItemsPerPage)
+            }
         case SET_ROOM_LIST_CURRENT_PAGE_NUMBER:
             return { ...state, currentPageNumber: action.payload }
         case SET_ROOM_LIST_SEARCH_QUERY:
