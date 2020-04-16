c7cb115796e162ff1d686c417b9b08e8878ece49
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-16 13:36:33 +0530

    Removed onSuccess and onFailure callbacks

--- client/src/redux/actions/room-actions/fetchPublicRooms.js
+++ client/src/redux/actions/room-actions/fetchPublicRooms.js
@@ -2,14 +2,13 @@ import { FETCH_PUBLIC_ROOMS } from '../../action-constants'
 import { rooms } from '../../../api/http';
 
 
-export default (onSuccess, onFailure) => async (dispatch, getState) => {
+export default () => async (dispatch, getState) => {
     const { roomListDisplaySettings: { currentPageNumber, itemsPerPage } } = getState()
     const offset = itemsPerPage * (currentPageNumber - 1)
 
     rooms.getRooms(itemsPerPage, offset)
         .then(response => {
-            onSuccess(response)
             dispatch({ type: FETCH_PUBLIC_ROOMS, payload: response.data })
         })
-        .catch(onFailure)
+        .catch(err => console.log('Action fetchPublicRooms: Failure fetching rooms: ', err))
 }
