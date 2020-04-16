7bf37f07441d7373cf683799297e0dffff56feaf
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-16 13:56:35 +0530

    Index added for easier importing of api functions

--- client/src/api/http/index.js
+++ client/src/api/http/index.js
@@ -4,6 +4,14 @@ import * as notifications from './notifications'
 import * as roomInvitations from './roomInvitations'
 import * as messages from './rooms.messages'
 import * as search from './search'
+import * as roomMembers from './room.members'
 
-
-export { users, rooms, notifications, roomInvitations, messages, search }
+export {
+    users,
+    rooms,
+    notifications,
+    roomInvitations,
+    messages,
+    roomMembers,
+    search
+}
