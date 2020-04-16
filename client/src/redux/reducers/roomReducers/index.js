4d2e13d7832ad9ce91aaf371f8b18e5a6d3bc864
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-16 13:16:49 +0530

    Refactored: currentUserRoomRoles -> currentUserRoomMemberships Added to state: activeRoom

--- client/src/redux/reducers/roomReducers/index.js
+++ client/src/redux/reducers/roomReducers/index.js
@@ -2,10 +2,13 @@ import { combineReducers } from 'redux';
 
 import countTotalPublicRoomsReducer from './countTotalPublicRoomsReducer'
 import currentRoomsReducer from './currentRoomsReducer'
-import currentUserRoomRolesReducer from './currentUserRoomRolesReducer'
+import currentUserRoomMembershipReducer from './currentUserRoomMembershipReducer'
+import activeRoomReducer from './activeRoomReducer'
+
 
 export default combineReducers({
     countTotalPublicRooms: countTotalPublicRoomsReducer,
     currentRooms: currentRoomsReducer,
-    currentUserRoomRoles: currentUserRoomRolesReducer
+    currentUserRoomMemberships: currentUserRoomMembershipReducer,
+    activeRoom: activeRoomReducer
 })
