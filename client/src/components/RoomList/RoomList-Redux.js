ed8b519bfb1cd71fc6b0d674bcf3a543e239debc
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-16 13:55:32 +0530

    Major changes. However, everything boils down to removing all nested components' functionality that affects and is retrieved from redux. Those components now recieve dispatches and props and perform their funtionalities independent of RoomList-Redux (except rooms prop) since they obtain data from redux directly

--- client/src/components/RoomList/RoomList-Redux.js
+++ client/src/components/RoomList/RoomList-Redux.js
@@ -1,133 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux'
 import _ from 'lodash'
 
-import RoomList from './RoomList-View'
+import RoomListView from './RoomList-View'
 import {
     fetchPublicRooms,
-    updateTotalPublicRoomsCount,
-    updateCurrentUserMemberRoles
+    updateTotalPublicRoomsCount
 } from '../../redux/actions/room-actions'
-import {
-    setSortTechnique,
-    setCurrentPageNumber,
-    setSearchQuery,
-    setItemsPerPage
-} from '../../redux/actions/room-list-display-settings-actions'
-import isEmpty from 'is-empty';
 
 
-class RoomListRedux extends React.Component {
-    componentDidMount() {
-        this.refreshStore()
-    }
+const RoomListRedux = ({
+    updateTotalPublicRoomsCount,
+    fetchPublicRooms,
+    rooms,
+    roomListDisplaySettings,
+    ...props }) => {
 
-    componentDidUpdate(prevProps) {
-        if (this.hasStoreChanged(prevProps)) {
-            this.refreshStore()
-        }
-    }
+    useEffect(() => {
+        updateTotalPublicRoomsCount()
+        fetchPublicRooms()
+    }, [roomListDisplaySettings, fetchPublicRooms, updateTotalPublicRoomsCount])
 
-    hasStoreChanged = (prevProps) => {
-        const hasDisplaySettingsChanged = () => {
-            const { roomListDisplaySettings: prevDisplaySettings } = prevProps
-            const { roomListDisplaySettings: currentDisplaySettings } = this.props
-
-            return !_.isEqual(prevDisplaySettings, currentDisplaySettings)
-        }
-
-        const hasUserDataChanged = () => {
-            const { user: prevUser } = prevProps
-            const { user: currentUser } = this.props
-
-            return !_.isEqual(prevUser, currentUser)
-        }
-
-        const hasRoomsDataChanged = () => {
-            const { user: prevRooms } = prevProps
-            const { user: currentRooms } = this.props
-
-            return !_.isEqual(prevRooms, currentRooms)
-        }
-
-        return hasDisplaySettingsChanged() || hasUserDataChanged() || hasRoomsDataChanged()
-    }
-
-    refreshStore = () => {
-        this.props.updateTotalPublicRoomsCount()
-        this.props.fetchPublicRooms(this.onRoomFetchSuccess, this.onRoomFetchFail)
-    }
-
-    onRoomFetchFail = ({ response }) => {
-        console.log('RoomList fetch fail: ', response)
-    }
-
-    onRoomFetchSuccess = ({ data }) => {
-        console.log('success: ', data)
-        this.props.updateCurrentUserMemberRoles()
-    }
-
-    onDisplayControlsFormSubmit = ({ sortBy, itemsPerPage }) => {
-        this.props.setSortTechnique(sortBy)
-        this.props.setItemsPerPage(Math.min(itemsPerPage, this.props.countTotalPublicRooms))
-    }
-
-    onSearchFieldInputChange = (e) => {
-        this.props.setSearchQuery(e.target.value)
-    }
-
-    getSearchQuery = () => {
-        const { roomListDisplaySettings: { searchQuery } } = this.props
+    const getSearchQuery = () => {
+        const { searchQuery } = roomListDisplaySettings
         return searchQuery && searchQuery.toLowerCase().trim()
     }
 
-    render() {
-        const {
-            rooms,
-            countTotalPublicRooms,
-            roomListDisplaySettings,
-            setCurrentPageNumber
-        } = this.props
+    const isSearchQueryEmpty = _.isEmpty(getSearchQuery())
 
-        const searchQuery = this.getSearchQuery()
-
-        return (
-            <RoomList
-                countTotalPublicRooms={countTotalPublicRooms}
-                rooms={rooms}
-                isSearchResult={!isEmpty(searchQuery)}
-                onDisplayControlsFormSubmit={this.onDisplayControlsFormSubmit}
-                onSearchFieldInputChange={this.onSearchFieldInputChange}
-                setCurrentPageNumber={setCurrentPageNumber}
-                {...roomListDisplaySettings}
-            />
-        )
-    }
+    return <RoomListView rooms={rooms} isSearchResult={!isSearchQueryEmpty} />
 }
 
 const mapStateToProps = (state) => {
-    const {
-        user,
-        rooms: { currentRooms, ...otherState },
-        roomListDisplaySettings
-    } = state
+    const { rooms: { currentRooms }, roomListDisplaySettings } = state
 
     return {
-        user,
-        roomListDisplaySettings,
         rooms: Object.values(currentRooms),
-        ...otherState
+        roomListDisplaySettings
     }
 }
 
 const mapDispatchToProps = ({
     fetchPublicRooms,
-    updateTotalPublicRoomsCount,
-    updateCurrentUserMemberRoles,
-    setSortTechnique,
-    setCurrentPageNumber,
-    setSearchQuery,
-    setItemsPerPage
+    updateTotalPublicRoomsCount
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(RoomListRedux);
