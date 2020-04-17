6fd38e450bd3e00569b8a3db2aee75aa01682d35
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-17 10:21:25 +0530

    Removed propFromRedux: currentUser Added propFromRedux: currentUserRoomMembership Modified propFromRedux: room

--- client/src/components/Room/Room-Redux.js
+++ client/src/components/Room/Room-Redux.js
@@ -2,24 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 import RoomContainer from './Room-Container'
-import { fetchRoom } from '../../redux/actions/room-actions'
+import { setActiveRoom } from '../../redux/actions/room-actions'
 
-const RoomRedux = (props) => {
-    //TODO: When directly visiting a room's url, the store gets confusingly malfunctioned
-    //Example: On clicking join, the component is not updated and it remains same
-    //i.e., with the "Joined Room" button instead of reloading and showing the room window
-    //Upon inspecting, the redux store seems to consider the user as the member of the room
-    //However, as stated, the component doesn't seems to do the same and still shows the 
-    //Room not joined screen with the green (successful) "Room Joined" button
-    return <RoomContainer {...props} />;
-}
-
-
-const mapStateToProps = (state, ownProps) => {
-    return {
-        currentUser: state.user && state.user.user,
-        room: state.rooms[ownProps.roomId]
+const RoomRedux = ({ currentUserRoomMembership, ...props }) => {
+    const isCurrentUserRoomMember = () => {
+        return currentUserRoomMembership && currentUserRoomMembership.isRoomMember
     }
+    
+    return (
+        <RoomContainer
+            isCurrentUserRoomMember={isCurrentUserRoomMember()}
+            {...props}
+        />
+    )
 }
 
-export default connect(mapStateToProps, { fetchRoom })(RoomRedux)
+
+const mapStateToProps = (state) => {
+    const { room, currentUserRoomMembership } = state.rooms.activeRoom
+
+    return { room, currentUserRoomMembership }
+}
+
+export default connect(mapStateToProps, { setActiveRoom })(RoomRedux)
