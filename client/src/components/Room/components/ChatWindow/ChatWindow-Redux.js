16955ceb8d4892638348294e5ca647f658f4d418
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-18 11:34:17 +0530

    Redux component to fetch -> props: messages, dispatch: fetchRoomMessages

---
+++ client/src/components/Room/components/ChatWindow/ChatWindow-Redux.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+import ChatWindowContainer from './ChatWindow-Container'
+import { fetchRoomMessages } from '../../../../redux/actions/room-actions'
+
+const ChatWindowRedux = ({ messages, fetchRoomMessages, roomId }) => {
+    console.log({ messages })
+
+    return <ChatWindowContainer
+        roomId={roomId}
+        messages={messages}
+        fetchRoomMessages={fetchRoomMessages}
+    />
+}
+
+const mapStateToProps = (state) => {
+    return { messages: state.rooms.activeRoom.messages }
+}
+
+export default connect(mapStateToProps, { fetchRoomMessages })(ChatWindowRedux);
