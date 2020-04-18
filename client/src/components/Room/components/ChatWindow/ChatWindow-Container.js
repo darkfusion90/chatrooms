d097df5bd315cad5de87580a67fec0630d46a9fa
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-18 11:35:14 +0530

    Adjusted on prop change from rooms to messages Also, updates messageColors on componentDidUpdate if props.messages changes

--- client/src/components/Room/components/ChatWindow/ChatWindow-Container.js
+++ client/src/components/Room/components/ChatWindow/ChatWindow-Container.js
@@ -1,34 +1,50 @@
 import React from 'react';
+import _ from 'lodash'
 
 import ChatWindowView from './ChatWindow-View'
-import { assignColorsRandomly } from './utils'
+import { onNewMessage } from '../../../../api/socketIo'
+import { mapKeyToColor } from './utils'
 
 class ChatWindowContainer extends React.Component {
-    state = { randomAuthorColors: [] }
+    state = { messageColors: {} }
 
     componentDidMount() {
-        const { room } = this.props
-        if (room) {
-            this.setState({
-                randomAuthorColors: this.assignRandomColorsToMessageAuthors()
-            })
+        console.log('roomId: ', this.props.roomId)
+        onNewMessage(this.props.roomId, this.refreshMessages)
+        this.refreshMessages()
+    }
+
+    componentDidUpdate(prevProps) {
+        if (_.isEqual(this.props.messages, prevProps.messages)) {
+            return
         }
+        this.setState({messageColors: this.assignRandomColorsToMessageAuthors()})
+    }
+
+    refreshMessages = () => {
+        console.log('will refresh!')
+        this.props.fetchRoomMessages(this.props.roomId)
     }
 
     assignRandomColorsToMessageAuthors() {
-        const { room } = this.props
-        const messageAuthors = room.messages.map(message => message.author && message.author._id)
-        return assignColorsRandomly(messageAuthors)
+        const { messages } = this.props
+        const colors = {}
+
+        const authors = messages.map(message => message.author && message.author._id)
+        authors.forEach(author => colors[author] = mapKeyToColor(author))
+
+        return colors
     }
 
     render() {
-        const { room } = this.props
-        const { randomAuthorColors } = this.state
-        if (!room) {
+        const { messages } = this.props
+        const { messageColors } = this.state
+        console.log({ messageColors })
+        if (!messages) {
             return null
         }
 
-        return <ChatWindowView room={room} colorsAssignedToMessageAuthors={randomAuthorColors} />
+        return <ChatWindowView messages={messages} messageColors={messageColors} />
     }
 }
 
