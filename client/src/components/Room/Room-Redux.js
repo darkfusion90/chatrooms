1813e015ec6ad1e5ba03d9bb45f78cf878a78fd9
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-18 12:04:19 +0530

    Added prop: user._id Added dispatch: updateCurrentUserRoomMembership Added prop for Container: isCurrentUserRoomMembershipUndetermined

--- client/src/components/Room/Room-Redux.js
+++ client/src/components/Room/Room-Redux.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import isEmpty from 'is-empty'
 import { connect } from 'react-redux'
 
 import RoomContainer from './Room-Container'
-import { setActiveRoom } from '../../redux/actions/room-actions'
+import {
+    setActiveRoom,
+    updateCurrentUserRoomMembership
+} from '../../redux/actions/room-actions'
 
 const RoomRedux = ({ currentUserRoomMembership, ...props }) => {
     const isCurrentUserRoomMember = () => {
         return currentUserRoomMembership && currentUserRoomMembership.isRoomMember
     }
-    
+
     return (
         <RoomContainer
+            isCurrentUserRoomMembershipUndetermined={isEmpty(currentUserRoomMembership)}
             isCurrentUserRoomMember={isCurrentUserRoomMember()}
             {...props}
         />
@@ -19,9 +24,13 @@ const RoomRedux = ({ currentUserRoomMembership, ...props }) => {
 
 
 const mapStateToProps = (state) => {
+    const { user } = state.user
     const { room, currentUserRoomMembership } = state.rooms.activeRoom
 
-    return { room, currentUserRoomMembership }
+    return { currentUserId: user && user._id, room, currentUserRoomMembership }
 }
 
-export default connect(mapStateToProps, { setActiveRoom })(RoomRedux)
+export default connect(
+    mapStateToProps,
+    { setActiveRoom, updateCurrentUserRoomMembership }
+)(RoomRedux)
