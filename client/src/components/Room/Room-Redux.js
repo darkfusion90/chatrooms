ffead61b34b1adc1dd1a05920ff288d9e2d895f7
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-18 16:35:40 +0530

    Added dispatch: resetChatMessageForm: ()=>reset('chat-message-form') Added prop: chatMessageForm Both are used when sending message during Ctrl(or Command) + Enter key press

--- client/src/components/Room/Room-Redux.js
+++ client/src/components/Room/Room-Redux.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import isEmpty from 'is-empty'
 import { connect } from 'react-redux'
+import { reset } from 'redux-form'
 
 import RoomContainer from './Room-Container'
 import {
@@ -8,15 +9,21 @@ import {
     updateCurrentUserRoomMembership
 } from '../../redux/actions/room-actions'
 
-const RoomRedux = ({ currentUserRoomMembership, ...props }) => {
+const RoomRedux = ({ currentUserRoomMembership, chatMessageForm, ...props }) => {
     const isCurrentUserRoomMember = () => {
         return currentUserRoomMembership && currentUserRoomMembership.isRoomMember
+    }
+
+    const getCurrentMessage = () => {
+        const formValues = chatMessageForm && chatMessageForm.values
+        return formValues && formValues.message
     }
 
     return (
         <RoomContainer
             isCurrentUserRoomMembershipUndetermined={isEmpty(currentUserRoomMembership)}
             isCurrentUserRoomMember={isCurrentUserRoomMember()}
+            currentMessage={getCurrentMessage()}
             {...props}
         />
     )
@@ -27,10 +34,21 @@ const mapStateToProps = (state) => {
     const { user } = state.user
     const { room, currentUserRoomMembership } = state.rooms.activeRoom
 
-    return { currentUserId: user && user._id, room, currentUserRoomMembership }
+    return {
+        currentUserId: user && user._id,
+        chatMessageForm: state.form['chat-message-form'],
+        room,
+        currentUserRoomMembership
+    }
+}
+
+const mapDispatchToProps = {
+    setActiveRoom,
+    updateCurrentUserRoomMembership,
+    resetChatMessageForm: () => reset('chat-message-form')
 }
 
 export default connect(
     mapStateToProps,
-    { setActiveRoom, updateCurrentUserRoomMembership }
+    mapDispatchToProps
 )(RoomRedux)
