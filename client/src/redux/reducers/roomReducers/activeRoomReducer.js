ca85d03f434900f00994095e3d4d82ff78868d28
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-18 11:29:38 +0530

    Added reducer for FETCH_ROOM_MESSAGES

--- client/src/redux/reducers/roomReducers/activeRoomReducer.js
+++ client/src/redux/reducers/roomReducers/activeRoomReducer.js
@@ -2,33 +2,45 @@ import {
     DELETE_ROOM,
     UPDATE_USER_ROOM_MEMBERSHIP,
     UPDATE_ROOM,
-    SET_ACTIVE_ROOM
+    SET_ACTIVE_ROOM,
+    FETCH_ROOM_MESSAGES
 } from "../../action-constants";
 import initialStates from '../../initial-states'
 
 
+const getRoomIdInState = (state) => {
+    return state.room && state.room._id
+}
+
 const handleDeleteRoom = (state, roomId) => {
-    if (roomId === state.room._id) {
+    if (roomId === getRoomIdInState(state)) {
         return initialStates.rooms.activeRoom
     }
     return state
 }
 
 const handleUpdateRoom = (state, roomToUpdate) => {
-    if (roomToUpdate._id === state.room._id) {
+    if (roomToUpdate._id === getRoomIdInState(state)) {
         return { ...state, room: roomToUpdate }
     }
     return state
 }
 
 const handleUpdateUserRole = (state, { roomId, ...currentUserRoomMembership }) => {
-    if (roomId === state.room._id) {
+    if (roomId === getRoomIdInState(state)) {
         return { ...state, currentUserRoomMembership }
     }
 
     return state
 }
 
+const handleFetchRoomMessages = (state, { roomId, roomMessages }) => {
+    if (roomId === getRoomIdInState(state)) {
+        return { ...state, messages: roomMessages }
+    }
+
+    return state
+}
 
 export default (state = initialStates.rooms.activeRoom, action) => {
     switch (action.type) {
@@ -40,6 +52,8 @@ export default (state = initialStates.rooms.activeRoom, action) => {
             return handleUpdateRoom(state, action.payload)
         case UPDATE_USER_ROOM_MEMBERSHIP:
             return handleUpdateUserRole(state, action.payload)
+        case FETCH_ROOM_MESSAGES:
+            return handleFetchRoomMessages(state, action.payload)
         default:
             return state
     }
