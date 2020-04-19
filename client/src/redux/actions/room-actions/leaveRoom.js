44fd9aea5a1fe787d320ae478180133c535eb9a3
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-19 16:09:08 +0530

    Changes reflecting change in db (rooms.members is no longer applicable)

--- client/src/redux/actions/room-actions/leaveRoom.js
+++ client/src/redux/actions/room-actions/leaveRoom.js
@@ -1,32 +1,16 @@
 import { UPDATE_USER_ROOM_MEMBERSHIP } from '../../action-constants'
-import { roomMembers, search } from '../../../api/http'
+import { roomMembers } from '../../../api/http'
 
+const cb = () => { }
 
-const getResponseData = ({ data }) => data
-
-
-const getUserRoomMemberData = async (roomId, userId) => {
-    const searchResult = await search.searchRoomMember(roomId, userId).then(getResponseData)
-
-    if (searchResult.length === 0) {
-        return null
-    }
-
-    return searchResult.payload[0]
-}
-
-
-export default (room, onSuccess, onFailure) => async (dispatch, getState) => {
-    const { user: { user } } = getState()
-
-    try {
-        const roomMemberData = await getUserRoomMemberData(room._id, user._id)
-        roomMembers.leaveRoom(room._id, roomMemberData._id)
-            .then(_ => {
-                onSuccess()
-                dispatch({ type: UPDATE_USER_ROOM_MEMBERSHIP, payload: room._id })
-            }).catch(onFailure)
-    } catch (err) {
-        onFailure(err)
-    }
+export default (roomId, currentUserMemberId, onSuccess = cb, onFailure = cb) => async dispatch => {
+    roomMembers.leaveRoom(roomId, currentUserMemberId)
+        .then(response => {
+            console.log({ response })
+            onSuccess()
+            dispatch({
+                type: UPDATE_USER_ROOM_MEMBERSHIP,
+                payload: { roomId, isRoomMember: false, membership: response.data }
+            })
+        }).catch(onFailure)
 }
