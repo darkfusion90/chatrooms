fa5f97f39429f16ba1013508ebd965cb2457b659
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-23 07:05:39 +0530

    Added populate to query (path 'room' and 'requestedBy')

--- server/controllers/roomJoinRequests/utils.js
+++ server/controllers/roomJoinRequests/utils.js
@@ -1,6 +1,24 @@
 const createPromiseCallbackFunction = require('../../utils/promiseCallbackFunction')
 
 
+const POPULATE_CONFIG = {
+    room: {
+        path: 'room',
+        model: 'Room',
+        select: 'name'
+    },
+    requestedBy: {
+        path: 'requestedBy',
+        model: 'User',
+        select: 'username isRegistered'
+    }
+}
+
+const populateQuery = (query) => {
+    const { room, requestedBy } = POPULATE_CONFIG
+    return query.populate(room).populate(requestedBy)
+}
+
 const applyProjections = (query) => {
     if (query) {
         return query.select('-__v')
@@ -11,7 +29,7 @@ const executeQuery = (query, callback) => {
     if (query) {
         const withProjections = applyProjections(query)
         return createPromiseCallbackFunction((resolve, reject) => {
-            withProjections.then(resolve).catch(reject)
+            populateQuery(withProjections).then(resolve).catch(reject)
         }, callback)
     }
 }
