47de952fc22bc5addceaa6a7e7dab4f3c57ee1fe
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-23 07:05:14 +0530

    Added retrieval of join requests by userId

--- server/controllers/roomJoinRequests/retrieve.js
+++ server/controllers/roomJoinRequests/retrieve.js
@@ -2,18 +2,30 @@ const RoomJoinRequest = require('../../models/RoomJoinRequest')
 const { executeQuery } = require('./utils')
 
 
+const getJoinRequestInRoom = (roomId, requestId, callback) => {
+    const query = RoomJoinRequest.findOne({ _id: requestId, room: roomId })
+    return executeQuery(query, callback)
+}
+
 const getAllJoinRequestsInRoom = (roomId, callback) => {
     const query = RoomJoinRequest.find({ room: roomId })
     return executeQuery(query, callback)
 }
 
-const getJoinRequest = (roomId, requestId, callback) => {
-    const query = RoomJoinRequest.findOne({ room: roomId, requestedBy: requestId })
+const getJoinRequestByUser = (userId, requestId, callback) => {
+    const query = RoomJoinRequest.findOne({ _id: requestId, requestedBy: userId })
+    return executeQuery(query, callback)
+}
+
+const getAllJoinRequestsByUser = (userId, callback) => {
+    const query = RoomJoinRequest.find({ requestedBy: userId })
     return executeQuery(query, callback)
 }
 
 
 module.exports = {
+    getJoinRequestInRoom,
     getAllJoinRequestsInRoom,
-    getJoinRequest
+    getJoinRequestByUser,
+    getAllJoinRequestsByUser,
 }
