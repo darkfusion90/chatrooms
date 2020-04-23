91f16546412b7ee821a0237fec26aa8f51359b28
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-23 08:34:41 +0530

    Using priority in sort to prioritize 'initial' over 'accepted' which is prioritized over 'rejected'

--- client/src/components/RoomJoinRequestList/utils.js
+++ client/src/components/RoomJoinRequestList/utils.js
@@ -1,12 +1,27 @@
-const values = {
-    'rejected': 0,
-    'accepted': 1,
-    'initial': 2
+const PRIORITY_HIGH = 'high'
+const PRIORITY_MEDIUM = 'medium'
+const PRIORITY_LOW = 'low'
+
+
+const sortWithPriority = (a, b, priority) => {
+    if (a === b) {
+        return 0
+    } else if (priority[a] === PRIORITY_HIGH) {
+        return -1
+    } else if (priority[b] === PRIORITY_HIGH) {
+        return 1
+    } else {
+        return priority[a] === PRIORITY_LOW ? 1 : -1
+    }
 }
 
 const sortRequestList = (a, b) => {
-    console.log(`compare ${a.status} and ${b.status}: `, values[a.status] - values[b.status])
-    return values[a.status] - values[b.status]
+    const priority = {
+        'initial': PRIORITY_HIGH,
+        'accepted': PRIORITY_MEDIUM,
+        'failure': PRIORITY_LOW
+    }
+    return sortWithPriority(a.status, b.status, priority)
 }
 
 
