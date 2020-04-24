403e55baa8e3f1c3d93ee120082086116aee1eed
darkfusion90 <gauravsharmaa341@gmail.com>
2020-04-24 07:06:00 +0530

    Added more detailed Header Removed empty container rendering on empty list

--- client/src/components/RoomJoinRequestList/RoomJoinRequestList-View.js
+++ client/src/components/RoomJoinRequestList/RoomJoinRequestList-View.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { Card, Container, ListGroup } from 'react-bootstrap'
-import isEmpty from 'is-empty'
-import RoomJoinRequestListItemView from './components/RoomJoinRequestListItem/RoomJoinRequestListItem-View';
+
+import { RoomJoinRequestListItem, RoomJoinRequestListHeader } from './components'
 
 
 const RoomJoinRequestListView = ({ joinRequestList }) => {
-    if (isEmpty(joinRequestList)) {
-        return (
-            <Container className='centered-content' fluid>
-                Nothing to show
-            </Container>
-        )
-    }
-
     const renderList = () => {
         return joinRequestList.map(val => {
-            return <RoomJoinRequestListItemView key={val._id} joinRequest={val} />
+            return <RoomJoinRequestListItem key={val._id} joinRequest={val} />
         })
     }
 
@@ -23,7 +15,9 @@ const RoomJoinRequestListView = ({ joinRequestList }) => {
         <Container className='h-100 py-3 px-lg-5 px-md-0 px-sm-0'>
             <Card>
                 <Card.Header>
-                    <Card.Title>Room Join Requests</Card.Title>
+                    <Card.Title>
+                        <RoomJoinRequestListHeader joinRequestList={joinRequestList} />
+                    </Card.Title>
                 </Card.Header>
                 <Card.Body className='pre-scrollable'>
                     <ListGroup className='mx-0' variant='flush'>
