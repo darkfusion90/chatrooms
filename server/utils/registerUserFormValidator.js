52e82c40f88346c90002ed5ab258b3b0ac828c0e
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-13 18:04:36 +0530

    Removed password matching (using confirm-password) while registering

--- server/utils/registerUserFormValidator.js
+++ server/utils/registerUserFormValidator.js
@@ -5,7 +5,6 @@ const isEmpty = require('is-empty')
 const convertEmptyFieldsToEmptyStrings = (data) => {
     data.username = isEmpty(data.username) ? '' : data.username
     data.password = isEmpty(data.password) ? '' : data.password
-    data.confirmPassword = isEmpty(data.confirmPassword) ? '' : data.confirmPassword
 }
 
 const ensureDataNotEmpty = (data) => {
@@ -18,21 +17,12 @@ const ensureDataNotEmpty = (data) => {
         errors.passwordEmpty = 'Password field cannot be empty'
     }
 
-    if (validator.isEmpty(data.confirmPassword)) {
-        errors.confirmPasswordEmpty = 'Confirm Password field cannot be empty'
-    }
-
     return errors
 }
 
 module.exports = (data) => {
     convertEmptyFieldsToEmptyStrings(data)
     const errors = { ...ensureDataNotEmpty(data) }
-    const { password, confirmPassword } = data
-
-    if (!validator.equals(password, confirmPassword)) {
-        errors.passwordsMismatch = 'Password and Confirm Password should match'
-    }
 
     return {
         errors: errors,
