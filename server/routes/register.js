f2bab6484eba966826f86f4f6f5448b754c1342a
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-13 18:09:01 +0530

    Added TODO (to remove the usage of route /api/register and replace with POST /api/user)

--- server/routes/register.js
+++ server/routes/register.js
@@ -3,6 +3,7 @@ const httpStatusCodes = require('../constants/httpStatusCodes')
 module.exports = (req, res) => {
     req.session.redirectedFromRegister = true
     req.session.save()
+    // TODO(29th June 2020): Remove this route and make the React client use the updated route (POST /api/user)
     //Using 307 Temporary Redirect to make sure the redirect is done using the same method as /api/register (i.e, POST)
     res.redirect(httpStatusCodes.TEMPORARY_REDIRECT, '/api/user')
 }
